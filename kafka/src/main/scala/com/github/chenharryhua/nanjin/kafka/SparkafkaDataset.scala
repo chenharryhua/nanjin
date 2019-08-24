97cc68dee82a45352ea6b7b09abfd9b6800c33a1
Harry Chen <chen.harry.hua@gmail.com>
2019-08-24 17:24:17 +1000

    localize injection methods

--- kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/SparkafkaDataset.scala
+++ kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/SparkafkaDataset.scala
@@ -20,16 +20,17 @@ import scala.collection.JavaConverters._
 import scala.reflect.ClassTag
 
 object SparkafkaDataset extends BitraverseKafkaRecord {
-  implicit val timestampTypeInjection: Injection[TimestampType, String] =
+  implicit private val timestampTypeInjection: Injection[TimestampType, String] =
     new Injection[TimestampType, String] {
       override def apply(a: TimestampType): String  = a.name
       override def invert(b: String): TimestampType = TimestampType.forName(b)
     }
 
-  implicit val headersInjection: Injection[Headers, String] = new Injection[Headers, String] {
-    override def apply(a: Headers): String  = a.toString
-    override def invert(b: String): Headers = new RecordHeaders()
-  }
+  implicit private val headersInjection: Injection[Headers, String] =
+    new Injection[Headers, String] {
+      override def apply(a: Headers): String  = a.toString
+      override def invert(b: String): Headers = new RecordHeaders()
+    }
 
   private def props(maps: Map[String, String]): util.Map[String, Object] =
     (Map(
