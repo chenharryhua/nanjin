fd6058aec550dfb63e8d511245500cc046412b48
Harry Chen <chen.harry.hua@gmail.com>
2019-08-24 10:55:08 +1000

    message must be showable

--- kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/ShowKafkaMessage.scala
+++ kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/ShowKafkaMessage.scala
@@ -7,86 +7,42 @@ import fs2.kafka.{CommittableConsumerRecord => Fs2CommittableMessage}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.{ProducerRecord, RecordMetadata}
 
-private[kafka] trait LowerPriorityShow extends BitraverseFs2Message {
-  protected def buildCR[K, V](
-    t: ConsumerRecord[K, V],
-    key: Option[String],
-    value: Option[String]): String = {
-    val (utc, local) = utils.kafkaTimestamp(t.timestamp())
-    s"""
-       |consumer record:
-       |topic:        ${t.topic()}
-       |partition:    ${t.partition()}
-       |offset:       ${t.offset()}
-       |timestamp:    ${t.timestamp()}
-       |utc:          $utc
-       |local-time:   $local
-       |ts-type:      ${t.timestampType()}
-       |key:          ${key.getOrElse("null")}
-       |value:        ${value.getOrElse("null")}
-       |key-size:     ${t.serializedKeySize()}
-       |value-size:   ${t.serializedValueSize()}
-       |headers:      ${t.headers()}
-       |leader epoch: ${t.leaderEpoch}""".stripMargin
-  }
-  protected def buildPR[K, V](
-    t: ProducerRecord[K, V],
-    key: Option[String],
-    value: Option[String]): String = {
-    val (utc, local) = utils.kafkaTimestamp(t.timestamp())
-    s"""
-       |producer record:
-       |topic:      ${t.topic}
-       |partition:  ${t.partition}
-       |timestamp:  ${t.timestamp()}
-       |utc:        $utc
-       |local-time: $local
-       |key:        ${key.getOrElse("null")}
-       |value:      ${value.getOrElse("null")}
-       |headers:    ${t.headers}""".stripMargin
-  }
-
-  implicit protected def showConsumerRecords2[K, V]: Show[ConsumerRecord[K, V]] =
-    (t: ConsumerRecord[K, V]) =>
-      buildCR(t, Option(t.key).map(_.toString), Option(t.value).map(_.toString))
-
-  implicit protected def showProducerRecord2[K, V]: Show[ProducerRecord[K, V]] =
-    (t: ProducerRecord[K, V]) =>
-      buildPR(t, Option(t.key).map(_.toString), Option(t.value).map(_.toString))
-
-  implicit protected def showFs2CommittableMessage2[F[_], K, V]
-    : Show[Fs2CommittableMessage[F, K, V]] =
-    (t: Fs2CommittableMessage[F, K, V]) => isoFs2ComsumerRecord.get(t.record).show
-
-  implicit protected def showAkkaCommittableMessage2[K, V]: Show[AkkaCommittableMessage[K, V]] =
-    (t: AkkaCommittableMessage[K, V]) => t.record.show
-}
-
-private[kafka] trait LowPriorityShow extends LowerPriorityShow {
-  implicit protected def showConsumerRecords1[K, V: Show]: Show[ConsumerRecord[K, V]] =
-    (t: ConsumerRecord[K, V]) =>
-      buildCR(t, Option(t.key).map(_.toString), Option(t.value).map(_.show))
-
-  implicit protected def showProducerRecord1[K, V: Show]: Show[ProducerRecord[K, V]] =
-    (t: ProducerRecord[K, V]) =>
-      buildPR(t, Option(t.key).map(_.toString), Option(t.value).map(_.show))
-
-  implicit protected def showFs2CommittableMessage1[F[_], K, V: Show]
-    : Show[Fs2CommittableMessage[F, K, V]] =
-    (t: Fs2CommittableMessage[F, K, V]) => isoFs2ComsumerRecord.get(t.record).show
-
-  implicit protected def showAkkaCommittableMessage1[K, V: Show]
-    : Show[AkkaCommittableMessage[K, V]] =
-    (t: AkkaCommittableMessage[K, V]) => t.record.show
-}
-
-trait ShowKafkaMessage extends LowPriorityShow {
+trait ShowKafkaMessage extends BitraverseFs2Message {
 
   implicit protected def showConsumerRecords[K: Show, V: Show]: Show[ConsumerRecord[K, V]] =
-    (t: ConsumerRecord[K, V]) => buildCR(t, Option(t.key).map(_.show), Option(t.value).map(_.show))
+    (t: ConsumerRecord[K, V]) => {
+      val (utc, local) = utils.kafkaTimestamp(t.timestamp())
+      s"""
+         |consumer record:
+         |topic:        ${t.topic()}
+         |partition:    ${t.partition()}
+         |offset:       ${t.offset()}
+         |local-time:   $local
+         |ts-type:      ${t.timestampType()}
+         |key:          ${Option(t.key).map(_.show).getOrElse("null")}
+         |value:        ${Option(t.value).map(_.show).getOrElse("null")}
+         |key-size:     ${t.serializedKeySize()}
+         |value-size:   ${t.serializedValueSize()}
+         |timestamp:    ${t.timestamp()}
+         |utc:          $utc
+         |headers:      ${t.headers()}
+         |leader epoch: ${t.leaderEpoch}""".stripMargin
+    }
 
   implicit protected def showProducerRecord[K: Show, V: Show]: Show[ProducerRecord[K, V]] =
-    (t: ProducerRecord[K, V]) => buildPR(t, Option(t.key).map(_.show), Option(t.value).map(_.show))
+    (t: ProducerRecord[K, V]) => {
+      val (utc, local) = utils.kafkaTimestamp(t.timestamp())
+      s"""
+         |producer record:
+         |topic:      ${t.topic}
+         |partition:  ${t.partition}
+         |local-time: $local
+         |key:        ${Option(t.key).map(_.show).getOrElse("null")}
+         |value:      ${Option(t.value).map(_.show).getOrElse("null")}
+         |timestamp:  ${t.timestamp()}
+         |utc:        $utc
+         |headers:    ${t.headers}""".stripMargin
+    }
 
   implicit protected def showFs2CommittableMessage[F[_], K: Show, V: Show]
     : Show[Fs2CommittableMessage[F, K, V]] =
