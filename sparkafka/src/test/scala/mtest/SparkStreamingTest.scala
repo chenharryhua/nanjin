c7276322979a6c1896bfa82ee861c9c807383484
Harry Chen <chen.harry.hua@gmail.com>
2019-08-26 22:02:47 +1000

    checkpoint

--- sparkafka/src/test/scala/mtest/SparkStreamingTest.scala
+++ sparkafka/src/test/scala/mtest/SparkStreamingTest.scala
@@ -2,15 +2,21 @@ package mtest
 
 import cats.effect.IO
 import cats.implicits._
-import com.github.chenharryhua.nanjin.sparkafka.{SparkConsumerRecord, SparkafkaStream}
-import org.apache.spark.sql.Dataset
+import com.github.chenharryhua.nanjin.sparkafka.SparkafkaStream
+import org.apache.spark.sql.streaming.Trigger
 import org.scalatest.FunSuite
+import frameless.cats.implicits._
+import scala.concurrent.duration._
 
 class SparkStreamingTest extends FunSuite {
   test("run streaming") {
     spark.use { implicit s =>
-      val df  = SparkafkaStream.sstream(topics.pencil_topic)
-      val kdf = df.dataset.writeStream.outputMode("append").format("console")
+      val df = SparkafkaStream.sstream(topics.pencil_topic)
+      val kdf = df.dataset.writeStream
+        .trigger(Trigger.ProcessingTime(60.seconds))
+        .option("checkpointLocation", "checkpoint-test")
+        .outputMode("append")
+        .format("console")
       IO(kdf.start()) >> IO.never //.map(println)
     }.unsafeRunSync()
   }
