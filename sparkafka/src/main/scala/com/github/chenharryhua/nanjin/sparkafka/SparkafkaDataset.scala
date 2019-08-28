747c2aa30844519d25c5a6600c58fa2a27c79d3d
Harry Chen <chen.harry.hua@gmail.com>
2019-08-29 09:13:45 +1000

    reword

--- sparkafka/src/main/scala/com/github/chenharryhua/nanjin/sparkafka/SparkafkaDataset.scala
+++ sparkafka/src/main/scala/com/github/chenharryhua/nanjin/sparkafka/SparkafkaDataset.scala
@@ -6,11 +6,10 @@ import java.util
 import cats.Monad
 import cats.implicits._
 import com.github.chenharryhua.nanjin.kafka.{BitraverseKafkaRecord, KafkaTopic}
-import frameless.{SparkDelay, TypedDataset, TypedEncoder}
+import frameless.{TypedDataset, TypedEncoder}
 import monocle.function.At.remove
-import org.apache.kafka.clients.consumer.{ConsumerConfig, ConsumerRecord}
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.kafka010.{KafkaUtils, LocationStrategies}
 
@@ -25,64 +24,63 @@ object SparkafkaDataset extends BitraverseKafkaRecord {
       "value.deserializer" -> classOf[ByteArrayDeserializer].getName) ++
       remove(ConsumerConfig.CLIENT_ID_CONFIG)(maps)).mapValues[Object](identity).asJava
 
-  private def rdd[F[_]: Monad, K, V](
-    spark: SparkSession,
+  def rawDS[F[_]: Monad, K, V](
     topic: KafkaTopic[F, K, V],
     start: LocalDateTime,
-    end: LocalDateTime): F[RDD[ConsumerRecord[Array[Byte], Array[Byte]]]] =
-    topic.consumer.offsetRangeFor(start, end).map { gtp =>
-      KafkaUtils.createRDD[Array[Byte], Array[Byte]](
-        spark.sparkContext,
-        props(topic.kafkaConsumerSettings.props),
-        SparkOffsets.offsetRange(gtp),
-        LocationStrategies.PreferConsistent)
-    }
+    end: LocalDateTime)(
+    implicit spark: SparkSession): F[TypedDataset[SparkConsumerRecord[Array[Byte], Array[Byte]]]] =
+    topic.consumer
+      .offsetRangeFor(start, end)
+      .map { gtp =>
+        KafkaUtils
+          .createRDD[Array[Byte], Array[Byte]](
+            spark.sparkContext,
+            props(topic.kafkaConsumerSettings.props),
+            SparkOffsets.offsetRange(gtp),
+            LocationStrategies.PreferConsistent)
+          .map(SparkConsumerRecord.from[Array[Byte], Array[Byte]])
+      }
+      .map(TypedDataset.create(_))
 
   def dataset[F[_]: Monad, K: TypedEncoder, V: TypedEncoder](
     topic: => KafkaTopic[F, K, V],
     start: LocalDateTime,
     end: LocalDateTime)(implicit spark: SparkSession): F[TypedDataset[SparkConsumerRecord[K, V]]] =
-    rdd(spark, topic, start, end).map {
-      _.mapPartitions(msgs => {
+    rawDS(topic, start, end).map {
+      _.deserialized.mapPartitions(msgs => {
         val t = topic
-        msgs.map(m => SparkConsumerRecord.from(m.bimap(t.keyIso.get, t.valueIso.get)))
+        msgs.map(m => m.bimap(t.keyIso.get, t.valueIso.get))
       })
-    }.map(TypedDataset.create(_))
+    }
 
   def safeDataset[F[_]: Monad, K: TypedEncoder, V: TypedEncoder](
     topic: => KafkaTopic[F, K, V],
     start: LocalDateTime,
     end: LocalDateTime)(implicit spark: SparkSession): F[TypedDataset[SparkConsumerRecord[K, V]]] =
-    rdd(spark, topic, start, end)
-      .map(_.mapPartitions(msgs => {
-        val t = topic
-        msgs
-          .flatMap(_.bitraverse[Option, K, V](t.keyPrism.getOption, t.valuePrism.getOption))
-          .map(SparkConsumerRecord.from)
-      }))
-      .map(TypedDataset.create(_))
+    rawDS(topic, start, end).map(_.deserialized.mapPartitions(msgs => {
+      val t = topic
+      msgs.flatMap(_.bitraverse[Option, K, V](t.keyPrism.getOption, t.valuePrism.getOption))
+    }))
 
   def valueDataset[F[_]: Monad, K, V: TypedEncoder: ClassTag](
     topic: => KafkaTopic[F, K, V],
     start: LocalDateTime,
     end: LocalDateTime)(implicit spark: SparkSession): F[TypedDataset[V]] =
-    rdd(spark, topic, start, end).map {
-      _.mapPartitions(msgs => {
+    rawDS(topic, start, end).map {
+      _.deserialized.mapPartitions(msgs => {
         val t = topic
-        msgs.map(m => SparkConsumerRecord.from(m.bimap(identity, t.valueIso.get)).value)
+        msgs.map(m => m.bimap(identity, t.valueIso.get).value)
       })
-    }.map(TypedDataset.create(_))
+    }
 
   def safeValueDataset[F[_]: Monad, K, V: TypedEncoder: ClassTag](
     topic: => KafkaTopic[F, K, V],
     start: LocalDateTime,
     end: LocalDateTime)(implicit spark: SparkSession): F[TypedDataset[V]] =
-    rdd(spark, topic, start, end)
-      .map(_.mapPartitions(msgs => {
-        val t = topic
-        msgs
-          .flatMap(_.bitraverse[Option, Array[Byte], V](Some(_), t.valuePrism.getOption))
-          .map(m => SparkConsumerRecord.from(m).value)
-      }))
-      .map(TypedDataset.create(_))
+    rawDS(topic, start, end).map(_.deserialized.mapPartitions(msgs => {
+      val t = topic
+      msgs
+        .flatMap(_.bitraverse[Option, Array[Byte], V](Some(_), t.valuePrism.getOption))
+        .map(m => m.value)
+    }))
 }
