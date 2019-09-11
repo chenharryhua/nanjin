93f71263becc74b2879f7fbb1a807bba133ae792
Harry Chen <chen.harry.hua@gmail.com>
2019-09-11 20:56:13 +1000

    trydecode test

--- codec/src/test/scala/mtest/KafkaAvroTest.scala
+++ codec/src/test/scala/mtest/KafkaAvroTest.scala
@@ -3,13 +3,25 @@ package mtest
 import com.github.chenharryhua.nanjin.codec.CodecException.InvalidObjectException
 import com.github.chenharryhua.nanjin.codec.SerdeOf
 import org.scalatest.funsuite.AnyFunSuite
+
 import scala.collection.JavaConverters._
+import scala.util.Success
 final case class Foo(a: String, b: Int)
 final case class Bar(a: Int, b: String)
 
 class KafkaAvroTest extends AnyFunSuite {
   val fooCodec = SerdeOf[Foo].asValue(sr).codec("avro.test")
   val barCode  = SerdeOf[Bar].asKey(sr).codec("avro.test")
+
+  test("tryDecode should fail if codec not match") {
+    assert(barCode.tryDecode(fooCodec.encode(Foo("a", 0))).isFailure)
+  }
+
+  test("tryDecode should be identical") {
+    val foo = Foo("a", 0)
+    assert(fooCodec.tryDecode(fooCodec.encode(foo)) === Success(foo))
+  }
+
   test("should throw InvalidObjectException when codec does not match") {
     assertThrows[InvalidObjectException](barCode.decode(fooCodec.encode(Foo("a", 0))))
   }
