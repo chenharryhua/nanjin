a72de61441a16616d22fcafbfb68f85370e10aad
Harry Chen <chen.harry.hua@gmail.com>
2019-09-29 16:38:12 +1000

    cogen

--- codec/src/test/scala/mtest/ArbitraryData.scala
+++ codec/src/test/scala/mtest/ArbitraryData.scala
@@ -22,6 +22,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.scalacheck.Arbitrary.arbitrary
 import org.scalacheck.{Arbitrary, Gen}
+import org.scalacheck.Cogen
 
 final case class PrimitiveTypeCombined(
   a: Int,
@@ -36,53 +37,40 @@ trait ArbitraryData extends GenKafkaMessage with GenFs2Message with GenAkkaMessa
   implicit val abKafkaConsumerRecord: Arbitrary[ConsumerRecord[Int, Int]] =
     Arbitrary(genConsumerRecord)
 
-  implicit val abKafkaConsumerRecordF
-    : Arbitrary[ConsumerRecord[Int, Int] => ConsumerRecord[Int, Int]] =
-    Arbitrary(for {
-      i <- arbitrary[Int]
-      j <- arbitrary[Int]
-    } yield (cm: ConsumerRecord[Int, Int]) => cm.bimap(_ + i, _ - j))
+  implicit val cogenConsumerRecord: Cogen[ConsumerRecord[Int, Int]] =
+    Cogen(m => m.key.toLong + m.value.toLong)
 
   implicit val abKafkaProducerRecord: Arbitrary[ProducerRecord[Int, Int]] =
     Arbitrary(genProducerRecord)
 
-  implicit val abKafkaProducerRecordF
-    : Arbitrary[ProducerRecord[Int, Int] => ProducerRecord[Int, Int]] =
-    Arbitrary(for {
-      i <- arbitrary[Int]
-      j <- arbitrary[Int]
-    } yield (cm: ProducerRecord[Int, Int]) => cm.bimap(_ - i, _ + j))
+  implicit val cogenKafkaProducerRecordF: Cogen[ProducerRecord[Int, Int]] =
+    Cogen(m => m.key.toLong + m.value.toLong)
 
   implicit val abKafkaProducerRecords: Arbitrary[Chunk[ProducerRecord[Int, Int]]] =
     Arbitrary(
       Gen.containerOfN[List, ProducerRecord[Int, Int]](10, genProducerRecord).map(Chunk.seq))
 
-  implicit val abKafkaProducerRecordsF
-    : Arbitrary[Chunk[ProducerRecord[Int, Int]] => Chunk[ProducerRecord[Int, Int]]] = {
-    Arbitrary((a: Chunk[ProducerRecord[Int, Int]]) => a)
-  }
+  implicit val cogenKafkaProducerRecords: Cogen[Chunk[ProducerRecord[Int, Int]]] =
+    Cogen(_.size.toLong)
 
   //fs2
   implicit val abFs2ConsumerRecord: Arbitrary[Fs2ConsumerRecord[Int, Int]] =
     Arbitrary(genFs2ConsumerRecord)
 
-  implicit val abFs2ConsumerRecordF
-    : Arbitrary[Fs2ConsumerRecord[Int, Int] => Fs2ConsumerRecord[Int, Int]] =
-    Arbitrary((cm: Fs2ConsumerRecord[Int, Int]) => cm.bimap(_ - 1, _ + 1))
+  implicit val cogenFs2ConsumerRecordF: Cogen[Fs2ConsumerRecord[Int, Int]] =
+    Cogen(m => m.key.toLong + m.value.toLong)
 
   implicit val abFs2ConsumerMessage: Arbitrary[Fs2ConsumerMessage[IO, Int, Int]] =
     Arbitrary(genFs2ConsumerMessage)
 
-  implicit val abFs2ConsumerMessageF
-    : Arbitrary[Fs2ConsumerMessage[IO, Int, Int] => Fs2ConsumerMessage[IO, Int, Int]] =
-    Arbitrary((cm: Fs2ConsumerMessage[IO, Int, Int]) => cm)
+  implicit val cogenFs2ConsumerMessageF: Cogen[Fs2ConsumerMessage[IO, Int, Int]] =
+    Cogen(m => m.record.key.toLong + m.record.value.toLong)
 
   implicit val abFs2ProducerRecord: Arbitrary[Fs2ProducerRecord[Int, Int]] =
     Arbitrary(genFs2ProducerRecord)
 
-  implicit val abFs2ProducerRecordF
-    : Arbitrary[Fs2ConsumerRecord[Int, Int] => Fs2ConsumerRecord[Int, Int]] =
-    Arbitrary((pr: Fs2ConsumerRecord[Int, Int]) => pr)
+  implicit val cogenFs2ProducerRecordF: Cogen[Fs2ConsumerRecord[Int, Int]] =
+    Cogen(m => m.key.toLong + m.value.toLong)
 
   implicit val abFs2ProducerRecords: Arbitrary[Fs2ProducerRecords[Int, Int, String]] =
     Arbitrary(genFs2ProducerRecords)
@@ -99,17 +87,15 @@ trait ArbitraryData extends GenKafkaMessage with GenFs2Message with GenAkkaMessa
   implicit val abAkkaConsumerRecord: Arbitrary[AkkaConsumerMessage[Int, Int]] =
     Arbitrary(genAkkaConsumerMessage)
 
-  implicit val abAkkaConsumerRecordF
-    : Arbitrary[AkkaConsumerMessage[Int, Int] => AkkaConsumerMessage[Int, Int]] =
-    Arbitrary((cm: AkkaConsumerMessage[Int, Int]) => cm)
+  implicit val cogenAkkaConsumerRecordF: Cogen[AkkaConsumerMessage[Int, Int]] =
+    Cogen(_.record.key().toLong)
 
   implicit val abAkkaProducerRecord: Arbitrary[AkkaProducerMessage[Int, Int, String]] =
     Arbitrary(genAkkaProducerMessage)
 
-  implicit val abAkkaProducerRecordF
-    : Arbitrary[AkkaProducerMessage[Int, Int, String] => AkkaProducerMessage[Int, Int, String]] =
-    Arbitrary((pr: AkkaProducerMessage[Int, Int, String]) => pr)
-
+  implicit val cogenAkkaProducerRecordF: Cogen[AkkaProducerMessage[Int, Int, String]] =
+    Cogen(_.record.value().toLong)
+    
   implicit val abAkkaProducerRecords: Arbitrary[AkkaMultiMessage[Int, Int, String]] = {
     Arbitrary(genAkkaProducerMultiMessage)
   }
