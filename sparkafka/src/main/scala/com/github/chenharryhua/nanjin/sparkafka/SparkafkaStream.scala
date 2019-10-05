1801b4677d3e646ef85ca7168e128df24c7d13db
Harry Chen <chen.harry.hua@gmail.com>
2019-10-05 18:47:07 +1000

    sparkafka stream

--- sparkafka/src/main/scala/com/github/chenharryhua/nanjin/sparkafka/SparkafkaStream.scala
+++ sparkafka/src/main/scala/com/github/chenharryhua/nanjin/sparkafka/SparkafkaStream.scala
@@ -1,6 +1,7 @@
 package com.github.chenharryhua.nanjin.sparkafka
 import cats.effect.Bracket
 import cats.implicits._
+import com.github.chenharryhua.nanjin.codec._
 import com.github.chenharryhua.nanjin.kafka.KafkaTopic
 import frameless.{TypedDataset, TypedEncoder}
 import monocle.function.At.remove
@@ -15,7 +16,7 @@ object SparkafkaStream {
     val rm2 = remove(ConsumerConfig.GROUP_ID_CONFIG)(_: Map[String, String])
     rm1.andThen(rm2)(m).map { case (k, v) => s"kafka.$k" -> v }
   }
-  /*
+
   def sstream[F[_], K: TypedEncoder, V: TypedEncoder](topic: => KafkaTopic[F, K, V])(
     implicit spark: SparkSession): TypedDataset[SparkafkaConsumerRecord[K, V]] = {
     import spark.implicits._
@@ -28,13 +29,15 @@ object SparkafkaStream {
           .load()
           .as[SparkafkaConsumerRecord[Array[Byte], Array[Byte]]])
       .deserialized
-      .map { msg =>
+      .mapPartitions { msgs =>
         val t = topic
-        msg.bimap(t.keyCodec.decode, t.valueCodec.decode)
+        val decoder = (msg: SparkafkaConsumerRecord[Array[Byte], Array[Byte]]) =>
+          msg.bimap(t.keyCodec.prism.getOption, t.valueCodec.prism.getOption).flattenKeyValue
+        msgs.map(decoder)
       }
   }
 
   def start[F[_], A](dsw: DataStreamWriter[A])(implicit bkt: Bracket[F, Throwable]): F[Unit] =
     bkt.bracket(bkt.pure(dsw.start))(s => bkt.pure(s.awaitTermination()))(s => bkt.pure(s.stop()))
-    */
+
 }
