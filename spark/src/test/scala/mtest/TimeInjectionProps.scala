dd15e3aee722c08c2becb1fa5e77caa49b6dbf21
Harry Chen <chen.harry.hua@gmail.com>
2019-10-22 20:24:49 +1100

    timezone

--- spark/src/test/scala/mtest/TimeInjectionProps.scala
+++ spark/src/test/scala/mtest/TimeInjectionProps.scala
@@ -1,20 +1,27 @@
 package mtest
 
-import java.sql.Date
-import java.time.{Instant, LocalDate, LocalDateTime, ZoneId}
+import java.sql.{Date, Timestamp}
+import java.time._
 
-import com.github.chenharryhua.nanjin.spark._
+import com.fortysevendeg.scalacheck.datetime.jdk8.ArbitraryJdk8._
 import com.github.chenharryhua.nanjin.database._
-
+import com.github.chenharryhua.nanjin.spark._
 import frameless.TypedEncoder
+import org.scalacheck.Prop.{forAll, propBoolean}
 import org.scalacheck.Properties
-import java.sql.Timestamp
 
-class TimeInjectionProps extends Properties("Injection") {
+class TimeInjectionProps extends Properties("date time") {
   implicit val zoneId = ZoneId.systemDefault()
   val date            = TypedEncoder[Date]
   val timestamp       = TypedEncoder[Timestamp]
   val lcoaldate       = TypedEncoder[LocalDate]
   val localdatetime   = TypedEncoder[LocalDateTime]
   val instant         = TypedEncoder[Instant]
+
+  property("timezone has no effect on epoch-second") = forAll { (ins: Instant) =>
+    val tz1: ZoneId = ZoneId.of("Australia/Sydney")
+    val tz2: ZoneId = ZoneId.of("America/Phoenix")
+    ZonedDateTime.ofInstant(ins, tz1).toEpochSecond ==
+      ZonedDateTime.ofInstant(ins, tz2).toEpochSecond
+  }
 }
