148a7b6d2873f8ac4e035290b674c745200fc92e
Harry Chen <chen.harry.hua@gmail.com>
2019-10-23 18:45:00 +1100

    invertable injection tests

--- spark/src/test/scala/mtest/TimeInjectionProps.scala
+++ spark/src/test/scala/mtest/TimeInjectionProps.scala
@@ -6,9 +6,10 @@ import java.time._
 import com.fortysevendeg.scalacheck.datetime.jdk8.ArbitraryJdk8._
 import com.github.chenharryhua.nanjin.database._
 import com.github.chenharryhua.nanjin.spark._
-import frameless.TypedEncoder
+import frameless.{Injection, TypedEncoder}
 import org.scalacheck.Prop.{forAll, propBoolean}
 import org.scalacheck.Properties
+import cats.implicits._
 
 class TimeInjectionProps extends Properties("date time") {
   implicit val zoneId = ZoneId.systemDefault()
@@ -25,5 +26,25 @@ class TimeInjectionProps extends Properties("date time") {
     val tz2: ZoneId = ZoneId.of("America/Phoenix")
     ZonedDateTime.ofInstant(ins, tz1).toEpochSecond ==
       ZonedDateTime.ofInstant(ins, tz2).toEpochSecond
+  }
+  property("invertable LocalDateTime") = forAll { (ins: LocalDateTime) =>
+    val in = implicitly[Injection[LocalDateTime, Timestamp]]
+    in.invert(in.apply(ins)) == ins
+  }
+  property("invertable ZonedDateTime") = forAll { (ins: ZonedDateTime) =>
+    val in = implicitly[Injection[ZonedDateTime, JavaZonedDateTime]]
+    in.invert(in.apply(ins)) == ins
+  }
+  property("invertable OffsetDateTime") = forAll { (ins: ZonedDateTime) =>
+    val in = implicitly[Injection[OffsetDateTime, JavaOffsetDateTime]]
+    in.invert(in.apply(ins.toOffsetDateTime)) == ins.toOffsetDateTime
+  }
+  property("invertable LocalDate") = forAll { (ins: LocalDate) =>
+    val in = implicitly[Injection[LocalDate, Date]]
+    in.invert(in.apply(ins)) == ins
+  }
+  property("invertable Instant") = forAll { (ins: Instant) =>
+    val in = implicitly[Injection[Instant, Timestamp]]
+    in.invert(in.apply(ins)) == ins
   }
 }
