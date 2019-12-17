f693fd1c88972de08163724ec92ada837ec3b649
Harry Chen <chen.harry.hua@gmail.com>
2019-12-17 15:10:18 +1100

    tests

--- kafka/src/test/scala/mtest/SchemaRegistryTest.scala
+++ kafka/src/test/scala/mtest/SchemaRegistryTest.scala
@@ -14,16 +14,16 @@ class SchemaRegistryTest extends AnyFunSuite {
   val topic: KafkaTopic[IO, Int, trip_record] =
     ctx.topic[Int, trip_record](nyc)
 
-  ignore("latest schema") {
+  test("latest schema") {
     topic.schemaRegistry.latestMeta.map(_.show).unsafeRunSync()
   }
-  ignore("compatiable test") {
+  test("compatiable test") {
     topic.schemaRegistry.testCompatibility.map(println).unsafeRunSync
   }
-  ignore("register schema") {
+  test("register schema") {
     topic.schemaRegistry.register.unsafeRunSync()
   }
-  ignore("schema registry is not necessarily configured if it is not used") {
+  test("schema registry is not necessarily configured if it is not used") {
     val noRegistry = KafkaSettings.empty.withBrokers("localhost:9092").ioContext
     val topic      = noRegistry.topic[Int, Int]("no_schema_registry_test")
     topic.producer.send(1, 1).unsafeRunSync()
