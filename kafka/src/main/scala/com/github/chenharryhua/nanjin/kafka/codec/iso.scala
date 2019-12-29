ccbbcda1053de00fb142ead6b41c2076f81fa1b1
Harry Chen <chen.harry.hua@gmail.com>
2019-12-29 21:08:17 +1100

    iso

--- kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/codec/iso.scala
+++ kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/codec/iso.scala
@@ -16,16 +16,16 @@ import scala.compat.java8.OptionConverters._
 
 object iso {
 
-  implicit def isoIdentityProducerRecord[K, V]: Iso[ProducerRecord[K, V], ProducerRecord[K, V]] =
+  def isoIdentityProducerRecord[K, V]: Iso[ProducerRecord[K, V], ProducerRecord[K, V]] =
     Iso[ProducerRecord[K, V], ProducerRecord[K, V]](identity)(identity)
 
-  implicit def isoIdentityConsumerRecord[K, V]: Iso[ConsumerRecord[K, V], ConsumerRecord[K, V]] =
+  def isoIdentityConsumerRecord[K, V]: Iso[ConsumerRecord[K, V], ConsumerRecord[K, V]] =
     Iso[ConsumerRecord[K, V], ConsumerRecord[K, V]](identity)(identity)
 
-  implicit def isoFs2ProducerRecord[K, V]: Iso[Fs2ProducerRecord[K, V], ProducerRecord[K, V]] =
+  def isoFs2ProducerRecord[K, V]: Iso[Fs2ProducerRecord[K, V], ProducerRecord[K, V]] =
     Iso[Fs2ProducerRecord[K, V], ProducerRecord[K, V]](toProducerRecord)(fromProducerRecord)
 
-  implicit def isoFs2ComsumerRecord[K, V]: Iso[Fs2ConsumerRecord[K, V], ConsumerRecord[K, V]] =
+  def isoFs2ComsumerRecord[K, V]: Iso[Fs2ConsumerRecord[K, V], ConsumerRecord[K, V]] =
     Iso[Fs2ConsumerRecord[K, V], ConsumerRecord[K, V]](toConsumerRecord)(fromConsumerRecord)
 
   private def fromProducerRecord[K, V](pr: ProducerRecord[K, V]): Fs2ProducerRecord[K, V] =
