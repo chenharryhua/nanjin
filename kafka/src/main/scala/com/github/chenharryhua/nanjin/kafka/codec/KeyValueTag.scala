1fd9c3a13e912f5111881f02f3c1cb306d56fe5a
Harry Chen <chen.harry.hua@gmail.com>
2020-01-21 18:06:31 +1100

    serializable

--- kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/codec/KeyValueTag.scala
+++ kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/codec/KeyValueTag.scala
@@ -5,7 +5,8 @@ import enumeratum.values.{CatsOrderValueEnum, IntEnum, IntEnumEntry}
 
 import scala.collection.immutable
 
-sealed abstract class KeyValueTag(val value: Int, val name: String) extends IntEnumEntry
+sealed abstract class KeyValueTag(val value: Int, val name: String)
+    extends IntEnumEntry with Serializable
 
 object KeyValueTag extends CatsOrderValueEnum[Int, KeyValueTag] with IntEnum[KeyValueTag] {
   override val values: immutable.IndexedSeq[KeyValueTag] = findValues
