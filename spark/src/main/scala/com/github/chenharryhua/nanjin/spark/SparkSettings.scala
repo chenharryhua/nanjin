db1cf5a8158a0bdbd3850fc28b83204d29610306
Harry Chen <chen.harry.hua@gmail.com>
2020-01-23 08:39:59 +1100

    shrink coproduct in order to speed up compilation

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/SparkSettings.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/SparkSettings.scala
@@ -9,17 +9,17 @@ import org.apache.spark.sql.SparkSession
 @Lenses final case class SparkSettings(conf: SparkConf, logLevel: String) {
 
   def withKms(kmsKey: String): SparkSettings =
-    withConf(
+    withConfigUpdate(
       _.set("spark.hadoop.fs.s3a.server-side-encryption-algorithm", "SSE-KMS")
         .set("spark.hadoop.fs.s3a.server-side-encryption.key", kmsKey))
 
   def withMaster(master: String): SparkSettings =
-    withConf(_.set("spark.master", master))
+    withConfigUpdate(_.set("spark.master", master))
 
   def withLogLevel(logLevel: String): SparkSettings =
     SparkSettings.logLevel.set(logLevel)(this)
 
-  def withConf(f: SparkConf => SparkConf): SparkSettings =
+  def withConfigUpdate(f: SparkConf => SparkConf): SparkSettings =
     SparkSettings.conf.modify(f)(this)
 
   def session: SparkSession = {
@@ -38,16 +38,16 @@ import org.apache.spark.sql.SparkSession
 object SparkSettings {
 
   val default: SparkSettings =
-    SparkSettings(new SparkConf, "warn").withConf(
+    SparkSettings(new SparkConf, "warn").withConfigUpdate(
       _.set("spark.master", "local[*]")
         .set("spark.ui.enabled", "true")
+        .set("spark.network.timeout", "800")
         .set("spark.debug.maxToStringFields", "1000")
         .set(
           "spark.hadoop.fs.s3a.aws.credentials.provider",
           "com.amazonaws.auth.DefaultAWSCredentialsProviderChain")
         .set("spark.hadoop.fs.s3a.connection.maximum", "100")
-        .set("spark.network.timeout", "800")
-        .set("spark.streaming.kafka.consumer.poll.ms", "180000")
         .set("spark.hadoop.fs.s3a.experimental.input.fadvise", "sequential")
+        .set("spark.streaming.kafka.consumer.poll.ms", "180000")
         .set("spark.streaming.kafka.allowNonConsecutiveOffsets", "true"))
 }
