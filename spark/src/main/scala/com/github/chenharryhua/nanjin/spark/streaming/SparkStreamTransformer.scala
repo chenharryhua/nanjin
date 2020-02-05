469f4c766a1a9d4020f9743108c1898fa0336682
Harry Chen <chen.harry.hua@gmail.com>
2020-02-05 22:41:20 +1100

    streaming

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/streaming/SparkStreamTransformer.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/streaming/SparkStreamTransformer.scala
@@ -1,7 +1,10 @@
 package com.github.chenharryhua.nanjin.spark.streaming
 
+import com.github.chenharryhua.nanjin.kafka.KafkaTopicKit
+import com.github.chenharryhua.nanjin.kafka.common.NJProducerRecord
 import frameless.{TypedDataset, TypedEncoder}
 import org.apache.spark.sql.Dataset
+import cats.implicits._
 
 final class SparkStreamTransformer[F[_], A: TypedEncoder](ds: Dataset[A]) extends Serializable {
   @transient lazy val typedDataset: TypedDataset[A] = TypedDataset.create(ds)
@@ -16,5 +19,15 @@ final class SparkStreamTransformer[F[_], A: TypedEncoder](ds: Dataset[A]) extend
     new SparkStreamTransformer[F, B](typedDataset.deserialized.flatMap(f).dataset)
 
   def withSink(sink: StreamOutputSink) = new SparkStreamRunner[F, A](ds.writeStream, sink)
+
+  def withKafkaSink[K, V](kit: KafkaTopicKit[K, V])(implicit ev: A =:= NJProducerRecord[K, V])
+    : SparkStreamRunner[F, NJProducerRecord[Array[Byte], Array[Byte]]] =
+    new SparkStreamTransformer[F, NJProducerRecord[Array[Byte], Array[Byte]]](
+      typedDataset.deserialized
+        .map(m =>
+          ev(m).bimap(
+            k => kit.codec.keySerde.serializer.serialize(kit.topicName.value, k),
+            v => kit.codec.valueSerde.serializer.serialize(kit.topicName.value, v)))
+        .dataset).withSink(KafkaSink.withUpdateMode(kit.settings.brokers.get, kit.topicName))
 
 }
