4bdb6cb1d0609e2e9e3e134d091b886670ca3f07
Harry Chen <chen.harry.hua@gmail.com>
2020-02-13 08:03:45 +1100

    rs

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/kafka/SparKafkaParamF.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/kafka/SparKafkaParamF.scala
@@ -1,4 +1,26 @@
 package com.github.chenharryhua.nanjin.spark.kafka
 
+import cats.Functor
+import higherkindness.droste.data.Fix
+
+import scala.concurrent.duration.FiniteDuration
+
 trait SparKafkaParamF[A]
-object SparKafkaParamF {}
+
+object SparKafkaParamF {
+  final case class End() extends SparKafkaParamF[Nothing]
+  final case class WithBatchSize[K](value: Int, cont: K) extends SparKafkaParamF[K]
+  final case class WithDuration[K](value: FiniteDuration, cont: K) extends SparKafkaParamF[K]
+
+  implicit val SparKafkaParamFunctor: Functor[SparKafkaParamF] =
+    cats.derived.semi.functor[SparKafkaParamF]
+
+  type SparKafkaParam = Fix[SparKafkaParamF]
+
+  def withBatchSize(v: Int, sum: SparKafkaParam): SparKafkaParam =
+    Fix(WithBatchSize(v, sum))
+
+  def withDuration(v: FiniteDuration, sum: SparKafkaParam): SparKafkaParam =
+    Fix(WithDuration(v, sum))
+
+}
