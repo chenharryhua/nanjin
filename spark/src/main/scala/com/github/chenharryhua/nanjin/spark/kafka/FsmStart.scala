bc6bad12fa8a4a9a82a8e770382dee1d7a535651
Harry Chen <chen.harry.hua@gmail.com>
2020-02-17 08:35:55 +1100

    replay with timestamp

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/kafka/FsmStart.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/kafka/FsmStart.scala
@@ -64,6 +64,13 @@ final class FsmStart[K, V](kit: KafkaTopicKit[K, V], cfg: SKConfig)(
     keyEncoder: TypedEncoder[K],
     valEncoder: TypedEncoder[V]): F[Unit] =
     fromDisk[F].consumerRecords.flatMap(
+      _.someValues.sorted.toProducerRecords.noMeta.upload.map(_ => print(".")).compile.drain)
+
+  def replayWithTimestamp[F[_]: ConcurrentEffect: Timer: ContextShift](
+    implicit
+    keyEncoder: TypedEncoder[K],
+    valEncoder: TypedEncoder[V]): F[Unit] =
+    fromDisk[F].consumerRecords.flatMap(
       _.someValues.sorted.toProducerRecords.noPartition.upload.map(_ => print(".")).compile.drain)
 
   def batchPipeTo[F[_]: ConcurrentEffect: Timer: ContextShift](otherTopic: KafkaTopicKit[K, V])(
