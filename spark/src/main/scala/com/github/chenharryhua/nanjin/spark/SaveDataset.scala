c2586c8486809f8d73824a1cde5696b6e08cc4c9
Harry Chen <chen.harry.hua@gmail.com>
2020-02-20 23:34:10 +1100

    json and avro

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/SaveDataset.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/SaveDataset.scala
@@ -13,7 +13,9 @@ final class SaveDataset[F[_]: Concurrent, A: TypedEncoder](ds: Dataset[A])(
     extends Serializable {
   @transient lazy val typedDataset: TypedDataset[A] = TypedDataset.create(ds)
 
-  def save[B: Encoder: SchemaFor](pathStr: String)(f: A => B): Stream[F, Unit] = {
+  def saveJackson[B: Encoder: SchemaFor](pathStr: String)(f: A => B): Stream[F, Unit] = {
+    val schema: Schema = SchemaFor[B].schema(DefaultFieldMapper)
+
     val fs: Resource[F, FileSystem] =
       Resource.make(Sync[F].delay(FileSystem.get(sparkSession.sparkContext.hadoopConfiguration)))(
         a => Sync[F].delay(a.close()))
@@ -21,10 +23,24 @@ final class SaveDataset[F[_]: Concurrent, A: TypedEncoder](ds: Dataset[A])(
     val outFileStream: Resource[F, FSDataOutputStream] = fs.flatMap(fs =>
       Resource.make(Sync[F].delay(fs.create(new Path(pathStr))))(a => Sync[F].delay(a.close())))
 
-    val schema: Schema = SchemaFor[B].schema(DefaultFieldMapper)
-
     for {
       os <- Stream.resource(outFileStream).map(AvroOutputStream.json[B].to(_).build(schema))
+      data <- typedDataset.stream[F].map(f)
+    } yield os.write(data)
+  }
+
+  def saveAvro[B: Encoder: SchemaFor](pathStr: String)(f: A => B): Stream[F, Unit] = {
+    val schema: Schema = SchemaFor[B].schema(DefaultFieldMapper)
+
+    val fs: Resource[F, FileSystem] =
+      Resource.make(Sync[F].delay(FileSystem.get(sparkSession.sparkContext.hadoopConfiguration)))(
+        a => Sync[F].delay(a.close()))
+
+    val outFileStream: Resource[F, FSDataOutputStream] = fs.flatMap(fs =>
+      Resource.make(Sync[F].delay(fs.create(new Path(pathStr))))(a => Sync[F].delay(a.close())))
+
+    for {
+      os <- Stream.resource(outFileStream).map(AvroOutputStream.data[B].to(_).build(schema))
       data <- typedDataset.stream[F].map(f)
     } yield os.write(data)
   }
