bfafce76f4d1e3244fe64911935c6507c42f5b67
Harry Chen <chen.harry.hua@gmail.com>
2020-03-13 19:32:47 +1100

    tests

--- spark/src/test/scala/mtest/spark/kafka/SparkCoproductTest.scala
+++ spark/src/test/scala/mtest/spark/kafka/SparkCoproductTest.scala
@@ -1,10 +1,13 @@
 package mtest.spark.kafka
 
+import com.github.chenharryhua.nanjin.kafka.TopicName
 import org.scalatest.funsuite.AnyFunSuite
 import shapeless._
 import shapeless.test.illTyped
 import mtest.spark.kafka.sparkCoproduct._
 import frameless.TypedEncoder
+import cats.implicits._
+import com.github.chenharryhua.nanjin.spark.kafka._
 
 object sparkCoproduct {
   sealed trait Parent
@@ -14,22 +17,48 @@ object sparkCoproduct {
 
   type CoParent = Child1 :+: Child2 :+: CNil
 
-  sealed trait EnumLike
-  object Enum1 extends EnumLike
-  object Enum2 extends EnumLike
+  sealed trait Address
+  case object Addr1 extends Address
+  case object Addr2 extends Address
 
   object PhoneType extends Enumeration {
-    type PhoneType = Value
-    val F, H, M, W = Value
+    val F, Z = Value
   }
+
+  final case class Person(name: String, addr: Address)
+  val p1 = Person("zz", Addr1)
+  val p2 = Person("ff", Addr2)
+
+  final case class Person2(name: String, phoneType: PhoneType.Value)
+  val f1 = Person2("cc", PhoneType.F)
+  val f2 = Person2("ww", PhoneType.Z)
+
+  val topic  = ctx.topic[Int, Person](TopicName("coproduct.person"))
+  val topic2 = ctx.topic[Int, Person2](TopicName("coproduct.person2"))
+
 }
 
 class SparkCoproductTest extends AnyFunSuite {
+  import sparkCoproduct._
 
   test("happy to see one day it fails") {
-    illTyped("""implicitly[TypedEncoder[Parent]]""")
-    illTyped("""implicitly[TypedEncoder[CoParent]]""")
-    illTyped(""" implicitly[TypedEncoder[EnumLike]] """)
-    illTyped(""" implicitly[TypedEncoder[PhoneType.PhoneType]] """)
+    illTyped(""" implicitly[TypedEncoder[Parent]] """)
+    illTyped(""" implicitly[TypedEncoder[CoParent]] """)
+    illTyped(""" implicitly[TypedEncoder[Address]] """)
+    illTyped(""" implicitly[TypedEncoder[PhoneType.Value]] """)
+  }
+
+  test("work well with case object") {
+    val run = topic.admin.idefinitelyWantToDeleteTheTopicAndUnderstoodItsConsequence >>
+      topic.schemaRegistry.register >>
+      topic.send(1, p1) >> topic.send(2, p2) >> topic.kit.sparKafka.save
+    run.unsafeRunSync()
+  }
+
+  ignore("do not work with scala Enumeration") {
+    val run = topic2.admin.idefinitelyWantToDeleteTheTopicAndUnderstoodItsConsequence >>
+      topic2.schemaRegistry.register >>
+      topic2.send(1, f1) >> topic2.send(2, f2) >> topic2.kit.sparKafka.save
+    run.unsafeRunSync()
   }
 }
