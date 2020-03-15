4945caa1eed084e710b43ad82f349af48aeb13f5
Harry Chen <chen.harry.hua@gmail.com>
2020-03-15 17:53:38 +1100

    protocols

--- common/src/main/scala/com/github/chenharryhua/nanjin/common/Protocols.scala
+++ common/src/main/scala/com/github/chenharryhua/nanjin/common/Protocols.scala
@@ -4,37 +4,61 @@ import enumeratum.{CatsEnum, Enum, EnumEntry}
 
 import scala.collection.immutable
 
-sealed abstract class Protocols(val value: String) extends EnumEntry with Serializable {}
+sealed abstract class WebProtocols(val value: String) extends EnumEntry with Serializable
 
-object Protocols extends Enum[Protocols] with CatsEnum[Protocols] {
-  override val values: immutable.IndexedSeq[Protocols] = findValues
-  case object S3 extends Protocols("S3")
-  case object S3A extends Protocols("S3A")
-  case object SASL_SSL extends Protocols("SASL_SSL")
-  case object PLAINTEXT extends Protocols("PLAINTEXT")
-  case object SASL_PLAINTEXT extends Protocols("SASL_PLAINTEXT")
-  case object SSL extends Protocols("SSL")
-  case object HTTP extends Protocols("HTTP")
-  case object HTTPS extends Protocols("HTTPS")
-  case object MongoDB extends Protocols("mongodb")
-  case object Postgres extends Protocols("jdbc:postgresql")
-  case object Redshift extends Protocols("jdbc:redshift")
-  case object SqlServer extends Protocols("jdbc:sqlserver")
-  case object Bolt extends Protocols("bolt")
-  case object FTP extends Protocols("ftp")
+object WebProtocols extends Enum[WebProtocols] with CatsEnum[WebProtocols] {
+  override val values: immutable.IndexedSeq[WebProtocols] = findValues
 
-  type S3             = S3.type
-  type S3A            = S3A.type
+  case object HTTP extends WebProtocols("HTTP")
+  case object HTTPS extends WebProtocols("HTTPS")
+  case object FTP extends WebProtocols("ftp")
+
+  type HTTP  = HTTP.type
+  type HTTPS = HTTPS.type
+  type FTP   = FTP.type
+}
+
+sealed abstract class KafkaProtocols(val value: String) extends EnumEntry with Serializable
+
+object KafkaProtocols extends Enum[KafkaProtocols] with CatsEnum[KafkaProtocols] {
+  override val values: immutable.IndexedSeq[KafkaProtocols] = findValues
+  case object SSL extends KafkaProtocols("SSL")
+  case object SASL_SSL extends KafkaProtocols("SASL_SSL")
+  case object PLAINTEXT extends KafkaProtocols("PLAINTEXT")
+  case object SASL_PLAINTEXT extends KafkaProtocols("SASL_PLAINTEXT")
+
   type SASL_SSL       = SASL_SSL.type
   type PLAINTEXT      = PLAINTEXT.type
   type SASL_PLAINTEXT = SASL_PLAINTEXT.type
   type SSL            = SSL.type
-  type HTTP           = HTTP.type
-  type HTTPS          = HTTPS.type
-  type MongoDB        = MongoDB.type
-  type Postgres       = Postgres.type
-  type Redshift       = Redshift.type
-  type SqlServer      = SqlServer.type
-  type Bolt           = Bolt.type
-  type FTP            = FTP.type
+}
+
+sealed abstract class DatabaseProtocols(val value: String) extends EnumEntry with Serializable
+
+object DatabaseProtocols extends Enum[DatabaseProtocols] with CatsEnum[DatabaseProtocols] {
+  override val values: immutable.IndexedSeq[DatabaseProtocols] = findValues
+
+  case object MongoDB extends WebProtocols("mongodb")
+  case object Postgres extends WebProtocols("jdbc:postgresql")
+  case object Redshift extends WebProtocols("jdbc:redshift")
+  case object SqlServer extends WebProtocols("jdbc:sqlserver")
+  case object Bolt extends WebProtocols("bolt")
+
+  type MongoDB   = MongoDB.type
+  type Postgres  = Postgres.type
+  type Redshift  = Redshift.type
+  type SqlServer = SqlServer.type
+  type Bolt      = Bolt.type
+}
+
+sealed abstract class S3Protocols(val value: String) extends EnumEntry with Serializable
+
+object S3Protocols extends Enum[S3Protocols] with CatsEnum[S3Protocols] {
+  override val values: immutable.IndexedSeq[S3Protocols] = findValues
+
+  case object S3 extends WebProtocols("S3")
+  case object S3A extends WebProtocols("S3A")
+
+  type S3  = S3.type
+  type S3A = S3A.type
 }
