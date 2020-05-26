6389f234fdfd51461b1f156842c6c2f8004ff8f7
Harry Chen <chen.harry.hua@gmail.com>
2020-05-26 23:43:29 +1000

    partition apply

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/kafka/FsmRdd.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/kafka/FsmRdd.scala
@@ -14,7 +14,7 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 final class FsmRdd[F[_], K, V](
-  val rdd: RDD[NJConsumerRecord[K, V]],
+  rdd: RDD[NJConsumerRecord[K, V]],
   topic: KafkaTopic[F, K, V],
   cfg: SKConfig)(implicit sparkSession: SparkSession)
     extends SparKafkaUpdateParams[FsmRdd[F, K, V]] {
@@ -25,12 +25,32 @@ final class FsmRdd[F[_], K, V](
   override def withParamUpdate(f: SKConfig => SKConfig): FsmRdd[F, K, V] =
     new FsmRdd[F, K, V](rdd, topic, f(cfg))
 
+  def partition(num: Int): FsmRdd[F, K, V] =
+    new FsmRdd[F, K, V](rdd.filter(_.partition === num), topic, cfg)
+
   def save(implicit F: Sync[F], cs: ContextShift[F]): F[Unit] =
     Blocker[F].use { blocker =>
       val path = sk.replayPath(topic.topicName)
       hadoop.delete(path, sparkSession.sparkContext.hadoopConfiguration, blocker) >>
         F.delay(rdd.saveAsObjectFile(path))
     }
+
+  def count: Long = rdd.count()
+
+  def stats: Statistics[F] =
+    new Statistics(TypedDataset.create(rdd.map(CRMetaInfo(_))).dataset, cfg)
+
+  def sortedRDD: RDD[NJConsumerRecord[K, V]] =
+    rdd
+      .filter(m => params.timeRange.isInBetween(m.timestamp))
+      .repartition(params.repartition.value)
+      .sortBy[NJConsumerRecord[K, V]](identity)
+
+  def crStream(implicit F: Sync[F]): Stream[F, NJConsumerRecord[K, V]] =
+    sortedRDD.stream[F]
+
+  def crSource(implicit F: ConcurrentEffect[F]): Source[NJConsumerRecord[K, V], NotUsed] =
+    sortedRDD.source[F]
 
   def saveJackson(implicit F: Sync[F], cs: ContextShift[F]): F[Unit] =
     crStream.through(fileSink.jackson(sk.jacksonPath(topic.topicName))).compile.drain
@@ -55,32 +75,11 @@ final class FsmRdd[F[_], K, V](
   def replay(implicit ce: ConcurrentEffect[F], timer: Timer[F], cs: ContextShift[F]): F[Unit] =
     pipeTo(topic)
 
-  def count: Long = rdd.count()
-
-  def partition(num: Int): FsmRdd[F, K, V] =
-    new FsmRdd[F, K, V](rdd.filter(_.partition === num), topic, cfg)
-
-  def sorted: RDD[NJConsumerRecord[K, V]] =
-    rdd
-      .filter(m => params.timeRange.isInBetween(m.timestamp))
-      .repartition(params.repartition.value)
-      .sortBy[NJConsumerRecord[K, V]](identity)
-
-  def crStream(implicit F: Sync[F]): Stream[F, NJConsumerRecord[K, V]] =
-    sorted.stream[F]
-
-  def crSource(implicit F: ConcurrentEffect[F]): Source[NJConsumerRecord[K, V], NotUsed] =
-    sorted.source[F]
-
   def crDataset(implicit
     keyEncoder: TypedEncoder[K],
     valEncoder: TypedEncoder[V]): FsmConsumerRecords[F, K, V] = {
-    val tds       = TypedDataset.create(rdd)
+    val tds       = TypedDataset.create(sortedRDD)
     val inBetween = tds.makeUDF[Long, Boolean](params.timeRange.isInBetween)
     new FsmConsumerRecords(tds.filter(inBetween(tds('timestamp))).dataset, topic, cfg)
   }
-
-  def stats: Statistics[F] =
-    new Statistics(TypedDataset.create(rdd.map(CRMetaInfo(_))).dataset, cfg)
-
 }
