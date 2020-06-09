3222f224cbb402c1e5830ab1b59aa64cf3de450d
Harry Chen <chen.harry.hua@gmail.com>
2020-06-09 14:42:11 +1000

    tests

--- pipes/src/test/scala/mtest/package.scala
+++ pipes/src/test/scala/mtest/package.scala
@@ -1,11 +1,9 @@
-import cats.effect.{Blocker, ContextShift, IO, Timer}
+import cats.effect.{ContextShift, IO, Timer}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 
 package object mtest {
   implicit val cs: ContextShift[IO] = IO.contextShift(global)
   implicit val timer: Timer[IO]     = IO.timer(global)
-
-  val blocker = Blocker.liftExecutionContext(global)
 
 }
