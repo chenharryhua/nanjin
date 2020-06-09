b4869c548ddb6d9fe8ced58e598fb9b5c882c13a
Harry Chen <chen.harry.hua@gmail.com>
2020-06-09 13:39:51 +1000

    ftp test

--- devices/src/test/scala/mtest/FtpTest.scala
+++ devices/src/test/scala/mtest/FtpTest.scala
@@ -1,21 +1,27 @@
 package mtest
 
 import cats.effect.IO
-import com.github.chenharryhua.nanjin.devices.AkkaFtpUploader
+import cats.implicits._
+import com.github.chenharryhua.nanjin.devices.{AkkaFtpDownloader, AkkaFtpUploader}
 import fs2.Stream
 import org.scalatest.funsuite.AnyFunSuite
 
 class FtpTest extends AnyFunSuite {
 
-  val ftp = new AkkaFtpUploader[IO](ftpSettins)
+  val uploader   = new AkkaFtpUploader[IO](ftpSettins)
+  val downloader = new AkkaFtpDownloader[IO](ftpSettins)
 
-  val pathStr    = "test.txt"
-  val testString = "save string to ftp"
+  val pathStr    = "ftp-test.txt"
+  val testString = "send a string to ftp and read it back"
 
   val ts: Stream[IO, Byte] =
     Stream(testString).through(fs2.text.utf8Encode)
 
   test("identity ftp") {
-    println(ts.through(ftp.upload(pathStr)).compile.toList.unsafeRunSync())
+    val action = ts.through(uploader.upload(pathStr)).compile.toList >>
+      downloader.download(pathStr).through(fs2.text.utf8Decode).compile.lastOrError
+
+    assert(action.unsafeRunSync() == testString)
+
   }
 }
