b6a10714fa44d85e2b93392845d244373f6a8672
Harry Chen <chen.harry.hua@gmail.com>
2020-06-25 10:50:37 +1000

    unused param

--- spark/src/test/scala/mtest/spark/SingleFileTest.scala
+++ spark/src/test/scala/mtest/spark/SingleFileTest.scala
@@ -28,7 +28,7 @@ class SingleFileTest extends AnyFunSuite {
   def delete(path: String) = sink.delete(path)
   import sparkSession.implicits._
 
-  test("avro") {
+  test("avro - identity") {
     val path = "./data/test/spark/singleFile/swordfish.avro"
     val run = delete(path) >>
       ss.through(sink.avro[Swordfish](path)).compile.drain >>
@@ -40,7 +40,7 @@ class SingleFileTest extends AnyFunSuite {
     assert(s == fishes.toSet)
   }
 
-  test("avro-binary") {
+  test("avro-binary - identity") {
     val path = "./data/test/spark/singleFile/swordfish-binary.avro"
     val run = delete(path) >>
       ss.through(sink.binary[Swordfish](path)).compile.drain >>
@@ -49,7 +49,7 @@ class SingleFileTest extends AnyFunSuite {
     assert(run.unsafeRunSync() === fishes)
   }
 
-  test("parquet") {
+  test("parquet - identity") {
     val path = "./data/test/spark/singleFile/swordfish.parquet"
     val run = delete(path) >>
       ss.through(sink.parquet[Swordfish](path)).compile.drain >>
@@ -60,7 +60,7 @@ class SingleFileTest extends AnyFunSuite {
     val s = sparkSession.read.parquet(path).as[Swordfish].collect().toSet
     assert(s == fishes.toSet)
   }
-  test("json") {
+  test("json - identity") {
     val path = "./data/test/spark/singleFile/swordfish.json"
     val run = delete(path) >>
       ss.through(sink.json[Swordfish](path)).compile.drain >>
@@ -69,7 +69,7 @@ class SingleFileTest extends AnyFunSuite {
     assert(run.unsafeRunSync() === fishes)
 
   }
-  test("jackson") {
+  test("jackson - identity") {
     val path = "./data/test/spark/singleFile/swordfish-jackson.json"
     val run = delete(path) >>
       ss.through(sink.jackson[Swordfish](path)).compile.drain >>
@@ -79,7 +79,7 @@ class SingleFileTest extends AnyFunSuite {
 
   }
 
-  test("csv") {
+  test("csv - identity") {
     val path = "./data/test/spark/singleFile/swordfish.csv"
     val run = delete(path) >>
       ss.through(sink.csv[Swordfish](path)).compile.drain >>
@@ -88,7 +88,7 @@ class SingleFileTest extends AnyFunSuite {
     assert(run.unsafeRunSync() === fishes)
   }
 
-  test("java-object") {
+  test("java-object - identity") {
     val path = "./data/test/spark/singleFile/swordfish.obj"
     val run = delete(path) >>
       ss.through(sink.javaObject[Swordfish](path)).compile.drain >>
