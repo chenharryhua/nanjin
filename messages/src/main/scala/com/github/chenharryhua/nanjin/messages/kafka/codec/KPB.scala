e110d7981c79dd39f3470bf0400b848188bad9f6
Harry Chen <chen.harry.hua@gmail.com>
2020-07-13 11:40:12 +1000

    protobuf

--- messages/src/main/scala/com/github/chenharryhua/nanjin/messages/kafka/codec/KPB.scala
+++ messages/src/main/scala/com/github/chenharryhua/nanjin/messages/kafka/codec/KPB.scala
@@ -2,20 +2,21 @@ package com.github.chenharryhua.nanjin.messages.kafka.codec
 
 import java.util
 
-import com.google.protobuf.Message
+import com.google.protobuf.DynamicMessage
 import com.sksamuel.avro4s.{SchemaFor, Decoder => AvroDecoder, Encoder => AvroEncoder}
 import io.confluent.kafka.serializers.protobuf.{KafkaProtobufDeserializer, KafkaProtobufSerializer}
+import scalapb.{GeneratedMessage, GeneratedMessageCompanion}
 
 // kafka protobuf
 final case class KPB[A](value: A)
 
 object KPB {
 
-  implicit def kpbSerializer[A <: Message]: KafkaAvroSerializer[KPB[A]] =
+  implicit def kpbSerializer[A](implicit ev: A <:< GeneratedMessage): KafkaAvroSerializer[KPB[A]] =
     new KafkaAvroSerializer[KPB[A]] {
 
-      @transient private[this] lazy val ser: KafkaProtobufSerializer[A] =
-        new KafkaProtobufSerializer[A]()
+      @transient private[this] lazy val ser: KafkaProtobufSerializer[DynamicMessage] =
+        new KafkaProtobufSerializer[DynamicMessage]()
 
       override def configure(configs: util.Map[String, _], isKey: Boolean): Unit =
         ser.configure(configs, isKey)
@@ -30,33 +31,32 @@ object KPB {
 
       override def serialize(topic: String, data: KPB[A]): Array[Byte] =
         Option(data).flatMap(v => Option(v.value)) match {
-          case None    => null.asInstanceOf[Array[Byte]]
-          case Some(a) => ser.serialize(topic, a)
+          case None => null.asInstanceOf[Array[Byte]]
+          case Some(a) =>
+            val dm = DynamicMessage.parseFrom(a.companion.javaDescriptor, a.toByteArray)
+            ser.serialize(topic, dm)
         }
     }
 
-  implicit def kpbDeserializer[A <: Message]: KafkaAvroDeserializer[KPB[A]] =
+  implicit def kpbDeserializer[A <: GeneratedMessage](implicit
+    ev: GeneratedMessageCompanion[A]): KafkaAvroDeserializer[KPB[A]] =
     new KafkaAvroDeserializer[KPB[A]] {
-
-      @transient private[this] lazy val deSer: KafkaProtobufDeserializer[A] =
-        new KafkaProtobufDeserializer[A]()
-
-      override def configure(configs: util.Map[String, _], isKey: Boolean): Unit =
-        deSer.configure(configs, isKey)
-
-      override def close(): Unit = deSer.close()
 
       override val avroDecoder: AvroDecoder[KPB[A]] = new AvroDecoder[KPB[A]] {
 
         override val schemaFor: SchemaFor[KPB[A]] = SchemaFor[Array[Byte]].forType[KPB[A]]
 
-        override def decode(value: Any): KPB[A] = sys.error("fix me")
+        override def decode(value: Any): KPB[A] =
+          value match {
+            case ab: Array[Byte] => KPB(ev.parseFrom(ab))
+            case ex              => sys.error(s"not a Array[Byte] ${ex.toString}")
+          }
       }
 
       override def deserialize(topic: String, data: Array[Byte]): KPB[A] =
         Option(data) match {
           case None    => null.asInstanceOf[KPB[A]]
-          case Some(v) => KPB(deSer.deserialize(topic, v))
+          case Some(v) => KPB(ev.parseFrom(data))
         }
     }
 }
