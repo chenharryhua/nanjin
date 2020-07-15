f539e6d8452cfc2c2d58d50121f2e719854c3534
Harry Chen <chen.harry.hua@gmail.com>
2020-07-15 15:45:41 +1000

    ignore

--- kafka/src/test/scala/mtest/kafka/ProtobufTest.scala
+++ kafka/src/test/scala/mtest/kafka/ProtobufTest.scala
@@ -5,17 +5,15 @@ import cats.implicits._
 import com.github.chenharryhua.nanjin.kafka.KafkaTopic
 import com.github.chenharryhua.nanjin.messages.kafka.codec.KPB
 import mtest.pb.test._
-import org.scalatest.Ignore
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.util.Random
 
-@Ignore
 class ProtobufTest extends AnyFunSuite {
 
   val topic: KafkaTopic[IO, String, KPB[Person]] =
     ctx.topic[String, KPB[Person]]("producer.test.protobuf")
-  test("producer protobuf messsages") {
+  ignore("producer protobuf messsages") {
 
     val produceTask = (0 until 100).toList.traverse { i =>
       topic.send(i.toString, KPB(Person(s"$i", Random.nextInt(100))))
