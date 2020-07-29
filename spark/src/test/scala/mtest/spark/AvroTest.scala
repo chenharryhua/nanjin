a8c798b762f466c7496cc041136c7c129ea52780
Harry Chen <chen.harry.hua@gmail.com>
2020-07-29 12:35:14 +1000

    avro test

---
+++ spark/src/test/scala/mtest/spark/AvroTest.scala
@@ -0,0 +1,69 @@
+package mtest.spark
+
+import cats.effect.IO
+import cats.implicits._
+import com.github.chenharryhua.nanjin.messages.kafka.codec.WithAvroSchema
+import com.github.chenharryhua.nanjin.spark._
+import fs2.Stream
+import org.scalatest.funsuite.AnyFunSuite
+
+import scala.util.Random
+
+object AvroTestData {
+
+  val schema =
+    """
+      |{
+      |  "type": "record",
+      |  "name": "HasByteArray",
+      |  "namespace": "mtest.spark.AvroTestData",
+      |  "fields": [
+      |    {
+      |      "name": "name",
+      |      "type": "string"
+      |    },
+      |    {
+      |      "name": "bytes",
+      |      "type": "bytes"
+      |    }
+      |  ]
+      |}       
+    """.stripMargin
+
+  final case class HasByteArray(name: String, bytes: Array[Byte])
+  val serde = WithAvroSchema[HasByteArray](schema)
+
+  val data = List.fill(10)(HasByteArray("a", Array[Byte](1, 2, 3, Random.nextInt().toByte)))
+
+}
+
+class AvroTest extends AnyFunSuite {
+  import AvroTestData._
+
+  test("provide schema to avoid auto-derived schema - bin") {
+    val path = "./data/test/spark/avro/bin.avro"
+
+    implicit val enc = serde.right.get.avroEncoder
+    implicit val dec = serde.right.get.avroDecoder
+
+    val run =
+      fileSink[IO](blocker).delete(path) >>
+        Stream.emits(data).through(fileSink[IO](blocker).binAvro[HasByteArray](path)).compile.drain
+    run.unsafeRunSync()
+    val rst = fileSource[IO](blocker).binAvro[HasByteArray](path).compile.toList
+    assert(rst.unsafeRunSync().zip(data).forall { case (a, b) => a.bytes.deep === b.bytes.deep })
+  }
+  test("provide schema to avoid auto-derived schema - data") {
+    val path = "./data/test/spark/avro/bin.avro"
+
+    implicit val enc = serde.right.get.avroEncoder
+    implicit val dec = serde.right.get.avroDecoder
+
+    val run =
+      fileSink[IO](blocker).delete(path) >>
+        Stream.emits(data).through(fileSink[IO](blocker).avro[HasByteArray](path)).compile.drain
+    run.unsafeRunSync()
+    val rst = fileSource[IO](blocker).avro[HasByteArray](path).compile.toList
+    assert(rst.unsafeRunSync().zip(data).forall { case (a, b) => a.bytes.deep === b.bytes.deep })
+  }
+}
