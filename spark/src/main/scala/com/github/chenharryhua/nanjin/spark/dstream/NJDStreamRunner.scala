9d65338a0558ee9483c7dde09e4695aac4d0ab5d
Harry Chen <chen.harry.hua@gmail.com>
2020-08-05 23:09:27 +1000

    dstream avro

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/NJDStreamRunner.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/NJDStreamRunner.scala
@@ -1,3 +1,13 @@
 package com.github.chenharryhua.nanjin.spark.dstream
 
-class NJDStreamRunner {}
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.streaming.{Duration, StreamingContext}
+
+final class NJDStreamRunner(sparkSession: SparkSession, checkpoint: String, duration: Duration) {
+  val ctx = new StreamingContext(sparkSession.sparkContext, duration)
+  ctx.checkpoint(checkpoint)
+
+  ctx.start()
+  ctx.stop(false, true)
+  ctx.awaitTermination()
+}
