c0217697260f3e1e54f896841701f1980f6b37a7
Harry Chen <chen.harry.hua@gmail.com>
2020-08-22 21:45:42 +1000

    gen schema

--- spark/src/test/scala/com/github/chenharryhua/nanjin/spark/NJDataTypeCompoundTypeTest.scala
+++ spark/src/test/scala/com/github/chenharryhua/nanjin/spark/NJDataTypeCompoundTypeTest.scala
@@ -1,21 +1,41 @@
 package com.github.chenharryhua.nanjin.spark
 
+import avrohugger.Generator
+import avrohugger.format.Standard
+import avrohugger.types.{AvroScalaTypes, ScalaArray}
 import frameless.TypedEncoder
-import org.apache.spark.sql.avro.SchemaConverters
 import org.scalatest.funsuite.AnyFunSuite
 
 object NJDataTypeCompoundTypeTestData {
-  final case class Food(f: Int, g: String)
+  final case class Food(f: Int, g: Array[Byte])
   final case class FishFood(d: String, e: Option[Food])
   final case class DogFish(a: Array[Int], b: Array[String], c: Array[FishFood])
   val dogfish: TypedEncoder[DogFish] = shapeless.cachedImplicit
+
+  // map key must be string.
+  //https://docs.oracle.com/database/nosql-12.1.3.0/GettingStartedGuide/avroschemas.html#avro-complexdatatypes
+  final case class Squalidae(a: Int, b: Map[String, Int], c: Map[String, List[FishFood]])
+  val squalidae: TypedEncoder[Squalidae] = shapeless.cachedImplicit
+
 }
 
 class NJDataTypeCompoundTypeTest extends AnyFunSuite {
   import NJDataTypeCompoundTypeTestData._
-  test("compound type") {
-    println(
-      SchemaConverters.toAvroType(dogfish.catalystRepr, true, "spark", "sparkns").toString(true))
-    println(NJDataTypeF.genSchema(dogfish.catalystRepr))
+
+  val generator: Generator =
+    Generator(Standard, avroScalaCustomTypes = Some(AvroScalaTypes(array = ScalaArray)))
+
+  test("array type") {
+    val s = NJDataTypeF.genSchema(dogfish.catalystRepr)
+    println(dogfish.catalystRepr)
+    println(s)
+    println(generator.schemaToStrings(s))
+  }
+
+  test("map type") {
+    val s = NJDataTypeF.genSchema(squalidae.catalystRepr)
+    println(squalidae.catalystRepr)
+    println(s)
+    println(generator.schemaToStrings(s))
   }
 }
