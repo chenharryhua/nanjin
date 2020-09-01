6f199eb53ec64c59da721850dd69c6c5dc817721
Harry Chen <chen.harry.hua@gmail.com>
2020-09-02 09:14:57 +1000

    protobuf

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/RddFileHoader.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/RddFileHoader.scala
@@ -1,12 +1,13 @@
 package com.github.chenharryhua.nanjin.spark.persist
 
 import cats.Show
-import com.github.chenharryhua.nanjin.common.NJFileFormat
+import com.github.chenharryhua.nanjin.common.NJFileFormat._
 import com.github.chenharryhua.nanjin.messages.kafka.codec.NJAvroCodec
 import io.circe.{Encoder => JsonEncoder}
 import kantan.csv.{CsvConfiguration, RowEncoder}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
+import scalapb.GeneratedMessage
 
 import scala.reflect.ClassTag
 
@@ -22,33 +23,43 @@ final class RddFileHoader[F[_], A: ClassTag](
   def errorIfExists: RddFileHoader[F, A]  = updateConfig(cfg.withError)
   def ignoreIfExists: RddFileHoader[F, A] = updateConfig(cfg.withIgnore)
 
-  def avro(outPath: String): SaveAvro[F, A] =
-    new SaveAvro[F, A](rdd, cfg.withFormat(NJFileFormat.Avro).withOutPutPath(outPath))
-
-  def parquet(outPath: String): SaveParquet[F, A] =
-    new SaveParquet[F, A](rdd, cfg.withFormat(NJFileFormat.Parquet).withOutPutPath(outPath))
-
+// 1
   def jackson(outPath: String): SaveJackson[F, A] =
-    new SaveJackson[F, A](rdd, cfg.withFormat(NJFileFormat.Jackson).withOutPutPath(outPath))
+    new SaveJackson[F, A](rdd, cfg.withFormat(Jackson).withOutPutPath(outPath))
 
-  def binAvro(outPath: String): SaveBinaryAvro[F, A] =
-    new SaveBinaryAvro[F, A](rdd, cfg.withFormat(NJFileFormat.BinaryAvro).withOutPutPath(outPath))
-
+// 2
   def circe(outPath: String)(implicit ev: JsonEncoder[A]): SaveCirce[F, A] =
-    new SaveCirce[F, A](rdd, cfg.withFormat(NJFileFormat.Circe).withOutPutPath(outPath))
+    new SaveCirce[F, A](rdd, cfg.withFormat(Circe).withOutPutPath(outPath))
 
-  def json(outPath: String) =
-    new SaveSparkJson[F, A](rdd, cfg.withFormat(NJFileFormat.SparkJson).withOutPutPath(outPath))
-
-  def csv(outPath: String)(implicit ev: RowEncoder[A]): SaveCsv[F, A] =
-    new SaveCsv[F, A](
-      rdd,
-      CsvConfiguration.rfc,
-      cfg.withFormat(NJFileFormat.Csv).withOutPutPath(outPath))
-
+// 3
   def text(outPath: String)(implicit ev: Show[A]): SaveText[F, A] =
-    new SaveText[F, A](rdd, cfg.withFormat(NJFileFormat.Text).withOutPutPath(outPath))
+    new SaveText[F, A](rdd, cfg.withFormat(Text).withOutPutPath(outPath))
 
+// 4
+  def csv(outPath: String)(implicit ev: RowEncoder[A]): SaveCsv[F, A] =
+    new SaveCsv[F, A](rdd, CsvConfiguration.rfc, cfg.withFormat(Csv).withOutPutPath(outPath))
+
+// 5
+  def json(outPath: String) =
+    new SaveSparkJson[F, A](rdd, cfg.withFormat(SparkJson).withOutPutPath(outPath))
+
+// 11
+  def parquet(outPath: String): SaveParquet[F, A] =
+    new SaveParquet[F, A](rdd, cfg.withFormat(Parquet).withOutPutPath(outPath))
+
+// 12
+  def avro(outPath: String): SaveAvro[F, A] =
+    new SaveAvro[F, A](rdd, cfg.withFormat(Avro).withOutPutPath(outPath))
+
+// 13
+  def binAvro(outPath: String): SaveBinaryAvro[F, A] =
+    new SaveBinaryAvro[F, A](rdd, cfg.withFormat(BinaryAvro).withOutPutPath(outPath))
+
+// 14
   def objectFile(outPath: String): SaveObjectFile[F, A] =
-    new SaveObjectFile[F, A](rdd, cfg.withFormat(NJFileFormat.JavaObject).withOutPutPath(outPath))
+    new SaveObjectFile[F, A](rdd, cfg.withFormat(JavaObject).withOutPutPath(outPath))
+
+// 15
+  def protobuf(outPath: String)(implicit ev: A <:< GeneratedMessage): SaveProtobuf[F, A] =
+    new SaveProtobuf[F, A](rdd, cfg.withFormat(ProtoBuf).withOutPutPath(outPath))
 }
