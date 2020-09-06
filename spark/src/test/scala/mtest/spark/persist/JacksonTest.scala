e1ce3a22fc31bad8f12fc1e2acf38b7161416a0d
Harry Chen <chen.harry.hua@gmail.com>
2020-09-06 23:26:45 +1000

    test

--- spark/src/test/scala/mtest/spark/persist/JacksonTest.scala
+++ spark/src/test/scala/mtest/spark/persist/JacksonTest.scala
@@ -7,7 +7,7 @@ import org.scalatest.funsuite.AnyFunSuite
 
 @DoNotDiscover
 class JacksonTest extends AnyFunSuite {
-  test("rdd read/write identity multi") {
+  test("datetime read/write identity multi") {
     import RoosterData._
     val path = "./data/test/spark/persist/jackson/multi.json"
     delete(path)
@@ -16,7 +16,7 @@ class JacksonTest extends AnyFunSuite {
     val r = loaders.raw.jackson[Rooster](path)
     assert(expected == r.collect().toSet)
   }
-  test("rdd read/write identity single") {
+  test("datetime read/write identity single") {
     import RoosterData._
     val path = "./data/test/spark/persist/jackson/single.json"
     delete(path)
@@ -24,5 +24,17 @@ class JacksonTest extends AnyFunSuite {
     saver.jackson(path).file.run(blocker).unsafeRunSync()
     val r = loaders.raw.jackson[Rooster](path)
     assert(expected == r.collect().toSet)
+  }
+
+  test("byte-array read/write identity single read") {
+    import BeeData._
+    import cats.implicits._
+    val path = "./data/test/spark/persist/jackson/bee/single.raw.avro"
+    delete(path)
+    val saver = new RddFileHoarder[IO, Bee](rdd).repartition(1)
+    saver.jackson(path).folder.run(blocker).unsafeRunSync()
+    val t = loaders.raw.jackson[Bee](path).collect().toList
+    println(t)
+    assert(bees.sortBy(_.b).zip(t.sortBy(_.b)).forall { case (a, b) => a.eqv(b) })
   }
 }
