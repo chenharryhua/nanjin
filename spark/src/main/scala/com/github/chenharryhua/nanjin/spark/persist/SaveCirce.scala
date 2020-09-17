06f04947573b848b80d3478f4e2cc82720688728
Harry Chen <chen.harry.hua@gmail.com>
2020-09-18 00:03:37 +1000

    compress circe

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveCirce.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveCirce.scala
@@ -3,9 +3,13 @@ package com.github.chenharryhua.nanjin.spark.persist
 import cats.effect.{Blocker, Concurrent, ContextShift}
 import cats.{Eq, Parallel}
 import com.github.chenharryhua.nanjin.common.NJFileFormat
+import com.github.chenharryhua.nanjin.devices.NJHadoop
 import com.github.chenharryhua.nanjin.messages.kafka.codec.AvroCodec
+import com.github.chenharryhua.nanjin.pipes.CirceSerialization
 import com.github.chenharryhua.nanjin.spark.RddExt
+import fs2.Pipe
 import io.circe.{Encoder => JsonEncoder}
+import org.apache.hadoop.io.compress.{CompressionCodec, DeflateCodec, GzipCodec}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -22,21 +26,49 @@ final class SaveCirce[F[_], A: ClassTag](rdd: RDD[A], codec: AvroCodec[A], cfg: 
   def file: SaveCirce[F, A]   = updateConfig(cfg.withSingleFile)
   def folder: SaveCirce[F, A] = updateConfig(cfg.withFolder)
 
+  def gzip: SaveCirce[F, A] = updateConfig(cfg.withCompression(Compression.Gzip))
+
+  def deflate(level: Int): SaveCirce[F, A] =
+    updateConfig(cfg.withCompression(Compression.Deflate(level)))
+
   def run(
     blocker: Blocker)(implicit F: Concurrent[F], cs: ContextShift[F], ss: SparkSession): F[Unit] = {
     val sma: SaveModeAware[F] = new SaveModeAware[F](params.saveMode, params.outPath, ss)
+
+    val single: Pipe[F, Byte, Byte] = params.compression match {
+      case Compression.Uncompressed   => identity
+      case Compression.Gzip           => fs2.compression.gzip()
+      case Compression.Deflate(level) => fs2.compression.deflate(level)
+      case c                          => throw new Exception(s"not support $c")
+    }
+
+    val multi: Class[_ <: CompressionCodec] = params.compression match {
+      case Compression.Uncompressed   => null
+      case Compression.Gzip           => classOf[GzipCodec]
+      case Compression.Deflate(level) => classOf[DeflateCodec]
+      case c                          => throw new Exception(s"not support $c")
+    }
+
     params.folderOrFile match {
       case FolderOrFile.SingleFile =>
+        val hadoop =
+          new NJHadoop[F](ss.sparkContext.hadoopConfiguration, blocker).byteSink(params.outPath)
+        val pipe = new CirceSerialization[F, A](jsonEncoder)
         sma.checkAndRun(blocker)(
           rdd
             .map(codec.idConversion)
             .stream[F]
-            .through(fileSink[F](blocker).circe(params.outPath))
+            .through(pipe.serialize)
+            .through(single)
+            .through(hadoop)
             .compile
             .drain)
       case FolderOrFile.Folder =>
-        sma.checkAndRun(blocker)(F.delay(
-          rdd.map(a => jsonEncoder(codec.idConversion(a)).noSpaces).saveAsTextFile(params.outPath)))
+        sma.checkAndRun(blocker)(
+          F.delay(
+            rdd
+              .map(a => jsonEncoder(codec.idConversion(a)).noSpaces)
+              .saveAsTextFile(params.outPath, multi)))
     }
   }
 }
@@ -50,6 +82,17 @@ final class PartitionCirce[F[_], A: ClassTag, K: ClassTag: Eq](
     extends AbstractPartition[F, A, K] {
 
   val params: HoarderParams = cfg.evalConfig
+
+  private def updateConfig(cfg: HoarderConfig): PartitionCirce[F, A, K] =
+    new PartitionCirce[F, A, K](rdd, codec, cfg, bucketing, pathBuilder)
+
+  def file: PartitionCirce[F, A, K]   = updateConfig(cfg.withSingleFile)
+  def folder: PartitionCirce[F, A, K] = updateConfig(cfg.withFolder)
+
+  def gzip: PartitionCirce[F, A, K] = updateConfig(cfg.withCompression(Compression.Gzip))
+
+  def deflate(level: Int): PartitionCirce[F, A, K] =
+    updateConfig(cfg.withCompression(Compression.Deflate(level)))
 
   def run(blocker: Blocker)(implicit
     F: Concurrent[F],
