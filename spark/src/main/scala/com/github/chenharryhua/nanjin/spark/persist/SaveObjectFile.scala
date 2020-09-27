49f49e9e21a122ea5d516a3f8723c46217630060
Harry Chen <chen.harry.hua@gmail.com>
2020-09-27 18:57:26 +1000

    checkAndRun

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveObjectFile.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveObjectFile.scala
@@ -2,6 +2,7 @@ package com.github.chenharryhua.nanjin.spark.persist
 
 import cats.effect.{Blocker, Concurrent, ContextShift}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
 
 import scala.reflect.ClassTag
 
@@ -9,7 +10,13 @@ final class SaveObjectFile[F[_], A](rdd: RDD[A], cfg: HoarderConfig) extends Ser
 
   val params: HoarderParams = cfg.evalConfig
 
-  def run(
-    blocker: Blocker)(implicit F: Concurrent[F], cs: ContextShift[F], tag: ClassTag[A]): F[Unit] =
-    F.delay(rdd.saveAsObjectFile(params.outPath))
+  def run(blocker: Blocker)(implicit
+    F: Concurrent[F],
+    cs: ContextShift[F],
+    tag: ClassTag[A],
+    ss: SparkSession): F[Unit] = {
+    val sma: SaveModeAware[F] = new SaveModeAware[F](params.saveMode, params.outPath, ss)
+
+    sma.checkAndRun(blocker)(F.delay(rdd.saveAsObjectFile(params.outPath)))
+  }
 }
