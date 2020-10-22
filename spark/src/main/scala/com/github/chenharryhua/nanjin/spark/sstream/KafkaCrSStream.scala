da9526937b6d4ef08771e4ed81e738495176be62
Harry Chen <chen.harry.hua@gmail.com>
2020-10-22 13:50:14 +1100

    map/flatMap

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/sstream/KafkaCrSStream.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/sstream/KafkaCrSStream.scala
@@ -2,8 +2,8 @@ package com.github.chenharryhua.nanjin.spark.sstream
 
 import com.github.chenharryhua.nanjin.spark.DatePartitionedCR
 import com.github.chenharryhua.nanjin.spark.kafka.OptionalKV
-import frameless.{TypedDataset, TypedEncoder}
-import org.apache.spark.sql.Dataset
+import frameless.{TypedDataset, TypedEncoder, TypedExpressionEncoder}
+import org.apache.spark.sql.{Dataset, Encoder}
 
 final class KafkaCrSStream[F[_], K: TypedEncoder, V: TypedEncoder](
   ds: Dataset[OptionalKV[K, V]],
@@ -13,9 +13,27 @@ final class KafkaCrSStream[F[_], K: TypedEncoder, V: TypedEncoder](
   override def withParamUpdate(f: SStreamConfig => SStreamConfig): KafkaCrSStream[F, K, V] =
     new KafkaCrSStream[F, K, V](ds, f(cfg))
 
+  implicit private val te: TypedEncoder[OptionalKV[K, V]] = shapeless.cachedImplicit
+
   @transient lazy val typedDataset: TypedDataset[OptionalKV[K, V]] = TypedDataset.create(ds)
 
   override val params: SStreamParams = cfg.evalConfig
+
+  def map[K1, V1](f: OptionalKV[K, V] => OptionalKV[K1, V1])(implicit
+    k1: TypedEncoder[K1],
+    v1: TypedEncoder[V1]): KafkaCrSStream[F, K1, V1] = {
+    implicit val te: TypedEncoder[OptionalKV[K1, V1]] = shapeless.cachedImplicit
+    implicit val encoder: Encoder[OptionalKV[K1, V1]] = TypedExpressionEncoder[OptionalKV[K1, V1]]
+    new KafkaCrSStream[F, K1, V1](ds.map(f), cfg)
+  }
+
+  def flatMap[K1, V1](f: OptionalKV[K, V] => TraversableOnce[OptionalKV[K1, V1]])(implicit
+    k1: TypedEncoder[K1],
+    v1: TypedEncoder[V1]): KafkaCrSStream[F, K1, V1] = {
+    implicit val te: TypedEncoder[OptionalKV[K1, V1]] = shapeless.cachedImplicit
+    implicit val encoder: Encoder[OptionalKV[K1, V1]] = TypedExpressionEncoder[OptionalKV[K1, V1]]
+    new KafkaCrSStream[F, K1, V1](ds.flatMap(f), cfg)
+  }
 
   def someValues: KafkaCrSStream[F, K, V] =
     new KafkaCrSStream[F, K, V](typedDataset.filter(typedDataset('value).isNotNone).dataset, cfg)
