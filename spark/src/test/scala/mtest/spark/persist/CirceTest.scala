8b273fdae2ff84c179ecd92d72d1c8600c14c7a7
Harry Chen <chen.harry.hua@gmail.com>
2020-11-13 00:30:45 +1100

    circe test

--- spark/src/test/scala/mtest/spark/persist/CirceTest.scala
+++ spark/src/test/scala/mtest/spark/persist/CirceTest.scala
@@ -11,15 +11,6 @@ import org.scalatest.funsuite.AnyFunSuite
 @DoNotDiscover
 class CirceTest extends AnyFunSuite {
 
-  test("rdd read/write identity multi.uncompressed") {
-    import RoosterData._
-    val path  = "./data/test/spark/persist/circe/rooster/multi.uncompressed"
-    val saver = new RddFileHoarder[IO, Rooster](rdd, Rooster.avroCodec)
-    saver.circe(path).folder.run(blocker).unsafeRunSync()
-    val t = loaders.rdd.circe[Rooster](path)
-    assert(expected == t.collect().toSet)
-  }
-
   test("rdd read/write identity multi.gzip") {
     import RoosterData._
     val path  = "./data/test/spark/persist/circe/rooster/multi.gzip"
@@ -76,8 +67,44 @@ class CirceTest extends AnyFunSuite {
     import BeeData._
     val path  = "./data/test/spark/persist/circe/bee/single.json"
     val saver = new RddFileHoarder[IO, Bee](rdd, Bee.codec).repartition(1)
-    saver.circe(path).file.dropNull.run(blocker).unsafeRunSync()
+    saver.circe(path).file.run(blocker).unsafeRunSync()
     val t = loaders.rdd.circe[Bee](path)
     assert(t.collect.map(_.toWasp).toSet === bees.map(_.toWasp).toSet)
   }
+
+  test("rdd read/write identity multi.uncompressed - keep null") {
+    import RoosterData._
+    val path  = "./data/test/spark/persist/circe/rooster/multi.keepNull.uncompressed"
+    val saver = new RddFileHoarder[IO, Rooster](rdd, Rooster.avroCodec)
+    saver.repartition(1).circe(path).folder.keepNull.run(blocker).unsafeRunSync()
+    val t = loaders.rdd.circe[Rooster](path)
+    assert(expected == t.collect().toSet)
+  }
+
+  test("rdd read/write identity multi.uncompressed - drop null") {
+    import RoosterData._
+    val path  = "./data/test/spark/persist/circe/rooster/multi.dropNull.uncompressed"
+    val saver = new RddFileHoarder[IO, Rooster](rdd, Rooster.avroCodec)
+    saver.repartition(1).circe(path).folder.dropNull.run(blocker).unsafeRunSync()
+    val t = loaders.rdd.circe[Rooster](path)
+    assert(expected == t.collect().toSet)
+  }
+  test("rdd read/write identity single.uncompressed - keep null") {
+    import RoosterData._
+    val path  = "./data/test/spark/persist/circe/rooster/single.keepNull.uncompressed.json"
+    val saver = new RddFileHoarder[IO, Rooster](rdd, Rooster.avroCodec)
+    saver.repartition(1).circe(path).file.keepNull.run(blocker).unsafeRunSync()
+    val t = loaders.rdd.circe[Rooster](path)
+    assert(expected == t.collect().toSet)
+  }
+
+  test("rdd read/write identity single.uncompressed - drop null") {
+    import RoosterData._
+    val path  = "./data/test/spark/persist/circe/rooster/single.dropNull.uncompressed.json"
+    val saver = new RddFileHoarder[IO, Rooster](rdd, Rooster.avroCodec)
+    saver.repartition(1).circe(path).file.dropNull.run(blocker).unsafeRunSync()
+    val t = loaders.rdd.circe[Rooster](path)
+    assert(expected == t.collect().toSet)
+  }
+
 }
