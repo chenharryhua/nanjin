32d151d42560cae1a8ce23c6b50e7c63ddc20adc
Harry Chen <chen.harry.hua@gmail.com>
2020-11-17 23:43:52 +1100

    withNamespace

---
+++ messages/src/test/scala/mtest/codec/SchemaChangeTest.scala
@@ -0,0 +1,76 @@
+package mtest.codec
+
+import cats.data.Ior
+import com.github.chenharryhua.nanjin.messages.kafka.codec.AvroCodec
+import org.apache.avro.Schema
+import org.apache.avro.generic.GenericData
+import org.scalatest.funsuite.AnyFunSuite
+import shapeless.{:+:, CNil, Coproduct}
+
+object SchemaChangeTestData {
+  final case class Nest(a: Int)
+  final case class Nest2(b: String)
+  final case class UnderTest(a: Int, b: Nest :+: Nest2 :+: CNil)
+
+  val schema =
+    """
+{
+  "type": "record",
+  "name": "UnderTest",
+  "namespace": "to.be.changed",
+  "fields": [
+    {
+      "name": "a",
+      "type": "int"
+    },
+    {
+      "name": "b",
+      "type": [
+        {
+          "type": "record",
+          "name": "Nest",
+          "fields": [
+            {
+              "name": "a",
+              "type": "int"
+            }
+          ]
+        },
+        {
+          "type": "record",
+          "name": "Nest2",
+          "fields": [
+            {
+              "name": "b",
+              "type": "string"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+}
+        """
+
+  val oldSchema: Schema           = (new Schema.Parser).parse(schema)
+  val codec: AvroCodec[UnderTest] = AvroCodec[UnderTest](schema).right.get
+}
+
+class SchemaChangeTest extends AnyFunSuite {
+  import SchemaChangeTestData._
+  test("change namespace") {
+    val newCodec: AvroCodec[UnderTest] = codec.withNamespace("mtest.avro.SchemaChangeTestData")
+
+    val data = UnderTest(1, Coproduct(Nest(1)))
+    val en   = newCodec.avroEncoder.encode(data)
+    val res  = newCodec.avroDecoder.decode(en)
+    assert(res == data)
+  }
+
+  test("namespace different should throw exception") {
+    val newCodec: AvroCodec[UnderTest] = codec.withNamespace("mtest.avro.SchemaChangeTestData")
+    val data                           = UnderTest(1, Coproduct(Nest(1)))
+    val en                             = codec.avroEncoder.encode(data)
+    assertThrows[Exception](newCodec.avroDecoder.decode(en))
+  }
+}
