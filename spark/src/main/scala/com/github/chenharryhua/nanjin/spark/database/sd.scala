25aff523b5c97138763170b87653c96a1ad47f6b
Harry Chen <chen.harry.hua@gmail.com>
2020-11-19 20:25:24 +1100

    db

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/database/sd.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/database/sd.scala
@@ -1,17 +1,19 @@
 package com.github.chenharryhua.nanjin.spark.database
 
 import com.github.chenharryhua.nanjin.database.{ConnectionString, DriverString, TableName}
+import com.zaxxer.hikari.HikariConfig
 import frameless.{TypedDataset, TypedEncoder}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
 private[spark] object sd {
 
-  def unloadDF(
-    connStr: ConnectionString,
-    driver: DriverString,
-    tableName: TableName,
-    query: Option[String])(implicit sparkSession: SparkSession): DataFrame = {
-    val mandatory = Map("url" -> connStr.value, "driver" -> driver.value)
+  def unloadDF(cfg: HikariConfig, tableName: TableName, query: Option[String])(implicit
+    sparkSession: SparkSession): DataFrame = {
+    val mandatory = Map(
+      "url" -> cfg.getJdbcUrl,
+      "driver" -> cfg.getDriverClassName,
+      "user" -> cfg.getUsername,
+      "password" -> cfg.getPassword)
     //https://spark.apache.org/docs/latest/sql-data-sources-jdbc.html
     val sparkOptions: Map[String, String] =
       mandatory + query.fold("dbtable" -> tableName.value)(q => "query" -> q)
