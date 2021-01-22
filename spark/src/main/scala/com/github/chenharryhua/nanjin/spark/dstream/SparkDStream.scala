4b29e0a45105c55313db7029a4c9bedeffcf2bfd
Harry Chen <chen.harry.hua@gmail.com>
2021-01-22 21:16:55 +1100

    dstream

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/SparkDStream.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/SparkDStream.scala
@@ -10,20 +10,20 @@ import scala.reflect.ClassTag
 
 sealed class SparkDStream[A](ds: DStream[A]) extends Serializable {
 
-  protected def pathBuilder(path: String)(ts: NJTimestamp): String =
-    if (path.endsWith("/"))
-      s"$path${ts.`Year=yyyy/Month=mm/Day=dd`(sydneyTime)}"
-    else
-      s"$path/${ts.`Year=yyyy/Month=mm/Day=dd`(sydneyTime)}"
-
   def transform[B: ClassTag](f: RDD[A] => RDD[B]): SparkDStream[B] = new SparkDStream[B](ds.transform(f))
 
-  def circe(path: String)(implicit enc: Encoder[A]): Unit =
-    persist.circe[A](ds)(pathBuilder(path))
+  def circe(path: String)(implicit enc: Encoder[A]): Unit = persist.circe[A](ds)(pathBuilder(path))
 }
 
-final class SparkAvroDStream[A](ds: DStream[A], encoder: AvroEncoder[A]) extends SparkDStream[A](ds) {
+final class SparkAvroDStream[A](ds: DStream[A], encoder: AvroEncoder[A]) extends Serializable {
 
-  def avro(path: String): Unit    = persist.avro(ds, encoder)(pathBuilder(path))
-  def jackson(path: String): Unit = persist.jackson(ds, encoder)(pathBuilder(path))
+  def transform[B: ClassTag](f: RDD[A] => RDD[B], encoder: AvroEncoder[B]): SparkAvroDStream[B] =
+    new SparkAvroDStream[B](ds.transform(f), encoder)
+
+  def transform(f: RDD[A] => RDD[A])(implicit ev: ClassTag[A]): SparkAvroDStream[A] =
+    new SparkAvroDStream[A](ds.transform(f), encoder)
+
+  def circe(path: String)(implicit enc: Encoder[A]): Unit = persist.circe[A](ds)(pathBuilder(path))
+  def avro(path: String): Unit                            = persist.avro(ds, encoder)(pathBuilder(path))
+  def jackson(path: String): Unit                         = persist.jackson(ds, encoder)(pathBuilder(path))
 }
