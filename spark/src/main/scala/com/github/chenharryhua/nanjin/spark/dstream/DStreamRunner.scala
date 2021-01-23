ae9cb9626c0aa1ec44943d960a2b6f05eaf3cb09
Harry Chen <chen.harry.hua@gmail.com>
2021-01-23 18:02:20 +1100

    dstream

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/DStreamRunner.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/DStreamRunner.scala
@@ -1,8 +1,14 @@
 package com.github.chenharryhua.nanjin.spark.dstream
 
 import cats.data.Reader
-import cats.effect.{Blocker, Sync}
-import org.apache.spark.streaming.StreamingContext
+import cats.effect.Sync
+import fs2.Stream
+import monocle.Getter
+import org.apache.spark.SparkContext
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+import scala.concurrent.duration.FiniteDuration
 
 final class EndMark private {}
 
@@ -17,13 +23,22 @@ final class DStreamRunner[F[_]] private (sc: StreamingContext) extends Serializa
     this
   }
 
-  def run(blocker: Blocker)(implicit F: Sync[F]): F[Unit] =
-    F.delay {
+  def dstream[A, B](rd: Reader[StreamingContext, A])(implicit getter: Getter[A, DStream[B]]): DStream[B] =
+    getter.get(rd.run(sc))
+
+  def run(implicit F: Sync[F]): Stream[F, Unit] =
+    Stream.bracket(F.delay {
       sc.start()
       sc.awaitTermination()
-    }
+    })(_ => F.delay(sc.stop(stopSparkContext = false, stopGracefully = true)))
 }
 
 object DStreamRunner {
   def apply[F[_]](sc: StreamingContext) = new DStreamRunner[F](sc)
+
+  def apply[F[_]](sparkContext: SparkContext, checkpoint: String, batchDuration: FiniteDuration): DStreamRunner[F] = {
+    val sc = new StreamingContext(sparkContext, Seconds(batchDuration.toSeconds))
+    sc.checkpoint(checkpoint)
+    apply[F](sc)
+  }
 }
--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/SparkDStream.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/SparkDStream.scala
@@ -2,30 +2,43 @@ package com.github.chenharryhua.nanjin.spark.dstream
 
 import com.github.chenharryhua.nanjin.datetime.{sydneyTime, NJTimestamp}
 import com.sksamuel.avro4s.{Encoder => AvroEncoder}
-import io.circe.Encoder
+import io.circe.{Encoder => JsonEncoder}
+import monocle.Getter
 import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.DStream
 
 import scala.reflect.ClassTag
 
-final class SparkDStream[A](ds: DStream[A], cfg: SDConfig) extends Serializable {
+final class SparkDStream[A](val dstream: DStream[A], cfg: SDConfig) extends Serializable {
   val params: SDParams = cfg.evalConfig
 
-  def transform[B: ClassTag](f: RDD[A] => RDD[B]): SparkDStream[B] = new SparkDStream[B](ds.transform(f), cfg)
+  def transform[B: ClassTag](f: RDD[A] => RDD[B]): SparkDStream[B] = new SparkDStream[B](dstream.transform(f), cfg)
 
-  def circe(path: String)(implicit enc: Encoder[A]): EndMark = persist.circe[A](ds)(params.pathBuilder(path))
+  def circe(path: String)(implicit enc: JsonEncoder[A]): EndMark = persist.circe[A](dstream)(params.pathBuilder(path))
 }
 
-final class SparkAvroDStream[A](ds: DStream[A], encoder: AvroEncoder[A], cfg: SDConfig) extends Serializable {
+object SparkDStream {
+
+  implicit def getterSparkDStream[A]: Getter[SparkDStream[A], DStream[A]] =
+    (s: SparkDStream[A]) => s.dstream
+}
+
+final class SparkAvroDStream[A](val dstream: DStream[A], encoder: AvroEncoder[A], cfg: SDConfig) extends Serializable {
   val params: SDParams = cfg.evalConfig
 
   def transform[B: ClassTag](f: RDD[A] => RDD[B], encoder: AvroEncoder[B]): SparkAvroDStream[B] =
-    new SparkAvroDStream[B](ds.transform(f), encoder, cfg)
+    new SparkAvroDStream[B](dstream.transform(f), encoder, cfg)
 
   def transform(f: RDD[A] => RDD[A])(implicit ev: ClassTag[A]): SparkAvroDStream[A] =
-    new SparkAvroDStream[A](ds.transform(f), encoder, cfg)
+    new SparkAvroDStream[A](dstream.transform(f), encoder, cfg)
 
-  def circe(path: String)(implicit enc: Encoder[A]): EndMark = persist.circe[A](ds)(params.pathBuilder(path))
-  def avro(path: String): EndMark                            = persist.avro(ds, encoder)(params.pathBuilder(path))
-  def jackson(path: String): EndMark                         = persist.jackson(ds, encoder)(params.pathBuilder(path))
+  def circe(path: String)(implicit enc: JsonEncoder[A]): EndMark = persist.circe[A](dstream)(params.pathBuilder(path))
+  def avro(path: String): EndMark                                = persist.avro(dstream, encoder)(params.pathBuilder(path))
+  def jackson(path: String): EndMark                             = persist.jackson(dstream, encoder)(params.pathBuilder(path))
+}
+
+object SparkAvroDStream {
+
+  implicit def getterSparkAvroDStream[A]: Getter[SparkAvroDStream[A], DStream[A]] =
+    (s: SparkAvroDStream[A]) => s.dstream
 }
