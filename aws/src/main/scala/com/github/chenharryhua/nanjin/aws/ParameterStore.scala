38682e866dde3b12a51784661970023180c406cd
Harry Chen <chen.harry.hua@gmail.com>
2021-05-16 16:30:35 +1000

    parameter store

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/ParameterStore.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/ParameterStore.scala
@@ -1,3 +1,36 @@
 package com.github.chenharryhua.nanjin.aws
 
-class ParameterStore {}
+import cats.effect.Sync
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.simplesystemsmanagement.model.GetParametersRequest
+import com.amazonaws.services.simplesystemsmanagement.{
+  AWSSimpleSystemsManagement,
+  AWSSimpleSystemsManagementClientBuilder
+}
+
+import java.util.Base64
+final case class ParameterStorePath(value: String, isSecure: Boolean = true)
+
+final case class ParameterStoreContent(value: String) {
+  val base64: Array[Byte] = Base64.getDecoder.decode(value.getBytes)
+}
+
+trait ParameterStore[F[_]] {
+  def fetch(path: ParameterStorePath): F[ParameterStoreContent]
+}
+
+object ParameterStore {
+
+  def apply[F[_]](regions: Regions = Regions.AP_SOUTHEAST_2)(implicit F: Sync[F]): ParameterStore[F] =
+    new ParameterStore[F] {
+
+      private lazy val ssmClient: AWSSimpleSystemsManagement =
+        AWSSimpleSystemsManagementClientBuilder.standard.withRegion(regions).build
+
+      override def fetch(path: ParameterStorePath): F[ParameterStoreContent] = {
+        val req =
+          new GetParametersRequest().withNames(path.value).withWithDecryption(path.isSecure)
+        F.blocking(ParameterStoreContent(ssmClient.getParameters(req).getParameters.get(0).getValue))
+      }
+    }
+}
