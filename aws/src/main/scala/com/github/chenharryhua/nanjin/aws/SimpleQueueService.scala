8504fe91507bd2710d7869f5087d9365895e2807
Harry Chen <chen.harry.hua@gmail.com>
2021-05-20 22:24:41 +1000

    sqs s3 parser

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SimpleQueueService.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SimpleQueueService.scala
@@ -11,7 +11,6 @@ import com.github.chenharryhua.nanjin.common.aws.{S3Path, SqsUrl}
 import com.github.matsluni.akkahttpspi.AkkaHttpClient
 import fs2.Stream
 import fs2.interop.reactivestreams._
-import io.circe.Error
 import io.circe.optics.JsonPath._
 import io.circe.parser._
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
@@ -23,9 +22,7 @@ sealed trait SimpleQueueService[F[_]] {
   def fetchRecords(sqs: SqsUrl)(implicit F: Async[F]): Stream[F, SqsAckResult]
 
   final def fetchS3(sqs: SqsUrl)(implicit F: Async[F]): Stream[F, S3Path] =
-    fetchRecords(sqs).flatMap { sar =>
-      Stream.fromEither(sqs_s3_parser(sar.messageAction.message.body())).flatMap(Stream.emits)
-    }
+    fetchRecords(sqs).flatMap(sar => Stream.emits(sqs_s3_parser(sar.messageAction.message.body())))
 }
 
 object SimpleQueueService {
@@ -58,12 +55,12 @@ private object sqs_s3_parser {
   /** [[https://docs.aws.amazon.com/AmazonS3/latest/userguide/notification-content-structure.html]]
     * ignore messages which does not have s3 structure
     */
-  def apply(body: String): Either[Error, List[S3Path]] =
-    parse(body).map { json =>
+  def apply(body: String): List[S3Path] =
+    parse(body).toOption.map { json =>
       root.Records.each.s3.json.getAll(json).flatMap { js =>
         val bucket = js.hcursor.downField("bucket").get[String]("name")
         val key    = js.hcursor.downField("object").get[String]("key")
         (bucket, key).mapN((b, k) => S3Path(b, URLDecoder.decode(k, "UTF-8"))).toOption
       }
-    }
+    }.sequence.flatten
 }
