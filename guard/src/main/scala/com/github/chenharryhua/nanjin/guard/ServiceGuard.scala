2ed4b7c6639a0c4c3f5edd7d51f7ae233376e6be
Harry Chen <chen.harry.hua@gmail.com>
2021-05-22 19:46:10 +1000

    ServiceGuard

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/ServiceGuard.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/ServiceGuard.scala
@@ -13,6 +13,37 @@ final class ServiceGuard[F[_]](alertServices: List[AlertService[F]], config: Ser
   def updateConfig(f: ServiceConfig => ServiceConfig): ServiceGuard[F] =
     new ServiceGuard[F](alertServices, f(config))
 
+  private def healthCheck(implicit F: Async[F]): F[Unit] =
+    alertServices
+      .traverse(
+        _.alert(
+          ServiceHealthCheck(
+            applicationName = params.applicationName,
+            serviceName = params.serviceName,
+            healthCheckInterval = params.healthCheckInterval)))
+      .delayBy(params.healthCheckInterval)
+      .void
+      .foreverM[Unit]
+
+  private def start(implicit F: Async[F]): F[Unit] =
+    alertServices
+      .traverse(_.alert(ServiceStarted(applicationName = params.applicationName, serviceName = params.serviceName)))
+      .void
+      .delayBy(params.retryPolicy.value)
+
+  private def abnormalStop(implicit F: Async[F]): F[Unit] =
+    alertServices
+      .traverse(
+        _.alert(
+          ServiceAbnormalStop(
+            applicationName = params.applicationName,
+            serviceName = params.serviceName,
+            error = new Exception(s"service(${params.serviceName}) was abnormally stopped.")
+          )))
+      .delayBy(params.retryPolicy.value)
+      .void
+      .foreverM[Unit]
+
   def run[A](action: F[A])(implicit F: Async[F]): F[Unit] = {
 
     def onError(err: Throwable, details: RetryDetails): F[Unit] =
@@ -38,42 +69,8 @@ final class ServiceGuard[F[_]](alertServices: List[AlertService[F]], config: Ser
             .void
       }
 
-    val healthChecking: F[Nothing] =
-      alertServices
-        .traverse(
-          _.alert(
-            ServiceHealthCheck(
-              applicationName = params.applicationName,
-              serviceName = params.serviceName,
-              healthCheckInterval = params.healthCheckInterval)))
-        .delayBy(params.healthCheckInterval)
-        .void
-        .foreverM[Nothing]
-
-    val startService: F[Unit] =
-      alertServices
-        .traverse(_.alert(ServiceStarted(applicationName = params.applicationName, serviceName = params.serviceName)))
-        .void
-        .delayBy(params.retryPolicy.value)
-
-    val enrich: F[Unit] = for {
-      fiber <- (startService <* healthChecking).start
-      _ <- action.onCancel(fiber.cancel).onError { case _ => fiber.cancel }
-      _ <- fiber.cancel
-      _ <- alertServices
-        .traverse(
-          _.alert(
-            ServiceAbnormalStop(
-              applicationName = params.applicationName,
-              serviceName = params.serviceName,
-              error = new Exception(s"service(${params.serviceName}) was abnormally stopped.")
-            )))
-        .delayBy(params.retryPolicy.value)
-        .void
-        .foreverM[Unit]
-    } yield ()
-
-    retry.retryingOnAllErrors(params.retryPolicy.policy[F], onError)(enrich)
+    retry.retryingOnAllErrors(params.retryPolicy.policy[F], onError)(
+      F.bracket((start *> healthCheck).start)(_ => action)(_.cancel).flatMap(_ => abnormalStop))
   }
 
   def run[A](stream: Stream[F, A])(implicit F: Async[F]): Stream[F, Unit] =
