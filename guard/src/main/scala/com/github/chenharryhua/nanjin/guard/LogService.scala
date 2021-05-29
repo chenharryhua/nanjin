dac31adda19bf5c7a95cbf0dfbe76e07603e1d07
Harry Chen <chen.harry.hua@gmail.com>
2021-05-29 12:49:32 +1000

    log

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/LogService.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/LogService.scala
@@ -24,7 +24,10 @@ final private class LogService[F[_]](implicit F: Sync[F]) extends AlertService[F
       case ss @ ServicePanic(_, _, error)   => F.blocking(logger.warn(error)(ss.show))
       case ss @ ActionRetrying(_, _, error) => F.blocking(logger.warn(error)(ss.show))
 
-      case ss: ServiceStopped                => F.blocking(logger.error(ss.show))
+      case ss @ ServiceStopped(info) =>
+        if (info.isNormalStop) F.blocking(logger.info(ss.show))
+        else F.blocking(logger.error(new Exception("service was abnormally stopped"))(ss.show))
+
       case ss @ ActionFailed(_, _, _, error) => F.blocking(logger.error(error)(ss.show))
     }
 }
