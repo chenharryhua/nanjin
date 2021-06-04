01c9702f5e3a71218284fa6f9584daf43f168d90
Harry Chen <chen.harry.hua@gmail.com>
2021-06-04 22:15:43 +1000

    redundant metrics

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/alert/MetricsService.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/alert/MetricsService.scala
@@ -20,17 +20,15 @@ final private class MetricsService[F[_]](metrics: MetricRegistry)(implicit F: Sy
     case ActionRetrying(actionInfo, _, _) =>
       F.blocking(metrics.counter(s"${actionInfo.metricsKey}.counter.retry").inc())
     case ActionFailed(actionInfo, _, endAt, _, _) =>
-      F.blocking(metrics.counter(s"${actionInfo.metricsKey}.counter.fail").inc()) >>
-        F.blocking(
-          metrics
-            .timer(s"${actionInfo.metricsKey}.timer.fail")
-            .update(JavaDuration.between(actionInfo.launchTime, endAt)))
+      F.blocking(
+        metrics
+          .timer(s"${actionInfo.metricsKey}.timer.fail")
+          .update(JavaDuration.between(actionInfo.launchTime, endAt)))
     case ActionSucced(actionInfo, endAt, _, _) =>
-      F.blocking(metrics.counter(s"${actionInfo.metricsKey}.counter.succ").inc()) >>
-        F.blocking(
-          metrics
-            .timer(s"${actionInfo.metricsKey}.timer.succ")
-            .update(JavaDuration.between(actionInfo.launchTime, endAt)))
+      F.blocking(
+        metrics
+          .timer(s"${actionInfo.metricsKey}.timer.succ")
+          .update(JavaDuration.between(actionInfo.launchTime, endAt)))
     case _: ForYouInformation =>
       F.blocking(metrics.counter("fyi").inc())
   }
