d2d83762f1b99f59e56014c2016c6bbf80e8957e
Harry Chen <chen.harry.hua@gmail.com>
2021-06-13 14:16:48 +1000

    Merge branch '0.12' of github.com:chenharryhua/nanjin into 0.12

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/alert/SlackService.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/alert/SlackService.scala
@@ -47,14 +47,14 @@ final private class SlackService[F[_]](service: SimpleNotificationService[F], fm
     case ServicePanic(at, info, params, details, error) =>
       val upcomingDelay: String = details.upcomingDelay.map(fmt.format) match {
         case None     => "should never see this" // never happen
-        case Some(ts) => s"Next attempt will happen in *$ts* meanwhile the service is *dysfunctional*."
+        case Some(ts) => s"Next attempt will take place in *$ts* meanwhile the service is *dysfunctional*."
       }
       val msg =
         SlackNotification(
           params.taskParams.appName,
           s""":system_restore: The service experienced a panic and started to recover itself.
              |$upcomingDelay 
-             |Full exception can be found in log file by *Error ID*: ${error.id}""".stripMargin,
+             |Full exception can be found in log file by *${error.id}*""".stripMargin,
           List(
             Attachment(
               "danger",
@@ -62,10 +62,10 @@ final private class SlackService[F[_]](service: SimpleNotificationService[F], fm
               List(
                 SlackField("Service", params.serviceName, short = true),
                 SlackField("Host", info.hostName, short = true),
-                SlackField("Up Time", fmt.format(info.launchTime, at), short = true),
                 SlackField("Status", "Restarting", short = true),
-                SlackField("Retry Policy", params.retryPolicy.policy[F].show, short = true),
+                SlackField("Up Time", fmt.format(info.launchTime, at), short = true),
                 SlackField("Restart so far", details.retriesSoFar.toString, short = true),
+                SlackField("Retry Policy", params.retryPolicy.policy[F].show, short = true),
                 SlackField("Cumulative Delay", fmt.format(details.cumulativeDelay), short = true),
                 SlackField("Cause", StringUtils.abbreviate(error.message, params.maxCauseSize), short = false)
               )
