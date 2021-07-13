57a7729af83bc7b6ccae8599190aa401f3e74ad8
Harry Chen <chen.harry.hua@gmail.com>
2021-07-13 23:25:56 +1000

    auth

--- http/src/main/scala/com/github/chenharryhua/nanjin/http/aep/auth/Adobe.scala
+++ http/src/main/scala/com/github/chenharryhua/nanjin/http/aep/auth/Adobe.scala
@@ -1,5 +1,7 @@
 package com.github.chenharryhua.nanjin.http.aep.auth
-import cats.effect.{Async, Concurrent}
+import cats.effect.syntax.all.*
+import cats.effect.{Async, Concurrent, Ref, Resource}
+import cats.syntax.all.*
 import com.github.chenharryhua.nanjin.http.auth.privateKey
 import io.circe.generic.JsonCodec
 import io.jsonwebtoken.{Jwts, SignatureAlgorithm}
@@ -7,13 +9,13 @@ import org.http4s.Method.*
 import org.http4s.circe.CirceEntityCodec.circeEntityDecoder
 import org.http4s.client.Client
 import org.http4s.client.dsl.Http4sClientDsl
-import org.http4s.client.middleware.Retry
-import org.http4s.{Header, Request, Response, Uri}
+import org.http4s.{Header, Headers, Uri}
 import org.typelevel.ci.CIString
 
 import java.io.File
 import java.lang.Boolean.TRUE
 import java.security.interfaces.RSAPrivateKey
+import java.util.concurrent.TimeUnit
 import scala.collection.JavaConverters.*
 import scala.concurrent.duration.*
 
@@ -52,33 +54,46 @@ object AdobeTokenType {
     private val JWT_AUD_KEY: String            = "aud"
     private val JWT_SUB_KEY: String            = "sub"
     private val JWT_TOKEN_EXPIRATION_THRESHOLD = 86400L // 24 hours
+    // https://www.adobe.io/authentication/auth-methods.html#!AdobeDocs/adobeio-auth/master/JWT/JWT.md
+    def login(client: Client[F])(implicit F: Async[F]): Resource[F, Client[F]] = {
+      val jwtToken = F.realTimeInstant.map { ts =>
+        val claims: Map[String, AnyRef] = Map(
+          JWT_ISS_KEY -> ims_org_id,
+          JWT_SUB_KEY -> technical_account_key,
+          JWT_EXPIRY_KEY -> new java.lang.Long(ts.getEpochSecond + JWT_TOKEN_EXPIRATION_THRESHOLD),
+          JWT_AUD_KEY -> s"$auth_endpoint/c/$client_id",
+          s"$auth_endpoint/s/ent_dataservices_sdk" -> TRUE
+        )
+        val pk: RSAPrivateKey = private_key.fold(privateKey.pkcs8, privateKey.pkcs8)
+        Jwts.builder.setClaims(claims.asJava).signWith(pk, SignatureAlgorithm.RS256).compact
+      }.flatMap(jwt =>
+        client.expect[AdobeTokenResponse](
+          POST(
+            Uri
+              .unsafeFromString(s"$auth_endpoint/ims/exchange/jwt")
+              .withQueryParam("client_id", client_id)
+              .withQueryParam("client_secret", client_secret)
+              .withQueryParam("jwt_token", jwt)).putHeaders(
+            Header.Raw(CIString("Content-Type"), "application/x-www-form-urlencoded"),
+            Header.Raw(CIString("Cache-Control"), "no-cache"))))
 
-    def login(client: Client[F])(implicit F: Async[F]): F[AdobeTokenResponse] = {
-      val claims: Map[String, AnyRef] = Map(
-        JWT_ISS_KEY -> ims_org_id,
-        JWT_SUB_KEY -> technical_account_key,
-        JWT_EXPIRY_KEY -> new java.lang.Long(System.currentTimeMillis() / 1000 + JWT_TOKEN_EXPIRATION_THRESHOLD),
-        JWT_AUD_KEY -> s"$auth_endpoint/c/$client_id",
-        s"$auth_endpoint/s/ent_dataservices_sdk" -> TRUE
-      )
-      val pk: RSAPrivateKey = private_key.fold(privateKey.pkcs8, privateKey.pkcs8)
-      val jwtToken          = Jwts.builder.setClaims(claims.asJava).signWith(pk, SignatureAlgorithm.RS256).compact
+      Resource.eval(for {
+        ref <- jwtToken.flatMap(Ref.of(_))
+        _ <- ref.get
+          .flatMap(t => jwtToken.delayBy(FiniteDuration(t.expires_in / 2, TimeUnit.SECONDS)).flatMap(ref.set))
+          .foreverM[Unit]
+          .start
+      } yield Client[F] { req =>
+        val auth_req = ref.get.map(t =>
+          req.putHeaders(
+            Headers(
+              Header.Raw(CIString("Authorization"), s"${t.token_type} ${t.access_token}"),
+              Header.Raw(CIString("x-gw-ims-org-id"), ims_org_id),
+              Header.Raw(CIString("x-api-key"), client_id)
+            )))
 
-      // https://www.adobe.io/authentication/auth-methods.html#!AdobeDocs/adobeio-auth/master/JWT/JWT.md
-      val req = POST(
-        Uri
-          .unsafeFromString(s"$auth_endpoint/ims/exchange/jwt")
-          .withQueryParam("client_id", client_id)
-          .withQueryParam("client_secret", client_secret)
-          .withQueryParam("jwt_token", jwtToken)).putHeaders(
-        Header.Raw(CIString("Content-Type"), "application/x-www-form-urlencoded"),
-        Header.Raw(CIString("Cache-Control"), "no-cache")
-      )
-      def policy(req: Request[F], rep: Either[Throwable, Response[F]], n: Int): Option[FiniteDuration] =
-        if (n < 10) Some(10.seconds) else None
-
-      Retry(policy)(client)
-      client.expect[AdobeTokenResponse](req)
+        Resource.eval(auth_req).flatMap(client.run)
+      })
     }
   }
 }
