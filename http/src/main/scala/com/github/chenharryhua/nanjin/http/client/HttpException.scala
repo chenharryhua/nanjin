3c720f0529e8c00a1ef802f5529f07bbda2f4a81
Harry Chen <chen.harry.hua@gmail.com>
2021-07-14 23:40:19 +1000

    purge

--- http/src/main/scala/com/github/chenharryhua/nanjin/http/client/HttpException.scala
+++
@@ -1,5 +0,0 @@
-package com.github.chenharryhua.nanjin.http.client
-
-sealed trait HttpException
-final case class RetryableException() extends HttpException
-final case class UnretryableException() extends HttpException
