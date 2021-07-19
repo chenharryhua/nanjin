117568e1bcc33b22f2d1ae9b79e7348590ce9ed3
Harry Chen <chen.harry.hua@gmail.com>
2021-07-19 18:59:15 +1000

    bayeux

--- http/src/main/scala/com/github/chenharryhua/nanjin/http/longpoll/BayeuxFsm.scala
+++ http/src/main/scala/com/github/chenharryhua/nanjin/http/longpoll/BayeuxFsm.scala
@@ -1,19 +1,42 @@
 package com.github.chenharryhua.nanjin.http.longpoll
 
 import cats.effect.Async
-import cats.effect.std.Dispatcher
-import fs2.concurrent.Channel
-import org.cometd.bayeux.Message
-import org.cometd.bayeux.client.ClientSession
+import org.cometd.bayeux.client.ClientSession.MessageListener
+import org.cometd.bayeux.client.ClientSessionChannel
+import org.cometd.bayeux.{Channel, Message}
 import org.cometd.client.BayeuxClient
-import cats.effect.syntax.all._
-import cats.syntax.all._
 
-class BayeuxFsm[F[_]](client: BayeuxClient, bus: Channel[F, Message])(implicit F: Async[F]) {
-  def handshak: F[Message] =
+class BayeuxFsm[F[_]](client: BayeuxClient)(implicit F: Async[F]) {
+
+  private def listenTo(channel: String): F[Message] =
     F.async_ { (callback: Either[Throwable, Message] => Unit) =>
-      client.handshake((message: Message) =>
-        if (message.isSuccessful) callback(Right(message))
-        else callback(Left(BayeuxException.HandshakeException(message))))
+      client
+        .getChannel(channel)
+        .addListener(new ClientSessionChannel.MessageListener {
+          override def onMessage(channel: ClientSessionChannel, message: Message): Unit =
+            if (message.isSuccessful) callback(Right(message))
+            else callback(Left(BayeuxException.ListenerException(message)))
+        })
     }
+
+  def handshake: F[Message] = F.async_ { (callback: Either[Throwable, Message] => Unit) =>
+    client.handshake((message: Message) =>
+      if (message.isSuccessful) callback(Right(message))
+      else callback(Left(BayeuxException.HandshakeException(message))))
+  }
+
+  def subscribe(channel: String): F[Message] = F.async_ { (callback: Either[Throwable, Message] => Unit) =>
+    client
+      .getChannel(channel)
+      .subscribe((channel: ClientSessionChannel, message: Message) =>
+        if (message.isSuccessful) callback(Right(message))
+        else callback(Left(BayeuxException.SubscribeException(message))))
+        ()
+  }
+  def subscribe: F[Message] = subscribe(Channel.META_SUBSCRIBE)
+
+  def unsubscribe: F[Message] = listenTo(Channel.META_UNSUBSCRIBE)
+  def connect: F[Message]     = listenTo(Channel.META_CONNECT)
+  def disconnect: F[Message]  = listenTo(Channel.META_DISCONNECT)
+
 }
