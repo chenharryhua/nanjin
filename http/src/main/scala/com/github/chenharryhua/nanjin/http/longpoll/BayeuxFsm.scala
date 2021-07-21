6f8ecb4f280831622548f8a16764bb3965838ad2
Harry Chen <chen.harry.hua@gmail.com>
2021-07-22 00:20:31 +1000

    subscribe

--- http/src/main/scala/com/github/chenharryhua/nanjin/http/longpoll/BayeuxFsm.scala
+++ http/src/main/scala/com/github/chenharryhua/nanjin/http/longpoll/BayeuxFsm.scala
@@ -1,6 +1,9 @@
 package com.github.chenharryhua.nanjin.http.longpoll
 
 import cats.effect.Async
+import cats.effect.std.Dispatcher
+import cats.syntax.all.*
+import fs2.Stream
 import org.cometd.bayeux.client.ClientSessionChannel
 import org.cometd.bayeux.{Channel, Message}
 import org.cometd.client.BayeuxClient
@@ -17,7 +20,20 @@ object BayeuxState {
           else callback(Left(BayeuxException.HandshakeException(message))))
       }
   }
-  final case class Handshaked[F[_]](client: BayeuxClient) extends BayeuxState[F]
+  final case class Handshaked[F[_]](client: BayeuxClient) extends BayeuxState[F] {
+    def subscribe(channel: String)(implicit F: Async[F]): Stream[F, Message] =
+      for {
+        dispatcher <- Stream.resource(Dispatcher[F])
+        msg <- Stream.eval(fs2.concurrent.Channel.unbounded[F, Message]).flatMap { bus =>
+          client
+            .getChannel(channel)
+            .subscribe((channel: ClientSessionChannel, message: Message) =>
+              dispatcher.unsafeRunSync(bus.send(message).void))
+          bus.stream
+        }
+      } yield msg
+  }
+
   final case class Connected[F[_]](client: BayeuxClient) extends BayeuxState[F]
   final case class Subscribed[F[_]](client: BayeuxClient) extends BayeuxState[F]
   final case class DataExchanging[F[_]](client: BayeuxClient) extends BayeuxState[F]
@@ -37,12 +53,6 @@ class BayeuxFsm[F[_]](client: BayeuxClient)(implicit F: Async[F]) {
             else callback(Left(BayeuxException.ListenerException(message)))
         })
     }
-
-  def handshake: F[Message] = F.async_ { (callback: Either[Throwable, Message] => Unit) =>
-    client.handshake((message: Message) =>
-      if (message.isSuccessful) callback(Right(message))
-      else callback(Left(BayeuxException.HandshakeException(message))))
-  }
 
   def subscribe(channel: String): F[Message] = F.async_ { (callback: Either[Throwable, Message] => Unit) =>
     client
