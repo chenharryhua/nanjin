727bd2236d3bf83498b78f4d75e9607e0e5262b5
Harry Chen <chen.harry.hua@gmail.com>
2021-07-27 23:58:28 +1000

    long poll

--- http/src/main/scala/com/github/chenharryhua/nanjin/http/client/longpoll/LongpollTransport.scala
+++ http/src/main/scala/com/github/chenharryhua/nanjin/http/client/longpoll/LongpollTransport.scala
@@ -2,7 +2,7 @@ package com.github.chenharryhua.nanjin.http.client.longpoll
 
 import cats.effect.Async
 import cats.effect.std.Dispatcher
-import fs2.concurrent.Channel
+import cats.syntax.all.*
 import io.circe.Json
 import io.circe.parser.parse
 import org.cometd.bayeux.Message
@@ -19,18 +19,26 @@ import java.util
 import java.util.concurrent.Executors
 import scala.collection.JavaConverters.*
 
-class LongpollTransport[F[_]: Async](url: String, client: Client[F], dispatcher: Dispatcher[F])
+class LongpollTransport[F[_]](url: String, client: Client[F], dispatcher: Dispatcher[F])(implicit F: Async[F])
     extends AbstractHttpClientTransport(url, new util.HashMap[String, AnyRef](), Executors.newScheduledThreadPool(1))
     with Http4sClientDsl[F] {
+
   private val jackson = new JacksonJSONContextClient
+
   override def send(listener: TransportListener, messages: util.List[Message.Mutable]): Unit =
     messages.asScala.foreach { msg =>
-      parse(jackson.generate(msg)) match {
-        case Left(value) => listener.onFailure(value, List(msg).asJava)
-        case Right(value) =>
-          val req  = POST(value, Uri.unsafeFromString(url + msg.getChannel))
-          val resp = dispatcher.unsafeRunSync(client.expect[Json](req)).noSpaces
-          listener.onMessages(jackson.parse(resp).toList.asJava)
-      }
+      val run: F[Unit] = F
+        .fromEither(parse(jackson.generate(msg)))
+        .flatMap { reqJson =>
+          val req = POST(reqJson, Uri.unsafeFromString(url + msg.getChannel))
+          client.expect[Json](req)
+        }
+        .map(respJson => jackson.parse(respJson.noSpaces).toList)
+        .attempt
+        .map {
+          case Left(ex)  => listener.onFailure(ex, List(msg).asJava)
+          case Right(ls) => listener.onMessages(ls.asJava)
+        }
+      dispatcher.unsafeRunSync(run)
     }
 }
