a454556d58a55b64d374273b85afb7e20cff55f3
Harry Chen <chen.harry.hua@gmail.com>
2021-08-03 11:47:18 +1000

    kafka

--- kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/streaming/StreamingChannel.scala
+++ kafka/src/main/scala/com/github/chenharryhua/nanjin/kafka/streaming/StreamingChannel.scala
@@ -4,37 +4,33 @@ import cats.data.Reader
 import com.github.chenharryhua.nanjin.kafka.TopicDef
 import org.apache.kafka.streams.kstream.GlobalKTable
 import org.apache.kafka.streams.scala.ByteArrayKeyValueStore
-import org.apache.kafka.streams.scala.kstream.Materialized
+import org.apache.kafka.streams.scala.kstream.{Consumed, Materialized}
 import org.apache.kafka.streams.state.KeyValueBytesStoreSupplier
 
 final class StreamingChannel[K, V] private[kafka] (topicDef: TopicDef[K, V]) {
   import org.apache.kafka.streams.scala.StreamsBuilder
   import org.apache.kafka.streams.scala.kstream.{KStream, KTable}
 
+  private val consumed: Consumed[K, V] = Consumed.`with`(topicDef.serdeOfKey, topicDef.serdeOfVal)
+
   val kstream: Reader[StreamsBuilder, KStream[K, V]] =
-    Reader(builder => builder.stream[K, V](topicDef.topicName.value)(topicDef.consumed))
+    Reader(builder => builder.stream[K, V](topicDef.topicName.value)(consumed))
 
   val ktable: Reader[StreamsBuilder, KTable[K, V]] =
-    Reader(builder => builder.table[K, V](topicDef.topicName.value)(topicDef.consumed))
+    Reader(builder => builder.table[K, V](topicDef.topicName.value)(consumed))
 
   def ktable(mat: Materialized[K, V, ByteArrayKeyValueStore]): Reader[StreamsBuilder, KTable[K, V]] =
-    Reader(builder => builder.table[K, V](topicDef.topicName.value, mat)(topicDef.consumed))
+    Reader(builder => builder.table[K, V](topicDef.topicName.value, mat)(consumed))
 
-  def ktable(supplier: KeyValueBytesStoreSupplier): Reader[StreamsBuilder, KTable[K, V]] = {
-    val mat: Materialized[K, V, ByteArrayKeyValueStore] =
-      Materialized.as[K, V](supplier)(topicDef.serdeOfKey, topicDef.serdeOfVal)
-    Reader((builder: StreamsBuilder) => builder.table[K, V](topicDef.topicName.value, mat)(topicDef.consumed))
-  }
+  def ktable(supplier: KeyValueBytesStoreSupplier): Reader[StreamsBuilder, KTable[K, V]] =
+    ktable(Materialized.as[K, V](supplier)(topicDef.serdeOfKey, topicDef.serdeOfVal))
 
   val gktable: Reader[StreamsBuilder, GlobalKTable[K, V]] =
-    Reader(builder => builder.globalTable[K, V](topicDef.topicName.value)(topicDef.consumed))
+    Reader(builder => builder.globalTable[K, V](topicDef.topicName.value)(consumed))
 
   def gktable(mat: Materialized[K, V, ByteArrayKeyValueStore]): Reader[StreamsBuilder, GlobalKTable[K, V]] =
-    Reader(builder => builder.globalTable[K, V](topicDef.topicName.value, mat)(topicDef.consumed))
+    Reader(builder => builder.globalTable[K, V](topicDef.topicName.value, mat)(consumed))
 
-  def gktable(supplier: KeyValueBytesStoreSupplier): Reader[StreamsBuilder, GlobalKTable[K, V]] = {
-    val mat: Materialized[K, V, ByteArrayKeyValueStore] =
-      Materialized.as[K, V](supplier)(topicDef.serdeOfKey, topicDef.serdeOfVal)
-    Reader((builder: StreamsBuilder) => builder.globalTable[K, V](topicDef.topicName.value, mat)(topicDef.consumed))
-  }
+  def gktable(supplier: KeyValueBytesStoreSupplier): Reader[StreamsBuilder, GlobalKTable[K, V]] =
+    gktable(Materialized.as[K, V](supplier)(topicDef.serdeOfKey, topicDef.serdeOfVal))
 }
