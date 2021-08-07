13574192dbf25b5dcff25935cfe1dc04b61d9938
Harry Chen <chen.harry.hua@gmail.com>
2021-08-07 11:45:44 +1000

    retry

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/ActionRetry.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/ActionRetry.scala
@@ -3,11 +3,26 @@ package com.github.chenharryhua.nanjin.guard.action
 import cats.collections.Predicate
 import cats.data.{Kleisli, Reader}
 import cats.effect.kernel.{Async, Outcome, Ref}
+import cats.effect.std.UUIDGen
 import cats.effect.syntax.all.*
 import cats.syntax.all.*
-import com.github.chenharryhua.nanjin.guard.alert.{ActionStart, DailySummaries, NJEvent, ServiceInfo}
+import com.github.chenharryhua.nanjin.guard.alert.{
+  ActionFailed,
+  ActionInfo,
+  ActionRetrying,
+  ActionStart,
+  ActionSucced,
+  DailySummaries,
+  NJError,
+  NJEvent,
+  Notes,
+  ServiceInfo
+}
 import com.github.chenharryhua.nanjin.guard.config.ActionParams
+import com.github.chenharryhua.nanjin.guard.realZonedDateTime
 import fs2.concurrent.Channel
+import retry.RetryDetails
+import retry.RetryDetails.{GivingUp, WillDelayAndRetry}
 
 // https://www.microsoft.com/en-us/research/wp-content/uploads/2016/07/asynch-exns.pdf
 final class ActionRetry[F[_], A, B](
@@ -85,27 +100,97 @@ final class ActionRetry[F[_], A, B](
       isWorthRetry = isWorthRetry,
       postCondition = Predicate(postCondition))
 
+  private val actionInfo: F[ActionInfo] = for {
+    ts <- realZonedDateTime(params.serviceParams)
+    uuid <- UUIDGen.randomUUID
+  } yield ActionInfo(id = uuid, launchTime = ts, actionName = actionName, serviceInfo = serviceInfo)
+
+  private def failNotes(error: Throwable): F[Notes] = fail.run((input, error)).map(Notes(_))
+  private def succNotes(b: B): F[Notes]             = succ.run((input, b)).map(Notes(_))
+
+  private def onError(actionInfo: ActionInfo, retryCount: Ref[F, Int])(
+    error: Throwable,
+    details: RetryDetails): F[Unit] =
+    details match {
+      case wdr: WillDelayAndRetry =>
+        for {
+          now <- realZonedDateTime(params.serviceParams)
+          _ <- channel.send(
+            ActionRetrying(
+              timestamp = now,
+              actionInfo = actionInfo,
+              actionParams = params,
+              willDelayAndRetry = wdr,
+              error = NJError(error)))
+          _ <- retryCount.update(_ + 1)
+          _ <- dailySummaries.update(_.incActionRetries)
+        } yield ()
+      case _: GivingUp => F.unit
+    }
+
+  private def handleOutcome(actionInfo: ActionInfo, retryCount: Ref[F, Int])(
+    outcome: Outcome[F, Throwable, B]): F[Unit] =
+    outcome match {
+      case Outcome.Canceled() =>
+        for {
+          count <- retryCount.get // number of retries
+          now <- realZonedDateTime(params.serviceParams)
+          _ <- dailySummaries.update(_.incActionFail)
+          fn <- failNotes(ActionException.ActionCanceledExternally)
+          _ <- channel.send(
+            ActionFailed(
+              timestamp = now,
+              actionInfo = actionInfo,
+              actionParams = params,
+              numRetries = count,
+              notes = fn,
+              error = NJError(ActionException.ActionCanceledExternally)
+            ))
+        } yield ()
+      case Outcome.Errored(error) =>
+        for {
+          count <- retryCount.get // number of retries
+          now <- realZonedDateTime(params.serviceParams)
+          _ <- dailySummaries.update(_.incActionFail)
+          fn <- failNotes(error)
+          _ <- channel.send(
+            ActionFailed(
+              timestamp = now,
+              actionInfo = actionInfo,
+              actionParams = params,
+              numRetries = count,
+              notes = fn,
+              error = NJError(error)
+            ))
+        } yield ()
+      case Outcome.Succeeded(fb) =>
+        for {
+          count <- retryCount.get // number of retries before success
+          now <- realZonedDateTime(params.serviceParams)
+          b <- fb
+          sn <- succNotes(b)
+          _ <- dailySummaries.update(_.incActionSucc)
+          _ <- channel.send(
+            ActionSucced(
+              timestamp = now,
+              actionInfo = actionInfo,
+              actionParams = params,
+              numRetries = count,
+              notes = sn))
+        } yield ()
+    }
+
   def run: F[B] =
     for {
       retryCount <- F.ref(0) // hold number of retries
-      base = new ActionRetryBase[F, A, B](
-        actionName = actionName,
-        serviceInfo = serviceInfo,
-        retryCount = retryCount,
-        channel = channel,
-        dailySummaries = dailySummaries,
-        params = params,
-        input = input,
-        succ = succ,
-        fail = fail)
-      actionInfo <- base.actionInfo
-      _ <- channel.send(ActionStart(actionInfo.launchTime, actionInfo, params))
+      ai <- actionInfo
+      _ <- channel.send(ActionStart(ai.launchTime, ai, params))
       res <- F.uncancelable(poll =>
         retry.mtl
           .retryingOnSomeErrors[B](
             params.retry.policy[F],
             isWorthRetry.map(F.pure).run,
-            base.onError(actionInfo)
+            onError(ai, retryCount)
           ) {
             for {
               gate <- F.deferred[Outcome[F, Throwable, B]]
@@ -117,7 +202,7 @@ final class ActionRetry[F[_], A, B](
               _ <- F.raiseError(ActionException.PostConditionUnsatisfied).whenA(!postCondition(oc))
             } yield oc
           }
-          .guaranteeCase(base.handleOutcome(actionInfo)))
+          .guaranteeCase(handleOutcome(ai, retryCount)))
     } yield res
 }
 
