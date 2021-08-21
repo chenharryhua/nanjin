a6aa37a100a1a7d65abca3aec735fb737bb2ccbe
Harry Chen <chen.harry.hua@gmail.com>
2021-08-21 11:54:55 +1000

    cron

--- datetime/src/main/scala/com/github/chenharryhua/nanjin/datetime/crontabs.scala
+++ datetime/src/main/scala/com/github/chenharryhua/nanjin/datetime/crontabs.scala
@@ -4,15 +4,18 @@ import cron4s.Cron
 import cron4s.expr.CronExpr
 
 object crontabs {
-  val everySundayNight: CronExpr = Cron.unsafeParse("0 0 0 ? * 0")
 
-  val midnightEveryday: CronExpr = Cron.unsafeParse("0 0 0 ? * *")
-
-  val hourly: CronExpr    = Cron.unsafeParse("0 0 */1 ? * *")
-  val bihourly: CronExpr  = Cron.unsafeParse("0 0 */2 ? * *")
-  val trihourly: CronExpr = Cron.unsafeParse("0 0 */3 ? * *")
+  val hourly: CronExpr      = Cron.unsafeParse("0 0 */1 ? * *")
+  val bihourly: CronExpr    = Cron.unsafeParse("0 0 */2 ? * *")
+  val trihourly: CronExpr   = Cron.unsafeParse("0 0 */3 ? * *")
+  val every4Hour: CronExpr  = Cron.unsafeParse("0 0 */4 ? * *")
+  val every6Hour: CronExpr  = Cron.unsafeParse("0 0 */6 ? * *")
+  val every8Hour: CronExpr  = Cron.unsafeParse("0 0 */8 ? * *")
+  val every12Hour: CronExpr = Cron.unsafeParse("0 0 */12 ? * *")
 
   val minutely: CronExpr       = Cron.unsafeParse("0 */1 * ? * *")
+  val biminutely: CronExpr     = Cron.unsafeParse("0 */2 * ? * *")
+  val triminutely: CronExpr    = Cron.unsafeParse("0 */3 * ? * *")
   val every5Minutes: CronExpr  = Cron.unsafeParse("0 */5 * ? * *")
   val every10Minutes: CronExpr = Cron.unsafeParse("0 */10 * ? * *")
   val every15Minutes: CronExpr = Cron.unsafeParse("0 */15 * ? * *")
