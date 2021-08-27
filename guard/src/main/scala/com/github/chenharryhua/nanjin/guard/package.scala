411d42acb43eb029af96aa75a6f24cd7f32acd31
Harry Chen <chen.harry.hua@gmail.com>
2021-08-27 22:31:01 +1000

    alignment

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/package.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/package.scala
@@ -10,11 +10,11 @@ package object guard {
   private[guard] def realZonedDateTime[F[_]](serviceParams: ServiceParams)(implicit F: Temporal[F]): F[ZonedDateTime] =
     F.realTimeInstant.map(_.atZone(serviceParams.taskParams.zoneId))
 
-  private[guard] def passThroughMRName(desc: String): String = s"06.[$desc].number"
+  private[guard] def passThroughMRName(desc: String): String = s"06.[$desc].count"
   private[guard] def actionFailMRName(name: String): String  = s"07.[$name].`fail`"
 
-  // number retrys and succed have 6 chars so that the number after is aligned
-  private[guard] def actionStartMRName(name: String): String = s"07.[$name].number"
-  private[guard] def actionRetryMRName(name: String): String = s"07.[$name].retrys"
-  private[guard] def actionSuccMRName(name: String): String  = s"07.[$name].succed"
+  // alignment
+  private[guard] def actionStartMRName(name: String): String = s"07.[$name].count"
+  private[guard] def actionRetryMRName(name: String): String = s"07.[$name].retry"
+  private[guard] def actionSuccMRName(name: String): String  = s"07.[$name].succd"
 }
