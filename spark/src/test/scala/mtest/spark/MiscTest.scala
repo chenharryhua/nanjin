6442ca94567bff6483c46fd9c6c8e259bf183625
Harry Chen <chen.harry.hua@gmail.com>
2021-09-11 20:49:24 +1000

    db

--- spark/src/test/scala/mtest/spark/MiscTest.scala
+++ spark/src/test/scala/mtest/spark/MiscTest.scala
@@ -1,15 +1,12 @@
 package mtest.spark
 
-import cats.derived.auto.eq._
+import cats.derived.auto.eq.*
 import cats.kernel.Eq
-import com.github.chenharryhua.nanjin.spark.injection._
+import com.github.chenharryhua.nanjin.spark.injection.*
 import frameless.{TypedDataset, TypedEncoder, TypedExpressionEncoder}
-import mtest.spark.pb.test.Whale
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.funsuite.AnyFunSuite
-
-import scala.util.Random
 
 object JoinTestData {
   final case class Brother(id: Int, rel: String)
@@ -42,7 +39,7 @@ object JoinTestData {
 }
 
 class MiscTest extends AnyFunSuite {
-  import JoinTestData._
+  import JoinTestData.*
   implicit val ss: SparkSession = sparkSession
   test("spark left join") {
     val db = TypedDataset.create(brothers)
@@ -53,20 +50,20 @@ class MiscTest extends AnyFunSuite {
 
   }
 
-  test("typed encoder of scalapb generate case class") {
-    import scalapb.spark.Implicits._
-    val pt: TypedEncoder[Whale] = TypedEncoder[Whale] // should compile
-    val whales = List(
-      Whale("aaa", Random.nextInt()),
-      Whale("bbb", Random.nextInt()),
-      Whale("ccc", Random.nextInt())
-    )
-    val path = "./data/test/spark/protobuf/whales.pb"
-    TypedDataset.create(whales).write.mode(SaveMode.Overwrite).parquet(path)
-    val rst =
-      TypedDataset.createUnsafe[Whale](sparkSession.read.parquet(path)).dataset.collect().toSet
-    assert(rst == whales.toSet)
-  }
+//  test("typed encoder of scalapb generate case class") {
+//    import scalapb.spark.Implicits._
+//    val pt: TypedEncoder[Whale] = TypedEncoder[Whale] // should compile
+//    val whales = List(
+//      Whale("aaa", Random.nextInt()),
+//      Whale("bbb", Random.nextInt()),
+//      Whale("ccc", Random.nextInt())
+//    )
+//    val path = "./data/test/spark/protobuf/whales.pb"
+//    TypedDataset.create(whales).write.mode(SaveMode.Overwrite).parquet(path)
+//    val rst =
+//      TypedDataset.createUnsafe[Whale](sparkSession.read.parquet(path)).dataset.collect().toSet
+//    assert(rst == whales.toSet)
+//  }
 
   test("gen case class") {
     val tds = TypedDataset.create(rdd)(se, sparkSession)
@@ -74,7 +71,7 @@ class MiscTest extends AnyFunSuite {
   }
 
   test("gen schema") {
-    import sparkSession.implicits._
+    import sparkSession.implicits.*
     val s             = TypedExpressionEncoder.targetStructType(se)
     val df: DataFrame = sparkSession.createDataFrame(rdd.toDF().rdd, s)
     println(s)
