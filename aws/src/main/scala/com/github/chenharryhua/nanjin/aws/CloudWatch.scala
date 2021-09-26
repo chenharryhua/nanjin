41cb8914b7bb7c394eaa495b6aa3f580523f94dc
Harry Chen <chen.harry.hua@gmail.com>
2021-09-27 09:14:58 +1000

    Cloudwatch

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/CloudWatch.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/CloudWatch.scala
@@ -2,21 +2,21 @@ package com.github.chenharryhua.nanjin.aws
 
 import cats.effect.kernel.{Resource, Sync}
 import cats.syntax.all.*
-import com.amazonaws.services.cloudwatch.model.{PutMetricDataRequest, PutMetricDataResult}
+import com.amazonaws.services.cloudwatch.model.PutMetricDataRequest
 import com.amazonaws.services.cloudwatch.{AmazonCloudWatch, AmazonCloudWatchClientBuilder}
 import org.typelevel.log4cats.SelfAwareStructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 sealed trait CloudWatch[F[_]] {
-  def putMetricData(putMetricDataRequest: PutMetricDataRequest): F[PutMetricDataResult]
+  def putMetricData(putMetricDataRequest: PutMetricDataRequest): F[Unit]
 }
 
 object CloudWatch {
   def fake[F[_]](implicit F: Sync[F]): Resource[F, CloudWatch[F]] = {
     val logger: SelfAwareStructuredLogger[F] = Slf4jLogger.getLogger[F]
     Resource.pure[F, CloudWatch[F]](new CloudWatch[F] {
-      override def putMetricData(putMetricDataRequest: PutMetricDataRequest): F[PutMetricDataResult] =
-        logger.info(putMetricDataRequest.toString) *> F.pure(new PutMetricDataResult())
+      override def putMetricData(putMetricDataRequest: PutMetricDataRequest): F[Unit] =
+        logger.info(putMetricDataRequest.toString)
     })
   }
 
@@ -24,11 +24,15 @@ object CloudWatch {
     Resource.make(F.delay(new CloudWathImpl))(_.shutdown)
 
   final private class CloudWathImpl[F[_]](implicit F: Sync[F]) extends CloudWatch[F] with ShutdownService[F] {
-    private val client: AmazonCloudWatch = AmazonCloudWatchClientBuilder.standard().build()
+    val logger: SelfAwareStructuredLogger[F] = Slf4jLogger.getLogger[F]
+    private val client: AmazonCloudWatch     = AmazonCloudWatchClientBuilder.standard().build()
 
     override def shutdown: F[Unit] = F.blocking(client.shutdown())
 
-    override def putMetricData(putMetricDataRequest: PutMetricDataRequest): F[PutMetricDataResult] =
-      F.delay(client.putMetricData(putMetricDataRequest))
+    override def putMetricData(putMetricDataRequest: PutMetricDataRequest): F[Unit] =
+      F.delay(client.putMetricData(putMetricDataRequest)).attempt.flatMap {
+        case Right(_) => F.unit
+        case Left(ex) => logger.warn(ex)("Cloudwatch")
+      }
   }
 }
