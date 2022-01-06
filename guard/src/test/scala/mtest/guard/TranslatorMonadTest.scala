4f9ddb39ef17a411d77bc1a47a8bd77f26efd261
Harry Chen <chen.harry.hua@gmail.com>
2022-01-06 16:20:35 +1100

    better than nothing test

--- guard/src/test/scala/mtest/guard/TranslatorMonadTest.scala
+++ guard/src/test/scala/mtest/guard/TranslatorMonadTest.scala
@@ -1,47 +1,70 @@
 package mtest.guard
 
-import cats.Eval
+import cats.Eq
 import cats.effect.IO
-import cats.kernel.Eq
-import cats.laws.discipline.*
-import cats.laws.discipline.SemigroupalTests.Isomorphisms
 import cats.laws.discipline.eq.*
+import cats.laws.discipline.{ExhaustiveCheck, MonadTests}
 import com.github.chenharryhua.nanjin.guard.TaskGuard
-import com.github.chenharryhua.nanjin.guard.event.{NJEvent, ServiceStart, ServiceStatus}
+import com.github.chenharryhua.nanjin.guard.event.*
+import com.github.chenharryhua.nanjin.guard.service.ServiceGuard
 import com.github.chenharryhua.nanjin.guard.translators.Translator
 import munit.DisciplineSuite
+import org.scalacheck.{Arbitrary, Gen}
 
-import java.time.{Instant, ZonedDateTime}
+import java.time.Instant
 import java.util.UUID
-import cats.*
-import cats.laws.discipline.arbitrary.*
-import cats.syntax.all.*
-import org.scalacheck.{Cogen, Gen}
-import org.scalacheck.Prop.*
 
 object gendata {
-  implicit val cogenEvent: Cogen[NJEvent] = Cogen[NJEvent]((e: NJEvent) => e.timestamp.toEpochMilli)
+  val service: ServiceGuard[IO] = TaskGuard[IO]("monad").service("tailrecM")
 
-  val f1 = Gen.function1[NJEvent, Int]
-
-}
-
-class TranslatorMonadTest extends DisciplineSuite {
-  import gendata.*
-  val service = TaskGuard[IO]("monad").service("tailrecM")
   implicit val exhaustiveCheck: ExhaustiveCheck[NJEvent] =
     ExhaustiveCheck.instance(
       List(ServiceStart(ServiceStatus.Up(UUID.randomUUID(), Instant.now()), Instant.now(), service.serviceParams)))
 
-  implicit def translatorEq: Eq[Translator[Eval, Int]] =
-    Eq.by[Translator[Eval, Int], NJEvent => Eval[Option[Int]]](_.translate)
+  implicit def translatorEq: Eq[Translator[Option, Int]] =
+    Eq.by[Translator[Option, Int], NJEvent => Option[Option[Int]]](_.translate)
 
-  implicit val iso: Isomorphisms[Translator[Eval, *]] = Isomorphisms.invariant[Translator[Eval, *]]
+  implicit val arbiTranslator: Arbitrary[Translator[Option, Int]] = Arbitrary(
+    Gen.const(
+      Translator
+        .empty[Option, Int]
+        .withServiceStart(_ => 1)
+        .withServiceStop(_ => 2)
+        .withServicePanic(_ => 3)
+        .withMetricsReport(_ => 4)
+        .withMetricsReset(_ => 5)
+        .withInstantAlert(_ => 6)
+        .withPassThrough(_ => 7)
+        .withActionStart(_ => 8)
+        .withActionFail(_ => 9)
+        .withActionSucc(_ => 10)
+        .withActionRetry(_ => 11)))
 
-  // checkAll("Translator.MonadLaws", MonadTests[Translator[Eval, *]].monad[Int, Int, Int])
+  val add: Int => Int = _ + 1
 
-  test("test") {
-    val ggg = f1.sample.map(_(Gen.posNum[Int]))
-  }
+  implicit val arbiAtoB: Arbitrary[Translator[Option, Int => Int]] = Arbitrary(
+    Gen.const(
+      Translator
+        .empty[Option, Int => Int]
+        .withServiceStart(_ => add)
+        .withServiceStop(_ => add)
+        .withServicePanic(_ => add)
+        .withMetricsReport(_ => add)
+        .withMetricsReset(_ => add)
+        .withInstantAlert(_ => add)
+        .withPassThrough(_ => add)
+        .withActionStart(_ => add)
+        .withActionFail(_ => add)
+        .withActionSucc(_ => add)
+        .withActionRetry(_ => add)))
+
+  implicit val eqAbc: Eq[Translator[Option, (Int, Int, Int)]] =
+    (x: Translator[Option, (Int, Int, Int)], y: Translator[Option, (Int, Int, Int)]) => true
+}
+
+class TranslatorMonadTest extends DisciplineSuite {
+  import gendata.*
+  // just check tailRecM stack safety
+  checkAll("Translator.MonadLaws", MonadTests[Translator[Option, *]].monad[Int, Int, Int])
 
 }
