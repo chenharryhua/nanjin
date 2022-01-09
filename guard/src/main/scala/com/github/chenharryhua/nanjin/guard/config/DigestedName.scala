6a33de4c19f25135418ef7dbd1d7e8f15ddbf47c
Harry Chen <chen.harry.hua@gmail.com>
2022-01-10 09:22:06 +1100

    refined

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/config/DigestedName.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/config/DigestedName.scala
@@ -16,14 +16,15 @@ private[guard] object DigestedName {
 
   implicit val showDigestedName: Show[DigestedName] = _.metricRepr
 
-  def apply(spans: List[String], serviceParams: ServiceParams): DigestedName = {
-    val name: String    = spans.mkString("/")
-    val sha1Hex: String = DigestUtils.sha1Hex(s"${serviceParams.taskParams.appName}/${serviceParams.serviceName}/$name")
+  def apply(spans: List[Span], serviceParams: ServiceParams): DigestedName = {
+    val name: String = spans.map(_.value).mkString("/")
+    val sha1Hex: String =
+      DigestUtils.sha1Hex(s"${serviceParams.taskParams.appName.value}/${serviceParams.serviceName.value}/$name")
     new DigestedName(name, sha1Hex.take(LENGTH))
   }
 
-  def apply(serviceName: String, taskParams: TaskParams): DigestedName = {
-    val sha1Hex: String = DigestUtils.sha1Hex(s"${taskParams.appName}/$serviceName")
-    new DigestedName(serviceName, sha1Hex.take(LENGTH))
+  def apply(serviceName: ServiceName, taskParams: TaskParams): DigestedName = {
+    val sha1Hex: String = DigestUtils.sha1Hex(s"${taskParams.appName.value}/${serviceName.value}")
+    new DigestedName(serviceName.value, sha1Hex.take(LENGTH))
   }
 }
