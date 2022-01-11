5408ecdad0b465107d33d5eabe0bcc0d46f79884
Harry Chen <chen.harry.hua@gmail.com>
2022-01-12 09:36:29 +1100

    add counter to meter

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/NJMeter.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/NJMeter.scala
@@ -1,13 +1,27 @@
 package com.github.chenharryhua.nanjin.guard.action
 
 import cats.effect.kernel.Sync
-import com.codahale.metrics.{Meter, MetricRegistry}
+import cats.syntax.all.*
+import com.codahale.metrics.{Counter, Meter, MetricRegistry}
 import com.github.chenharryhua.nanjin.guard.config.DigestedName
 
-final class NJMeter[F[_]: Sync] private[guard] (metricName: DigestedName, metricRegistry: MetricRegistry) {
+// counter can be reset, meter can't
+final class NJMeter[F[_]] private[guard] (
+  metricName: DigestedName,
+  metricRegistry: MetricRegistry,
+  isCountAsError: Boolean,
+  isCounting: Boolean)(implicit F: Sync[F]) {
 
-  private lazy val meter: Meter = metricRegistry.meter(meterMRName(metricName))
+  private lazy val meter: Meter     = metricRegistry.meter(meterMRName(metricName))
+  private lazy val counter: Counter = metricRegistry.counter(counterMRName(metricName, isCountAsError))
+
+  def asError   = new NJMeter[F](metricName, metricRegistry, true, true)
+  def withCount = new NJMeter[F](metricName, metricRegistry, false, true)
 
   def unsafeMark(num: Long): Unit = meter.mark(num)
-  def mark(num: Long): F[Unit]    = Sync[F].delay(unsafeMark(num))
+  def mark(num: Long): F[Unit] =
+    for {
+      _ <- F.delay(unsafeMark(num))
+      _ <- F.delay(counter.inc(num)).whenA(isCounting)
+    } yield ()
 }
