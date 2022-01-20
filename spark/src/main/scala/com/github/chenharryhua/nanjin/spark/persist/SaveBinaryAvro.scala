18302384af3966f76a06769f831fdbb6377dd85e
Harry Chen <chen.harry.hua@gmail.com>
2022-01-20 23:30:04 +1100

    listener

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveBinaryAvro.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveBinaryAvro.scala
@@ -1,18 +1,16 @@
 package com.github.chenharryhua.nanjin.spark.persist
 
-import cats.Applicative
 import cats.data.Kleisli
 import cats.effect.kernel.Sync
 import com.github.chenharryhua.nanjin.common.ChunkSize
 import com.github.chenharryhua.nanjin.spark.RddExt
 import com.sksamuel.avro4s.Encoder as AvroEncoder
-import fs2.Stream
+import fs2.{Pipe, Stream}
 import org.apache.hadoop.conf.Configuration
 import org.apache.spark.rdd.RDD
 
 final class SaveBinaryAvro[F[_], A](rdd: RDD[A], encoder: AvroEncoder[A], cfg: HoarderConfig) extends Serializable {
-  def file(implicit F: Applicative[F]): SaveSingleBinaryAvro[F, A] =
-    new SaveSingleBinaryAvro[F, A](rdd, encoder, cfg, Kleisli(_ => F.unit))
+  def file: SaveSingleBinaryAvro[F, A]  = new SaveSingleBinaryAvro[F, A](rdd, encoder, cfg, None)
   def folder: SaveMultiBinaryAvro[F, A] = new SaveMultiBinaryAvro[F, A](rdd, encoder, cfg)
 }
 
@@ -20,7 +18,7 @@ final class SaveSingleBinaryAvro[F[_], A](
   rdd: RDD[A],
   encoder: AvroEncoder[A],
   cfg: HoarderConfig,
-  listener: Kleisli[F, A, Unit])
+  listener: Option[Kleisli[F, A, Unit]])
     extends Serializable {
   val params: HoarderParams = cfg.evalConfig
 
@@ -33,16 +31,16 @@ final class SaveSingleBinaryAvro[F[_], A](
 
   def withChunkSize(cs: ChunkSize): SaveSingleBinaryAvro[F, A] = updateConfig(cfg.chunkSize(cs))
   def withListener(f: A => F[Unit]): SaveSingleBinaryAvro[F, A] =
-    new SaveSingleBinaryAvro[F, A](rdd, encoder, cfg, Kleisli(f))
+    new SaveSingleBinaryAvro[F, A](rdd, encoder, cfg, Some(Kleisli(f)))
 
   def sink(implicit F: Sync[F]): Stream[F, Unit] = {
     val hc: Configuration     = rdd.sparkContext.hadoopConfiguration
     val sma: SaveModeAware[F] = new SaveModeAware[F](params.saveMode, params.outPath, hc)
-    sma.checkAndRun(
-      rdd
-        .stream[F](params.chunkSize)
-        .evalTap(listener.run)
-        .through(sinks.binAvro(params.outPath, hc, encoder, params.chunkSize)))
+    val src: Stream[F, A]     = rdd.stream[F](params.chunkSize)
+    val tgt: Pipe[F, A, Unit] = sinks.binAvro(params.outPath, hc, encoder, params.chunkSize)
+    val ss: Stream[F, Unit]   = listener.fold(src.through(tgt))(k => src.evalTap(k.run).through(tgt))
+
+    sma.checkAndRun(ss)
   }
 }
 
--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveJackson.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveJackson.scala
@@ -1,18 +1,17 @@
 package com.github.chenharryhua.nanjin.spark.persist
 
-import cats.Applicative
 import cats.data.Kleisli
 import cats.effect.kernel.Sync
 import com.github.chenharryhua.nanjin.common.ChunkSize
 import com.github.chenharryhua.nanjin.spark.RddExt
 import com.sksamuel.avro4s.Encoder as AvroEncoder
-import fs2.Stream
+import fs2.{Pipe, Stream}
 import org.apache.hadoop.conf.Configuration
 import org.apache.spark.rdd.RDD
 
 final class SaveJackson[F[_], A](rdd: RDD[A], encoder: AvroEncoder[A], cfg: HoarderConfig) extends Serializable {
-  def file(implicit F: Applicative[F]): SaveSingleJackson[F, A] =
-    new SaveSingleJackson[F, A](rdd, encoder, cfg, Kleisli(_ => F.unit))
+  def file: SaveSingleJackson[F, A] =
+    new SaveSingleJackson[F, A](rdd, encoder, cfg, None)
   def folder: SaveMultiJackson[F, A] = new SaveMultiJackson[F, A](rdd, encoder, cfg)
 }
 
@@ -20,7 +19,7 @@ final class SaveSingleJackson[F[_], A](
   rdd: RDD[A],
   encoder: AvroEncoder[A],
   cfg: HoarderConfig,
-  listener: Kleisli[F, A, Unit])
+  listener: Option[Kleisli[F, A, Unit]])
     extends Serializable {
   val params: HoarderParams = cfg.evalConfig
 
@@ -37,17 +36,18 @@ final class SaveSingleJackson[F[_], A](
 
   def withChunkSize(cs: ChunkSize): SaveSingleJackson[F, A] = updateConfig(cfg.chunkSize(cs))
   def withListener(f: A => F[Unit]): SaveSingleJackson[F, A] =
-    new SaveSingleJackson[F, A](rdd, encoder, cfg, Kleisli(f))
+    new SaveSingleJackson[F, A](rdd, encoder, cfg, Some(Kleisli(f)))
 
   def sink(implicit F: Sync[F]): Stream[F, Unit] = {
     val hc: Configuration     = rdd.sparkContext.hadoopConfiguration
     val sma: SaveModeAware[F] = new SaveModeAware[F](params.saveMode, params.outPath, hc)
 
-    sma.checkAndRun(
-      rdd
-        .stream[F](params.chunkSize)
-        .evalTap(listener.run)
-        .through(sinks.jackson(params.outPath, hc, encoder, params.compression.fs2Compression, params.chunkSize)))
+    val src: Stream[F, A] = rdd.stream[F](params.chunkSize)
+    val tgt: Pipe[F, A, Unit] =
+      sinks.jackson(params.outPath, hc, encoder, params.compression.fs2Compression, params.chunkSize)
+    val ss: Stream[F, Unit] = listener.fold(src.through(tgt))(k => src.evalTap(k.run).through(tgt))
+
+    sma.checkAndRun(ss)
   }
 }
 
