7dff9c2bebf7e683ad89a939d1ae8b007caaebc6
Harry Chen <chen.harry.hua@gmail.com>
2022-01-22 14:37:16 +1100

    nj-path

--- pipes/src/test/scala/mtest/terminals/ParquetTest.scala
+++ pipes/src/test/scala/mtest/terminals/ParquetTest.scala
@@ -2,7 +2,7 @@ package mtest.terminals
 
 import cats.Eval
 import cats.effect.IO
-import com.github.chenharryhua.nanjin.terminals.NJParquet
+import com.github.chenharryhua.nanjin.terminals.{NJParquet, NJPath}
 import fs2.Stream
 import org.apache.avro.generic.{GenericData, GenericRecord}
 import org.apache.hadoop.fs.Path
@@ -12,16 +12,18 @@ import org.apache.parquet.hadoop.metadata.CompressionCodecName
 import org.apache.parquet.hadoop.util.{HadoopInputFile, HadoopOutputFile}
 import org.scalatest.funsuite.AnyFunSuite
 import cats.effect.unsafe.implicits.global
+import eu.timepit.refined.auto.*
+
 class ParquetTest extends AnyFunSuite {
   import HadoopTestData.*
   val pq = new NJParquet[IO]()
 
   test("snappy parquet write/read") {
     import HadoopTestData.*
-    val pathStr = "./data/test/devices/builder/panda.snappy.parquet"
+    val pathStr = NJPath("./data/test/devices/builder/panda.snappy.parquet")
     val ts      = Stream.emits(pandas).covary[IO]
     val wb = AvroParquetWriter
-      .builder[GenericRecord](HadoopOutputFile.fromPath(new Path(pathStr), cfg))
+      .builder[GenericRecord](HadoopOutputFile.fromPath(pathStr.hadoopPath, cfg))
       .withSchema(pandaSchema)
       .withCompressionCodec(CompressionCodecName.SNAPPY)
       .withDataModel(GenericData.get())
@@ -30,7 +32,7 @@ class ParquetTest extends AnyFunSuite {
 
     val rb = Eval.later(
       AvroParquetReader
-        .builder[GenericRecord](HadoopInputFile.fromPath(new Path(pathStr), cfg))
+        .builder[GenericRecord](HadoopInputFile.fromPath(pathStr.hadoopPath, cfg))
         .withDataModel(GenericData.get()))
 
     val action = hdp.delete(pathStr) >>
@@ -41,10 +43,10 @@ class ParquetTest extends AnyFunSuite {
   }
 
   test("gzip parquet write/read") {
-    val pathStr = "./data/test/devices/panda.gzip.parquet"
+    val pathStr = NJPath("./data/test/devices/panda.gzip.parquet")
     val ts      = Stream.emits(pandas).covary[IO]
     val wb = AvroParquetWriter
-      .builder[GenericRecord](HadoopOutputFile.fromPath(new Path(pathStr), cfg))
+      .builder[GenericRecord](HadoopOutputFile.fromPath(pathStr.hadoopPath, cfg))
       .withSchema(pandaSchema)
       .withCompressionCodec(CompressionCodecName.GZIP)
       .withDataModel(GenericData.get())
@@ -53,7 +55,7 @@ class ParquetTest extends AnyFunSuite {
 
     val rb = Eval.later(
       AvroParquetReader
-        .builder[GenericRecord](HadoopInputFile.fromPath(new Path(pathStr), cfg))
+        .builder[GenericRecord](HadoopInputFile.fromPath(pathStr.hadoopPath, cfg))
         .withDataModel(GenericData.get()))
 
     val action = hdp.delete(pathStr) >>
@@ -63,10 +65,10 @@ class ParquetTest extends AnyFunSuite {
     assert(action.unsafeRunSync() == pandas)
   }
   test("uncompressed parquet write/read") {
-    val pathStr = "./data/test/devices/panda.uncompressed.parquet"
+    val pathStr = NJPath("./data/test/devices/panda.uncompressed.parquet")
     val ts      = Stream.emits(pandas).covary[IO]
     val wb = AvroParquetWriter
-      .builder[GenericRecord](HadoopOutputFile.fromPath(new Path(pathStr), cfg))
+      .builder[GenericRecord](HadoopOutputFile.fromPath(pathStr.hadoopPath, cfg))
       .withSchema(pandaSchema)
       .withCompressionCodec(CompressionCodecName.UNCOMPRESSED)
       .withDataModel(GenericData.get())
@@ -75,7 +77,7 @@ class ParquetTest extends AnyFunSuite {
 
     val rb = Eval.later(
       AvroParquetReader
-        .builder[GenericRecord](HadoopInputFile.fromPath(new Path(pathStr), cfg))
+        .builder[GenericRecord](HadoopInputFile.fromPath(pathStr.hadoopPath, cfg))
         .withDataModel(GenericData.get()))
 
     val action =
