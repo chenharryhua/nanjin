222ac8ca45940d594d1da806f37bceb07ea566ca
Harry Chen <chen.harry.hua@gmail.com>
2022-01-23 18:37:08 +1100

    console

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/observers/console.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/observers/console.scala
@@ -8,6 +8,8 @@ import com.github.chenharryhua.nanjin.guard.event.NJEvent
 import com.github.chenharryhua.nanjin.guard.translators.{Translator, UpdateTranslator}
 import fs2.Chunk
 
+import java.time.format.DateTimeFormatter
+
 object console {
   def apply[F[_]: Console: Monad](translator: Translator[F, String]): TextConsole[F] = new TextConsole[F](translator)
 
@@ -19,7 +21,8 @@ final class TextConsole[F[_]: Monad](translator: Translator[F, String])(implicit
 
   override def updateTranslator(f: Translator[F, String] => Translator[F, String]): TextConsole[F] =
     new TextConsole[F](f(translator))
-
-  override def apply(event: NJEvent): F[Unit] = translator.translate(event).flatMap(_.traverse(C.println).void)
-  def chunk(events: Chunk[NJEvent]): F[Unit]  = events.traverse(apply).void
+  private[this] val fmt: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+  override def apply(event: NJEvent): F[Unit] =
+    translator.translate(event).flatMap(_.traverse(evt => C.println(s"${event.zonedDateTime.format(fmt)} $evt")).void)
+  def chunk(events: Chunk[NJEvent]): F[Unit] = events.traverse(apply).void
 }
