cbd70285fdcd22ebe6a104acb1754ef6d8140cdc
Harry Chen <chen.harry.hua@gmail.com>
2022-02-01 01:09:33 +1100

    akka

--- pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/AkkaHadoop.scala
+++ pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/AkkaHadoop.scala
@@ -1,9 +1,9 @@
 package com.github.chenharryhua.nanjin.terminals
 
 import akka.{Done, NotUsed}
-import akka.stream.{ActorAttributes, Attributes, IOResult, Inlet, SinkShape}
+import akka.stream.{ActorAttributes, Attributes, IOResult, Inlet, Outlet, SinkShape, SourceShape}
 import akka.stream.scaladsl.{Sink, Source, StreamConverters}
-import akka.stream.stage.{GraphStageLogic, GraphStageWithMaterializedValue, InHandler}
+import akka.stream.stage.{GraphStage, GraphStageLogic, GraphStageWithMaterializedValue, InHandler, OutHandler}
 import akka.util.ByteString
 import org.apache.avro.Schema
 import org.apache.avro.file.{CodecFactory, DataFileStream, DataFileWriter}
@@ -12,7 +12,7 @@ import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.{FSDataInputStream, FSDataOutputStream, FileSystem}
 import squants.information.Information
 
-import java.io.OutputStream
+import java.io.{InputStream, OutputStream}
 import java.net.URI
 import scala.concurrent.{Future, Promise}
 import scala.jdk.CollectionConverters.IteratorHasAsScala
@@ -31,12 +31,38 @@ final class AkkaHadoop(config: Configuration) {
   def byteSink(path: NJPath): Sink[ByteString, Future[IOResult]] =
     StreamConverters.fromOutputStream(() => fsOutput(path))
 
-  def avroSource(path: NJPath, schema: Schema): Source[GenericRecord, NotUsed] = {
-    val dfs: DataFileStream[GenericRecord] = new DataFileStream(fsInput(path), new GenericDatumReader(schema))
-    Source.fromIterator(() => dfs.iterator().asScala)
-  }
+  def avroSource(path: NJPath, schema: Schema): Source[GenericRecord, NotUsed] =
+    Source.fromGraph(new AvroSource(fsInput(path), schema))
   def avroSink(path: NJPath, schema: Schema, codecFactory: CodecFactory): Sink[GenericRecord, Future[Done]] =
     Sink.fromGraph(new AvroSink(fsOutput(path), schema, codecFactory))
+}
+
+private class AvroSource(is: InputStream, schema: Schema) extends GraphStage[SourceShape[GenericRecord]] {
+  private val out: Outlet[GenericRecord] = Outlet("akka.avro.source")
+
+  override protected val initialAttributes: Attributes = super.initialAttributes.and(ActorAttributes.IODispatcher)
+
+  private val reader: DataFileStream[GenericRecord] =
+    new DataFileStream(is, new GenericDatumReader[GenericRecord](schema))
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
+    new GraphStageLogic(shape) {
+      setHandler(
+        out,
+        new OutHandler {
+          override def onDownstreamFinish(cause: Throwable): Unit =
+            try {
+              reader.close()
+              is.close()
+            } finally super.onDownstreamFinish(cause)
+
+          override def onPull(): Unit =
+            if (reader.hasNext) push(out, reader.next())
+            else complete(out)
+        }
+      )
+    }
+  override val shape: SourceShape[GenericRecord] = SourceShape.of(out)
 }
 
 private class AvroSink(os: OutputStream, schema: Schema, codecFactory: CodecFactory)
@@ -59,24 +85,23 @@ private class AvroSink(os: OutputStream, schema: Schema, codecFactory: CodecFact
       setHandler(
         in,
         new InHandler {
-          override def onUpstreamFinish(): Unit = {
-            super.onUpstreamFinish()
+          override def onUpstreamFinish(): Unit =
             try {
               writer.close()
               os.close()
               promise.complete(Success(Done))
             } catch {
               case ex: Throwable => promise.complete(Failure(ex))
-            }
-          }
+            } finally super.onUpstreamFinish()
 
-          override def onUpstreamFailure(ex: Throwable): Unit = {
-            super.onUpstreamFailure(ex)
+          override def onUpstreamFailure(ex: Throwable): Unit =
             try {
               writer.close()
               os.close()
-            } finally promise.complete(Failure(ex))
-          }
+            } finally {
+              super.onUpstreamFailure(ex)
+              promise.complete(Failure(ex))
+            }
 
           override def onPush(): Unit = {
             val gr: GenericRecord = grab(in)
