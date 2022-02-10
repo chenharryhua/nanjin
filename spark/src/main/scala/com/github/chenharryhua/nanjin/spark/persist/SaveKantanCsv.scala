df2c7ac37b7aa70035074600dbb59f4e5d15a524
Harry Chen <chen.harry.hua@gmail.com>
2022-02-11 09:51:53 +1100

    kantan

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveKantanCsv.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/SaveKantanCsv.scala
@@ -1,27 +1,37 @@
 package com.github.chenharryhua.nanjin.spark.persist
 
 import cats.effect.kernel.Sync
-import kantan.csv.CsvConfiguration
-import kantan.csv.CsvConfiguration.QuotePolicy
+import kantan.csv.CsvConfiguration.Header
+import kantan.csv.{CsvConfiguration, HeaderEncoder}
+import monocle.macros.GenLens
 import org.apache.spark.sql.Dataset
-
-final class SaveKantanCsv[F[_], A](ds: Dataset[A], val csvConfiguration: CsvConfiguration, cfg: HoarderConfig)
+final class SaveKantanCsv[F[_], A](
+  ds: Dataset[A],
+  val csvConfiguration: CsvConfiguration,
+  cfg: HoarderConfig,
+  encoder: HeaderEncoder[A])
     extends Serializable {
 
   def updateCsvConfig(f: CsvConfiguration => CsvConfiguration): SaveKantanCsv[F, A] =
-    new SaveKantanCsv[F, A](ds, f(csvConfiguration), cfg)
+    new SaveKantanCsv[F, A](ds, f(csvConfiguration), cfg, encoder)
 
-  def withHeader: SaveKantanCsv[F, A]                    = updateCsvConfig(_.withHeader)
-  def withoutHeader: SaveKantanCsv[F, A]                 = updateCsvConfig(_.withoutHeader)
-  def quoteAll: SaveKantanCsv[F, A]                      = updateCsvConfig(_.quoteAll)
-  def quoteWhenNeeded: SaveKantanCsv[F, A]               = updateCsvConfig(_.quoteWhenNeeded)
-  def withQuote(char: Char): SaveKantanCsv[F, A]         = updateCsvConfig(_.withQuote(char))
+  // header
+  def withHeader: SaveKantanCsv[F, A]              = updateCsvConfig(_.withHeader)
+  def withHeader(ss: String*): SaveKantanCsv[F, A] = updateCsvConfig(_.withHeader(CsvConfiguration.Header.Explicit(ss)))
+  def withoutHeader: SaveKantanCsv[F, A]           = updateCsvConfig(_.withoutHeader)
+
+  // quote
+  def quoteAll: SaveKantanCsv[F, A]              = updateCsvConfig(_.quoteAll)
+  def quoteWhenNeeded: SaveKantanCsv[F, A]       = updateCsvConfig(_.quoteWhenNeeded)
+  def withQuote(char: Char): SaveKantanCsv[F, A] = updateCsvConfig(_.withQuote(char))
+
+  // seperator
   def withCellSeparator(char: Char): SaveKantanCsv[F, A] = updateCsvConfig(_.withCellSeparator(char))
 
   val params: HoarderParams = cfg.evalConfig
 
   private def updateConfig(cfg: HoarderConfig): SaveKantanCsv[F, A] =
-    new SaveKantanCsv[F, A](ds, csvConfiguration, cfg)
+    new SaveKantanCsv[F, A](ds, csvConfiguration, cfg, encoder)
 
   def append: SaveKantanCsv[F, A]         = updateConfig(cfg.appendMode)
   def overwrite: SaveKantanCsv[F, A]      = updateConfig(cfg.overwriteMode)
@@ -36,18 +46,10 @@ final class SaveKantanCsv[F[_], A](ds: Dataset[A], val csvConfiguration: CsvConf
   def run(implicit F: Sync[F]): F[Unit] =
     new SaveModeAware[F](params.saveMode, params.outPath, ds.sparkSession.sparkContext.hadoopConfiguration)
       .checkAndRun(F.interruptibleMany {
-        val quoteAll: Boolean = csvConfiguration.quotePolicy match {
-          case QuotePolicy.Always     => true
-          case QuotePolicy.WhenNeeded => false
+        val modifyHeader = GenLens[CsvConfiguration](_.header).modify {
+          case Header.Implicit => Header.Explicit(ds.schema.fields.map(_.name).toSeq)
+          case other           => other
         }
-        ds.write
-          .mode(params.saveMode)
-          .option("compression", params.compression.name)
-          .option("sep", csvConfiguration.cellSeparator.toString)
-          .option("header", csvConfiguration.hasHeader)
-          .option("quote", csvConfiguration.quote.toString)
-          .option("quoteAll", quoteAll)
-          .option("charset", "UTF8")
-          .csv(params.outPath.pathStr)
+        saveRDD.kantanCsv[A](ds.rdd, params.outPath, params.compression, modifyHeader(csvConfiguration), encoder)
       })
 }
--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/saveRDD.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/persist/saveRDD.scala
@@ -113,13 +113,17 @@ object saveRDD {
       .saveAsNewAPIHadoopFile(path.pathStr, classOf[NullWritable], classOf[Text], classOf[NJTextOutputFormat], config)
   }
 
-  def kantanCsv[A](rdd: RDD[A], path: NJPath, compression: NJCompression, csvConfiguration: CsvConfiguration)(implicit
-    enc: HeaderEncoder[A]): Unit = {
+  def kantanCsv[A](
+    rdd: RDD[A],
+    path: NJPath,
+    compression: NJCompression,
+    csvConfiguration: CsvConfiguration,
+    encoder: HeaderEncoder[A]): Unit = {
     val config: Configuration = new Configuration(rdd.sparkContext.hadoopConfiguration)
     config.set(NJTextOutputFormat.suffix, NJFileFormat.Kantan.suffix)
     CompressionCodecs.setCodecConfiguration(config, CompressionCodecs.getCodecClassName(compression.name))
     rdd
-      .mapPartitions(new KantanCsv[A](enc, csvConfiguration, _), preservesPartitioning = true)
+      .mapPartitions(new KantanCsv[A](encoder, csvConfiguration, _), preservesPartitioning = true)
       .saveAsNewAPIHadoopFile(path.pathStr, classOf[NullWritable], classOf[Text], classOf[NJTextOutputFormat], config)
   }
 }
