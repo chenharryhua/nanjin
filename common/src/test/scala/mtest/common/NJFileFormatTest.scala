f40245c8a1c52d300472b3dc287c72835d023367
Harry Chen <chen.harry.hua@gmail.com>
2022-02-19 13:56:34 +1100

    test

---
+++ common/src/test/scala/mtest/common/NJFileFormatTest.scala
@@ -0,0 +1,13 @@
+package mtest.common
+
+import com.github.chenharryhua.nanjin.common.NJFileFormat
+import com.github.chenharryhua.nanjin.common.NJFileFormat.*
+import org.scalatest.funsuite.AnyFunSuite
+
+class NJFileFormatTest extends AnyFunSuite {
+  test("no dup") {
+    val all: List[NJFileFormat] =
+      List(Unknown, Jackson, Circe, Text, Kantan, SparkJson, SparkCsv, Parquet, Avro, BinaryAvro, JavaObject, ProtoBuf)
+    assert(all.map(_.suffix).distinct.size === all.size)
+  }
+}
