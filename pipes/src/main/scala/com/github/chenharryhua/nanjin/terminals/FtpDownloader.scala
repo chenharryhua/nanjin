5fb3cfd20d62cdca63c83207887cbd7c2dadb48f
Harry Chen <chen.harry.hua@gmail.com>
2022-06-15 23:18:10 +1000

    ftp

--- pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/FtpDownloader.scala
+++ pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/FtpDownloader.scala
@@ -1,28 +1,63 @@
 package com.github.chenharryhua.nanjin.terminals
 
 import akka.stream.{IOResult, Materializer}
-import akka.stream.alpakka.ftp.scaladsl.{Ftp, FtpApi, Ftps, Sftp}
 import akka.stream.alpakka.ftp.{FtpSettings, FtpsSettings, RemoteFileSettings, SftpSettings}
+import akka.stream.alpakka.ftp.scaladsl.{Ftp, FtpApi, Ftps, Sftp}
 import akka.stream.scaladsl.{Sink, Source}
 import akka.util.ByteString
 import cats.effect.kernel.Async
 import com.github.chenharryhua.nanjin.common.ChunkSize
+import com.github.chenharryhua.nanjin.pipes.*
+import com.sksamuel.avro4s.Decoder as AvroDecoder
 import fs2.Stream
 import fs2.interop.reactivestreams.PublisherOps
+import io.circe.Decoder as JsonDecoder
+import kantan.csv.{CsvConfiguration, RowDecoder}
 import net.schmizz.sshj.SSHClient
 import org.apache.commons.net.ftp.{FTPClient, FTPSClient}
 
 import scala.concurrent.Future
-
 sealed abstract class FtpDownloader[F[_], C, S <: RemoteFileSettings](ftpApi: FtpApi[C, S], settings: S) {
 
-  final def download(pathStr: String, chunkSize: ChunkSize)(implicit F: Async[F], mat: Materializer): Stream[F, Byte] =
+  final def download(pathStr: String, chunkSize: ChunkSize)(implicit
+    F: Async[F],
+    mat: Materializer): Stream[F, Byte] =
     Stream.suspend {
       for {
-        bs <- akka.download(pathStr).runWith(Sink.asPublisher(fanout = false)).toStreamBuffered(chunkSize.value)
+        bs <- akka
+          .download(pathStr)
+          .runWith(Sink.asPublisher(fanout = false))
+          .toStreamBuffered(chunkSize.value)
         byte <- Stream.emits(bs)
       } yield byte
     }
+
+  def kantan[A](pathStr: String, csvConfig: CsvConfiguration, chunkSize: ChunkSize)(implicit
+    dec: RowDecoder[A],
+    F: Async[F],
+    mat: Materializer): Stream[F, A] =
+    download(pathStr, chunkSize).through(KantanSerde.fromBytes[F, A](csvConfig, chunkSize))
+
+  def kantan[A](pathStr: String, chunkSize: ChunkSize)(implicit
+    dec: RowDecoder[A],
+    F: Async[F],
+    mat: Materializer): Stream[F, A] =
+    kantan[A](pathStr, CsvConfiguration.rfc, chunkSize)
+
+  def circe[A: JsonDecoder](pathStr: String, chunkSize: ChunkSize)(implicit
+    F: Async[F],
+    mat: Materializer): Stream[F, A] =
+    download(pathStr, chunkSize).through(CirceSerde.fromBytes[F, A])
+
+  def jackson[A](pathStr: String, chunkSize: ChunkSize, dec: AvroDecoder[A])(implicit
+    F: Async[F],
+    mat: Materializer): Stream[F, A] =
+    download(pathStr, chunkSize).through(JacksonSerde.fromBytes(dec.schema)).map(dec.decode)
+
+  def text(pathStr: String, chunkSize: ChunkSize)(implicit
+    F: Async[F],
+    mat: Materializer): Stream[F, String] =
+    download(pathStr, chunkSize).through(TextSerde.fromBytes)
 
   object akka {
     final def download(pathStr: String): Source[ByteString, Future[IOResult]] =
--- pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/FtpUploader.scala
+++ pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/FtpUploader.scala
@@ -1,15 +1,20 @@
 package com.github.chenharryhua.nanjin.terminals
 
-import akka.stream.alpakka.ftp.scaladsl.{Ftp, FtpApi, Ftps, Sftp}
-import akka.stream.alpakka.ftp.{FtpSettings, FtpsSettings, RemoteFileSettings, SftpSettings}
-import akka.stream.scaladsl.{Sink, Source}
 import akka.stream.{IOResult, Materializer}
+import akka.stream.alpakka.ftp.{FtpSettings, FtpsSettings, RemoteFileSettings, SftpSettings}
+import akka.stream.alpakka.ftp.scaladsl.{Ftp, FtpApi, Ftps, Sftp}
+import akka.stream.scaladsl.{Sink, Source}
 import akka.util.ByteString
 import cats.effect.kernel.Async
-import fs2.interop.reactivestreams.StreamOps
+import com.github.chenharryhua.nanjin.pipes.*
+import com.sksamuel.avro4s.{Encoder as AvroEncoder, ToRecord}
 import fs2.{Pipe, Stream}
+import fs2.interop.reactivestreams.StreamOps
+import io.circe.Encoder as JsonEncoder
+import kantan.csv.{CsvConfiguration, RowEncoder}
 import net.schmizz.sshj.SSHClient
 import org.apache.commons.net.ftp.{FTPClient, FTPSClient}
+import squants.information.Information
 
 import scala.concurrent.Future
 
@@ -19,9 +24,37 @@ sealed abstract class FtpUploader[F[_], C, S <: RemoteFileSettings](ftpApi: FtpA
     (ss: Stream[F, Byte]) =>
       Stream.eval(ss.chunks.toUnicastPublisher.use { p =>
         F.fromFuture(
-          F.blocking(Source.fromPublisher(p).map(x => ByteString.apply(x.toArray)).runWith(akka.upload(pathStr))))
+          F.blocking(
+            Source.fromPublisher(p).map(x => ByteString.apply(x.toArray)).runWith(akka.upload(pathStr))))
       })
   }
+
+  def kantan[A](pathStr: String, csvConfig: CsvConfiguration, byteBuffer: Information)(implicit
+    enc: RowEncoder[A],
+    F: Async[F],
+    mat: Materializer): Pipe[F, A, IOResult] =
+    _.through(KantanSerde.toBytes[F, A](csvConfig, byteBuffer)).through(upload(pathStr))
+
+  def kantan[A](pathStr: String, byteBuffer: Information)(implicit
+    enc: RowEncoder[A],
+    F: Async[F],
+    mat: Materializer): Pipe[F, A, IOResult] =
+    kantan[A](pathStr, CsvConfiguration.rfc, byteBuffer)
+
+  def circe[A: JsonEncoder](pathStr: String, isKeepNull: Boolean = true)(implicit
+    F: Async[F],
+    mat: Materializer): Pipe[F, A, IOResult] =
+    _.through(CirceSerde.toBytes[F, A](isKeepNull)).through(upload(pathStr))
+
+  def jackson[A](pathStr: String, enc: AvroEncoder[A])(implicit
+    F: Async[F],
+    mat: Materializer): Pipe[F, A, IOResult] = {
+    val toRec: ToRecord[A] = ToRecord(enc)
+    _.map(toRec.to).through(JacksonSerde.toBytes[F](enc.schema)).through(upload(pathStr))
+  }
+
+  def text(pathStr: String)(implicit F: Async[F], mat: Materializer): Pipe[F, String, IOResult] =
+    _.through(TextSerde.toBytes).through(upload(pathStr))
 
   object akka {
     final def upload(pathStr: String): Sink[ByteString, Future[IOResult]] =
