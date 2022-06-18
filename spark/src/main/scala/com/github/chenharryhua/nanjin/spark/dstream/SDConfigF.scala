c8fe6c9d083fcea46057dcfecd1aa08ff4c38c34
Harry Chen <chen.harry.hua@gmail.com>
2022-06-18 16:22:35 +1000

    dstream

--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/SDConfigF.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/SDConfigF.scala
@@ -1,7 +1,7 @@
 package com.github.chenharryhua.nanjin.spark.dstream
 
 import cats.Functor
-import com.github.chenharryhua.nanjin.datetime.NJTimestamp
+import java.time.LocalDateTime
 import com.github.chenharryhua.nanjin.terminals.NJPath
 import higherkindness.droste.{scheme, Algebra}
 import higherkindness.droste.data.Fix
@@ -11,16 +11,16 @@ import java.time.ZoneId
 
 @Lenses final private[dstream] case class SDParams private (
   zoneId: ZoneId,
-  pathBuilder: NJPath => NJTimestamp => NJPath
+  pathBuilder: NJPath => LocalDateTime => NJPath
 )
 
 object SDParams {
 
-  private def pathBuilder(zoneId: ZoneId)(root: NJPath)(ts: NJTimestamp): NJPath =
-    root / ts.atZone(zoneId).toLocalDate
+  private def pathBuilder(root: NJPath)(ts: LocalDateTime): NJPath =
+    root / ts.toLocalDate
 
   def apply(zoneId: ZoneId): SDParams =
-    SDParams(zoneId = zoneId, pathBuilder = pathBuilder(zoneId))
+    SDParams(zoneId = zoneId, pathBuilder = pathBuilder)
 }
 
 sealed private[dstream] trait SDConfigF[X]
@@ -29,7 +29,7 @@ private object SDConfigF {
   implicit val functorSDConfigF: Functor[SDConfigF] = cats.derived.semiauto.functor[SDConfigF]
 
   final case class InitParams[K](zoneId: ZoneId) extends SDConfigF[K]
-  final case class WithPathBuilder[K](f: NJPath => NJTimestamp => NJPath, cont: K) extends SDConfigF[K]
+  final case class WithPathBuilder[K](f: NJPath => LocalDateTime => NJPath, cont: K) extends SDConfigF[K]
 
   private val algebra: Algebra[SDConfigF, SDParams] = Algebra[SDConfigF, SDParams] {
     case InitParams(zoneId)    => SDParams(zoneId)
@@ -42,7 +42,7 @@ final private[spark] case class SDConfig private (value: Fix[SDConfigF]) {
   import SDConfigF.*
   def evalConfig: SDParams = SDConfigF.evalConfig(this)
 
-  def pathBuilder(f: NJPath => NJTimestamp => NJPath): SDConfig =
+  def pathBuilder(f: NJPath => LocalDateTime => NJPath): SDConfig =
     SDConfig(Fix(WithPathBuilder(f, value)))
 }
 
--- spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/persist.scala
+++ spark/src/main/scala/com/github/chenharryhua/nanjin/spark/dstream/persist.scala
@@ -1,21 +1,26 @@
 package com.github.chenharryhua.nanjin.spark.dstream
 
 import cats.data.Reader
-import com.github.chenharryhua.nanjin.datetime.NJTimestamp
 import com.github.chenharryhua.nanjin.spark.persist.saveRDD
 import com.github.chenharryhua.nanjin.terminals.{NJCompression, NJPath}
 import com.sksamuel.avro4s.Encoder as AvroEncoder
 import io.circe.Encoder as JsonEncoder
 import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.streaming.Time
+
+import java.time.{Instant, LocalDateTime, ZoneId}
 
 private[dstream] object persist {
+  private def getPath(builder: Reader[LocalDateTime, NJPath], time: Time, zoneId: ZoneId): NJPath =
+    builder.run(Instant.ofEpochMilli(time.milliseconds).atZone(zoneId).toLocalDateTime)
 
   def circe[A: JsonEncoder](
     ds: DStream[A],
-    pathBuilder: Reader[NJTimestamp, NJPath],
+    zoneId: ZoneId,
+    pathBuilder: Reader[LocalDateTime, NJPath],
     isKeepNull: Boolean): DStreamRunner.Mark = {
     ds.foreachRDD { (rdd, time) =>
-      val path: NJPath = pathBuilder.run(NJTimestamp(time.milliseconds))
+      val path: NJPath = getPath(pathBuilder, time, zoneId)
       saveRDD.circe[A](rdd, path, NJCompression.Uncompressed, isKeepNull)
     }
     DStreamRunner.Mark
@@ -23,10 +28,11 @@ private[dstream] object persist {
 
   def jackson[A](
     ds: DStream[A],
+    zoneId: ZoneId,
     encoder: AvroEncoder[A],
-    pathBuilder: Reader[NJTimestamp, NJPath]): DStreamRunner.Mark = {
+    pathBuilder: Reader[LocalDateTime, NJPath]): DStreamRunner.Mark = {
     ds.foreachRDD { (rdd, time) =>
-      val path: NJPath = pathBuilder.run(NJTimestamp(time.milliseconds))
+      val path: NJPath = getPath(pathBuilder, time, zoneId)
       saveRDD.jackson[A](rdd, path, encoder, NJCompression.Uncompressed)
     }
     DStreamRunner.Mark
@@ -34,10 +40,11 @@ private[dstream] object persist {
 
   def avro[A](
     ds: DStream[A],
+    zoneId: ZoneId,
     encoder: AvroEncoder[A],
-    pathBuilder: Reader[NJTimestamp, NJPath]): DStreamRunner.Mark = {
+    pathBuilder: Reader[LocalDateTime, NJPath]): DStreamRunner.Mark = {
     ds.foreachRDD { (rdd, time) =>
-      val path: NJPath = pathBuilder.run(NJTimestamp(time.milliseconds))
+      val path: NJPath = getPath(pathBuilder, time, zoneId)
       saveRDD.avro[A](rdd, path, encoder, NJCompression.Uncompressed)
     }
     DStreamRunner.Mark
