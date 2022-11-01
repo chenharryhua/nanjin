865cbafc225b774d38bb0ea6964dd4648f62df0d
Harry Chen <chen.harry.hua@gmail.com>
2022-11-01 17:34:17 +1100

    test

--- guard/src/test/scala/mtest/guard/TicksTest.scala
+++ guard/src/test/scala/mtest/guard/TicksTest.scala
@@ -4,7 +4,13 @@ import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import cats.implicits.toTraverseOps
 import com.github.chenharryhua.nanjin.guard.*
-import com.github.chenharryhua.nanjin.guard.event.NJEvent.{ActionStart, ActionSucc, PassThrough, ServiceStart, ServiceStop}
+import com.github.chenharryhua.nanjin.guard.event.NJEvent.{
+  ActionStart,
+  ActionSucc,
+  PassThrough,
+  ServiceStart,
+  ServiceStop
+}
 import cron4s.Cron
 import eu.timepit.refined.auto.*
 import io.circe.syntax.EncoderOps
@@ -12,7 +18,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import retry.RetryPolicies
 
 import java.time.{Duration, Instant, ZoneId}
-import scala.concurrent.duration.{DurationDouble, DurationInt}
+import scala.concurrent.duration.{DurationDouble, DurationInt, FiniteDuration}
 import scala.jdk.DurationConverters.JavaDurationOps
 
 class TicksTest extends AnyFunSuite {
@@ -70,6 +76,7 @@ class TicksTest extends AnyFunSuite {
   }
 
   test("4.cron index") {
+
     val lst = service
       .eventStream(ag =>
         ag.ticks(Cron.unsafeParse("0 */5 * ? * *"), RetryPolicies.capDelay[IO](1.second, _))
@@ -85,14 +92,11 @@ class TicksTest extends AnyFunSuite {
   }
 
   test("5. fib awakeEvery") {
+    val policy = RetryPolicies.fibonacciBackoff[IO](1.second).join(RetryPolicies.limitRetries(3))
     val List(a, b, c, d, e, f, g, h) =
       service
         .eventStream(agent =>
-          agent
-            .ticks(RetryPolicies.fibonacciBackoff[IO](1.second).join(RetryPolicies.limitRetries(3)))
-            .evalMap(idx => agent.action("fib", _.notice).retry(IO(idx)).run)
-            .compile
-            .drain)
+          agent.ticks(policy).evalMap(idx => agent.action("fib", _.notice).retry(IO(idx)).run).compile.drain)
         .compile
         .toList
         .unsafeRunSync()
@@ -106,7 +110,7 @@ class TicksTest extends AnyFunSuite {
     assert(h.isInstanceOf[ServiceStop])
   }
 
-  test("6.tick") {
+  test("6.tick ") {
     val policy = policies.cronBackoff[IO](Cron.unsafeParse("* * * ? * *"), ZoneId.systemDefault())
     val ticks  = awakeEvery(policy)
 
@@ -116,7 +120,6 @@ class TicksTest extends AnyFunSuite {
       .merge(ticks)
       .merge(ticks)
       .evalMap(idx => IO.realTimeInstant.map((_, idx)))
-      .debug()
       .take(20)
       .fold(Map.empty[Int, List[Instant]]) { case (sum, (fd, idx)) =>
         sum.updatedWith(idx)(ls => Some(fd :: ls.sequence.flatten))
@@ -132,12 +135,30 @@ class TicksTest extends AnyFunSuite {
         }
 
         m.flatMap(_._2.headOption).toList.sorted.sliding(2).map { ls =>
-          val diff = Duration.between(ls(1),ls(0)).abs.toScala
-          assert(diff > 1.second && diff < 1.1.seconds)
+          val diff = Duration.between(ls(1), ls(0)).abs.toScala
+          assert(diff > 0.9.second && diff < 1.1.seconds)
         }
       }
       .compile
       .drain
       .unsafeRunSync()
+  }
+
+  test("6.process longer than 1 second") {
+    val policy = policies.cronBackoff[IO](Cron.unsafeParse("* * * ? * *"), ZoneId.systemDefault())
+    val ticks  = awakeEvery(policy)
+
+    val fds: List[FiniteDuration] =
+      ticks.evalMap(_ => IO.sleep(1.5.seconds) >> IO.monotonic).take(5).compile.toList.unsafeRunSync()
+
+    fds
+      .sliding(2)
+      .map {
+        case List(a, b) =>
+          val diff = b - a
+          assert(diff > 1.9.seconds && diff < 2.1.seconds)
+        case _ => throw new Exception("not happen")
+      }
+      .toList
   }
 }
