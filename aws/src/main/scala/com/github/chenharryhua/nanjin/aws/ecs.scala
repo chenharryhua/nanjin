b44ba070ec7de1bba7db09266ba4fa6fa181a41d
Harry Chen <chen.harry.hua@gmail.com>
2022-11-14 22:14:20 +1100

    ecs metadate

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/ecs.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/ecs.scala
@@ -4,13 +4,25 @@ import cats.syntax.all.*
 import io.circe.Json
 import org.http4s.circe.*
 import org.http4s.ember.client.EmberClientBuilder
+import org.http4s.Uri
 
 object ecs {
 
-  def container_metadata[F[_]: Async]: F[Json] = sys.env
-    .get("ECS_CONTAINER_METADATA_URI_V4")
-    .orElse(sys.env.get("ECS_CONTAINER_METADATA_URI"))
+  private val meta_uri: Option[Uri] =
+    sys.env
+      .get("ECS_CONTAINER_METADATA_URI_V4")
+      .orElse(sys.env.get("ECS_CONTAINER_METADATA_URI"))
+      .map(Uri.fromString)
+      .flatMap(_.toOption)
+
+  // https://docs.aws.amazon.com/AmazonECS/latest/developerguide/task-metadata-endpoint-v4.html
+  def container_metadata[F[_]: Async]: F[Json] = meta_uri
     .flatTraverse(addr =>
       EmberClientBuilder.default[F].build.use(_.expect[Json](addr).attempt.map(_.toOption)))
+    .map(_.fold(Json.Null)(identity))
+
+  def container_metadata_task[F[_]: Async]: F[Json] = meta_uri
+    .flatTraverse(addr =>
+      EmberClientBuilder.default[F].build.use(_.expect[Json](addr / "task").attempt.map(_.toOption)))
     .map(_.fold(Json.Null)(identity))
 }
