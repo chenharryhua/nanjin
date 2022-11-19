c269feabb6458f6c952a17fc6e9ad1455fa0031a
Harry Chen <chen.harry.hua@gmail.com>
2022-11-19 13:08:25 +1100

    trivial

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/NJLocker.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/NJLocker.scala
@@ -10,10 +10,8 @@ final class NJLocker[F[_]: Functor, A](vault: AtomicCell[F, Vault], key: Key[A],
   def put(value: A): F[Unit] = vault.update(_.insert(key, value))
   def update(f: Endo[A]): F[Unit] = vault.update { vt =>
     vt.lookup(key) match {
-      case Some(value) =>
-        vt.insert(key, f(value))
-      case None =>
-        vt.insert(key, f(initValue))
+      case Some(value) => vt.insert(key, f(value))
+      case None        => vt.insert(key, f(initValue))
     }
   }
   def reset: F[Unit] = vault.update(_.delete(key))
