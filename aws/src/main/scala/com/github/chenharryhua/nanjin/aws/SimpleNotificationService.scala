8a966727de971ec955b15853be100eee69c3d1e4
Harry Chen <chen.harry.hua@gmail.com>
2023-03-18 18:03:01 +1100

    sns

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SimpleNotificationService.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SimpleNotificationService.scala
@@ -3,14 +3,14 @@ package com.github.chenharryhua.nanjin.aws
 import cats.effect.kernel.{Resource, Sync}
 import cats.syntax.all.*
 import cats.Endo
-import com.amazonaws.services.sns.{AmazonSNS, AmazonSNSClientBuilder}
-import com.amazonaws.services.sns.model.{PublishRequest, PublishResult}
 import org.typelevel.log4cats.{Logger, SelfAwareStructuredLogger}
 import org.typelevel.log4cats.slf4j.Slf4jLogger
+import software.amazon.awssdk.services.sns.{SnsClient, SnsClientBuilder}
+import software.amazon.awssdk.services.sns.model.{PublishRequest, PublishResponse}
 
 sealed trait SimpleNotificationService[F[_]] {
-  def publish(request: PublishRequest): F[PublishResult]
-  def updateBuilder(f: Endo[AmazonSNSClientBuilder]): SimpleNotificationService[F]
+  def publish(request: PublishRequest): F[PublishResponse]
+  def updateBuilder(f: Endo[SnsClientBuilder]): SimpleNotificationService[F]
 }
 
 object SimpleNotificationService {
@@ -20,15 +20,20 @@ object SimpleNotificationService {
   def fake[F[_]](implicit F: Sync[F]): Resource[F, SimpleNotificationService[F]] = {
     val logger: SelfAwareStructuredLogger[F] = Slf4jLogger.getLogger[F]
     Resource.make(F.pure(new SimpleNotificationService[F] {
-      override def publish(request: PublishRequest): F[PublishResult] =
-        logger.info(request.toString) *> F.pure(new PublishResult)
+      override def publish(request: PublishRequest): F[PublishResponse] =
+        logger.info(request.toString) *> F.pure(
+          PublishResponse
+            .builder()
+            .messageId("fake.message.id")
+            .sequenceNumber("fake.sequence.number")
+            .build())
 
-      override def updateBuilder(f: Endo[AmazonSNSClientBuilder]): SimpleNotificationService[F] =
+      override def updateBuilder(f: Endo[SnsClientBuilder]): SimpleNotificationService[F] =
         this
     }))(_ => F.unit)
   }
 
-  def apply[F[_]: Sync](f: Endo[AmazonSNSClientBuilder]): Resource[F, SimpleNotificationService[F]] =
+  def apply[F[_]: Sync](f: Endo[SnsClientBuilder]): Resource[F, SimpleNotificationService[F]] =
     for {
       logger <- Resource.eval(Slf4jLogger.create[F])
       nr <- Resource.makeCase(logger.info(s"initialize $name").map(_ => new AwsSNS[F](f, logger))) {
@@ -36,18 +41,17 @@ object SimpleNotificationService {
       }
     } yield nr
 
-  final private class AwsSNS[F[_]](buildFrom: Endo[AmazonSNSClientBuilder], logger: Logger[F])(implicit
-    F: Sync[F])
+  final private class AwsSNS[F[_]](buildFrom: Endo[SnsClientBuilder], logger: Logger[F])(implicit F: Sync[F])
       extends ShutdownService[F] with SimpleNotificationService[F] {
 
-    private lazy val client: AmazonSNS = buildFrom(AmazonSNSClientBuilder.standard()).build()
+    private lazy val client = buildFrom(SnsClient.builder).build()
 
-    override def publish(request: PublishRequest): F[PublishResult] =
+    override def publish(request: PublishRequest): F[PublishResponse] =
       F.blocking(client.publish(request)).onError(ex => logger.error(ex)(request.toString))
 
-    override protected val closeService: F[Unit] = F.blocking(client.shutdown())
+    override protected val closeService: F[Unit] = F.blocking(client.close())
 
-    override def updateBuilder(f: Endo[AmazonSNSClientBuilder]): SimpleNotificationService[F] =
+    override def updateBuilder(f: Endo[SnsClientBuilder]): SimpleNotificationService[F] =
       new AwsSNS[F](buildFrom.andThen(f), logger)
   }
 }
