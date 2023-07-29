65356bdf36b9ba84943a185df97613effd356bc2
Harry Chen <chen.harry.hua@gmail.com>
2023-07-29 11:08:14 +1000

    path

--- pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/NJFileKind.scala
+++ pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/NJFileKind.scala
@@ -4,23 +4,25 @@ import com.github.chenharryhua.nanjin.common.time.Tick
 import io.circe.generic.JsonCodec
 
 @JsonCodec
-sealed abstract class NJFileKind(val format: NJFileFormat, compression: NJCompression) {
-  final val fileName: String           = compression.fileName(format)
+sealed abstract class NJFileKind(val fileFormat: NJFileFormat, val compression: NJCompression) {
+  final val fileName: String           = compression.fileName(fileFormat)
   final def rotate(tick: Tick): String = f"${tick.index}%09d.$fileName"
 }
 
-final case class AvroFile(compression: AvroCompression) extends NJFileKind(NJFileFormat.Avro, compression)
+final case class AvroFile(override val compression: AvroCompression)
+    extends NJFileKind(NJFileFormat.Avro, compression)
 
-final case class CirceFile(compression: CirceCompression) extends NJFileKind(NJFileFormat.Circe, compression)
+final case class CirceFile(override val compression: CirceCompression)
+    extends NJFileKind(NJFileFormat.Circe, compression)
 
-final case class BinAvroFile(compression: BinaryAvroCompression)
+final case class BinAvroFile(override val compression: BinaryAvroCompression)
     extends NJFileKind(NJFileFormat.BinaryAvro, compression)
 
-final case class JacksonFile(compression: JacksonCompression)
+final case class JacksonFile(override val compression: JacksonCompression)
     extends NJFileKind(NJFileFormat.Jackson, compression)
 
-final case class KantanFile(compression: KantanCompression)
+final case class KantanFile(override val compression: KantanCompression)
     extends NJFileKind(NJFileFormat.Kantan, compression)
 
-final case class ParquetFile(compression: ParquetCompression)
+final case class ParquetFile(override val compression: ParquetCompression)
     extends NJFileKind(NJFileFormat.Parquet, compression)
