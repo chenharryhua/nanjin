f5fcb364f3dc5f81611f84d7fc7470538dbffb29
Harry Chen <chen.harry.hua@gmail.com>
2023-08-01 23:59:16 +1000

    tests

--- datetime/src/test/scala/mtest/AwakeOnPolicyTest.scala
+++ datetime/src/test/scala/mtest/AwakeOnPolicyTest.scala
@@ -10,7 +10,7 @@ import org.scalatest.funsuite.AnyFunSuite
 import java.time.Duration as JavaDuration
 import java.time.temporal.ChronoUnit
 import scala.concurrent.duration.*
-import scala.jdk.DurationConverters.JavaDurationOps
+import scala.jdk.DurationConverters.{JavaDurationOps, ScalaDurationOps}
 
 class AwakeOnPolicyTest extends AnyFunSuite {
   test("1.tick") {
@@ -47,7 +47,7 @@ class AwakeOnPolicyTest extends AnyFunSuite {
     }
   }
 
-  test("4.ticks - cron") {
+  test("4.ticks - session id should not be same") {
     val policy = policies
       .limitRetriesByCumulativeDelay[IO](5.seconds, policies.cronBackoff[IO](crontabs.secondly, mumbaiTime))
     val ticks = awakeOnPolicy(policy).compile.toList.unsafeRunSync()
@@ -64,7 +64,7 @@ class AwakeOnPolicyTest extends AnyFunSuite {
     assert(res.isEmpty)
   }
 
-  test("6.jitters") {
+  test("6.cron") {
     val policy = policies.cronBackoff[IO](crontabs.secondly, berlinTime)
     val ticks  = awakeOnPolicy(policy)
     val sleep: IO[JavaDuration] =
@@ -89,12 +89,90 @@ class AwakeOnPolicyTest extends AnyFunSuite {
         assert(t1.sessionId === t2.sessionId)
         assert(t2.previous === t1.wakeTime)
         assert(t2.previous.plusNanos(t2.status.previousDelay.get.toNanos) === t2.wakeTime)
-        JavaDuration.between(t2.previous.plus(l1).plus(t2.snooze), t2.wakeTime).toNanos
+        val j = JavaDuration.between(t2.previous.plus(l1).plus(t2.snooze), t2.wakeTime).toNanos
+        assert(j > 0)
+        t1
       }
       .take(10)
       .compile
       .toList
       .unsafeRunSync()
-    assert(jitters.forall(_ > 0))
+    val t1 = jitters.head
+    val t5 = jitters(5)
+    val t9 = jitters(9)
+    assert(t1.previous.plus(t5.status.cumulativeDelay.toJava) == t5.wakeTime)
+    assert(t1.previous.plus(t9.status.cumulativeDelay.toJava) == t9.wakeTime)
+  }
+
+  test("7.constant") {
+    val policy = policies.constantDelay[IO](1.second)
+    val ticks  = awakeOnPolicy(policy)
+    val sleep: IO[JavaDuration] =
+      Random
+        .scalaUtilRandom[IO]
+        .flatMap(_.betweenLong(0, 500))
+        .flatMap(d => IO.sleep(d.millisecond).as(JavaDuration.ofMillis(d)))
+
+    val jitters = ticks
+      .evalMap(t => sleep.map((t, _)))
+      .sliding(2)
+      .map { ck =>
+        val (t1, l1) = ck(0)
+        val (t2, _)  = ck(1)
+        assert(t2.interval === JavaDuration.between(t1.wakeTime, t2.wakeTime))
+        assert(t2.interval.toScala === t2.status.previousDelay.get)
+        assert(t2.snooze.toScala < t2.interval.toScala)
+        assert(t1.sessionId === t2.sessionId)
+        assert(t2.previous === t1.wakeTime)
+        assert(t2.previous.plusNanos(t2.status.previousDelay.get.toNanos) === t2.wakeTime)
+        val j = JavaDuration.between(t2.previous.plus(l1).plus(t2.snooze), t2.wakeTime).toNanos
+        assert(j > 0)
+        t1
+      }
+      .take(10)
+      .compile
+      .toList
+      .unsafeRunSync()
+    val t1 = jitters.head
+    val t5 = jitters(5)
+    val t9 = jitters(9)
+    assert(t1.previous.plus(t5.status.cumulativeDelay.toJava) == t5.wakeTime)
+    assert(t1.previous.plus(t9.status.cumulativeDelay.toJava) == t9.wakeTime)
+  }
+
+  test("8.jitter") {
+    val policy = policies.jitterBackoff[IO](0.second, 1.seconds)
+    val ticks  = awakeOnPolicy(policy)
+    val sleep: IO[JavaDuration] =
+      Random
+        .scalaUtilRandom[IO]
+        .flatMap(_.betweenLong(0, 500))
+        .flatMap(d => IO.sleep(d.millisecond).as(JavaDuration.ofMillis(d)))
+
+    val jitters = ticks
+      .evalMap(t => sleep.map((t, _)))
+      .sliding(2)
+      .map { ck =>
+        val (t1, l1) = ck(0)
+        val (t2, _)  = ck(1)
+        assert(t2.interval === JavaDuration.between(t1.wakeTime, t2.wakeTime))
+        assert(t2.interval.toScala === t2.status.previousDelay.get)
+        assert(t2.snooze.toScala < t2.interval.toScala)
+        assert(t1.sessionId === t2.sessionId)
+        assert(t2.previous === t1.wakeTime)
+        assert(t2.previous.plusNanos(t2.status.previousDelay.get.toNanos) === t2.wakeTime)
+        val j = JavaDuration.between(t2.previous.plus(l1).plus(t2.snooze), t2.wakeTime).toNanos
+        assert(j > 0)
+        t1
+      }
+      .take(10)
+      .compile
+      .toList
+      .unsafeRunSync()
+    val t1 = jitters.head
+    val t5 = jitters(5)
+    val t9 = jitters(9)
+    assert(t1.previous.plus(t5.status.cumulativeDelay.toJava) == t5.wakeTime)
+    assert(t1.previous.plus(t9.status.cumulativeDelay.toJava) == t9.wakeTime)
   }
 }
