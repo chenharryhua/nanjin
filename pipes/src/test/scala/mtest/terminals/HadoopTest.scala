8ef5a3754abbcecb709b908ca1d9f505414adbc6
Harry Chen <chen.harry.hua@gmail.com>
2023-08-29 00:11:29 +1000

    tests

--- pipes/src/test/scala/mtest/terminals/HadoopTest.scala
+++ pipes/src/test/scala/mtest/terminals/HadoopTest.scala
@@ -11,13 +11,16 @@ import java.time.LocalDateTime
 
 class HadoopTest extends AnyFunSuite {
   private val path: NJPath = NJPath("./data/test/terminals/hadoop")
-  private val p1           = path / LocalDateTime.of(2023, 8, 28, 2, 0, 0) / "a.txt"
-  private val p2           = path / LocalDateTime.of(2023, 8, 29, 1, 0, 0) / "b.txt"
-  private val p3           = path / LocalDateTime.of(2023, 8, 30, 0, 0, 0) / "c.txt"
+
+  private val p1 = path / LocalDateTime.of(2023, 8, 28, 2, 0, 0) / "a.txt"
+  private val p2 = path / LocalDateTime.of(2023, 8, 29, 1, 0, 0) / "b.txt"
+  private val p3 = path / LocalDateTime.of(2023, 8, 30, 0, 0, 0) / "c.txt"
+  private val p4 = path / LocalDateTime.of(2023, 8, 30, 1, 0, 0) / "d.txt"
 
   File(p1.pathStr).createFileIfNotExists(createParents = true)
   File(p2.pathStr).createFileIfNotExists(createParents = true)
   File(p3.pathStr).createFileIfNotExists(createParents = true)
+  File(p4.pathStr).createFileIfNotExists(createParents = true)
 
   test("ymd") {
     assert(hdp.earliestYmd(path).unsafeRunSync().get.pathStr.takeRight(25) === "Year=2023/Month=08/Day=28")
@@ -32,10 +35,10 @@ class HadoopTest extends AnyFunSuite {
         .pathStr
         .takeRight(33) === "Year=2023/Month=08/Day=28/Hour=02")
     assert(
-      hdp.latestYmdh(path).unsafeRunSync().get.pathStr.takeRight(33) === "Year=2023/Month=08/Day=30/Hour=00")
+      hdp.latestYmdh(path).unsafeRunSync().get.pathStr.takeRight(33) === "Year=2023/Month=08/Day=30/Hour=01")
   }
   test("exist") {
     assert(hdp.isExist(p1).unsafeRunSync())
-    assert(hdp.locatedFileStatus(path).unsafeRunSync().count(_.isFile) == 3)
+    assert(hdp.locatedFileStatus(path).unsafeRunSync().count(_.isFile) == 4)
   }
 }
