5ce0669567f3804b454214b3149c77ee2013e813
Harry Chen <chen.harry.hua@gmail.com>
2023-08-29 08:12:51 +1000

    condition alert

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/NJAlert.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/NJAlert.scala
@@ -51,19 +51,28 @@ final class NJAlert[F[_]: Monad: Clock] private[guard] (
 
   def error[S: Encoder](msg: S): F[Unit] =
     alert(msg.asJson, AlertLevel.Error).map(_ => if (isCounting) errorCounter.inc(1))
-  def error[S: Encoder](msg: Option[S]): F[Unit]    = msg.traverse(error(_)).void
-  def unsafeError[S: Encoder](msg: S): Unit         = dispatcher.unsafeRunSync(error(msg))
-  def unsafeError[S: Encoder](msg: Option[S]): Unit = dispatcher.unsafeRunSync(error(msg))
+  def error[S: Encoder](msg: S, ifTrue: Boolean): F[Unit] = if (ifTrue) error(msg) else Monad[F].unit
+  def error[S: Encoder](msg: Option[S]): F[Unit]          = msg.traverse(error(_)).void
+
+  def unsafeError[S: Encoder](msg: S): Unit                  = dispatcher.unsafeRunSync(error(msg))
+  def unsafeError[S: Encoder](msg: S, ifTrue: Boolean): Unit = if (ifTrue) unsafeError(msg) else ()
+  def unsafeError[S: Encoder](msg: Option[S]): Unit          = dispatcher.unsafeRunSync(error(msg))
 
   def warn[S: Encoder](msg: S): F[Unit] =
     alert(msg.asJson, AlertLevel.Warn).map(_ => if (isCounting) warnCounter.inc(1))
-  def warn[S: Encoder](msg: Option[S]): F[Unit]    = msg.traverse(warn(_)).void
-  def unsafeWarn[S: Encoder](msg: S): Unit         = dispatcher.unsafeRunSync(warn(msg))
-  def unsafeWarn[S: Encoder](msg: Option[S]): Unit = dispatcher.unsafeRunSync(warn(msg))
+  def warn[S: Encoder](msg: S, ifTrue: Boolean): F[Unit] = if (ifTrue) warn(msg) else Monad[F].unit
+  def warn[S: Encoder](msg: Option[S]): F[Unit]          = msg.traverse(warn(_)).void
+
+  def unsafeWarn[S: Encoder](msg: S): Unit                  = dispatcher.unsafeRunSync(warn(msg))
+  def unsafeWarn[S: Encoder](msg: S, ifTrue: Boolean): Unit = if (ifTrue) unsafeWarn(msg) else ()
+  def unsafeWarn[S: Encoder](msg: Option[S]): Unit          = dispatcher.unsafeRunSync(warn(msg))
 
   def info[S: Encoder](msg: S): F[Unit] =
     alert(msg.asJson, AlertLevel.Info).map(_ => if (isCounting) infoCounter.inc(1))
-  def info[S: Encoder](msg: Option[S]): F[Unit]    = msg.traverse(info(_)).void
+  def info[S: Encoder](msg: S, ifTrue: Boolean): F[Unit] = if (ifTrue) info(msg) else Monad[F].unit
+  def info[S: Encoder](msg: Option[S]): F[Unit]          = msg.traverse(info(_)).void
+
   def unsafeInfo[S: Encoder](msg: S): Unit         = dispatcher.unsafeRunSync(info(msg))
+  def unsafeInfo[S: Encoder](msg: S, ifTrue: Boolean): Unit = if (ifTrue) unsafeInfo(msg) else ()
   def unsafeInfo[S: Encoder](msg: Option[S]): Unit = dispatcher.unsafeRunSync(info(msg))
 }
