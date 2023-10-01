bf8e06f58cd31b4c7881a7f1e19fd69576ab22c1
Harry Chen <chen.harry.hua@gmail.com>
2023-10-01 18:55:11 +1100

    publish

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/service/publisher.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/service/publisher.scala
@@ -1,8 +1,8 @@
 package com.github.chenharryhua.nanjin.guard.service
 
-import cats.MonadError
 import cats.effect.kernel.Clock
 import cats.syntax.all.*
+import cats.{Functor, Monad}
 import com.codahale.metrics.MetricRegistry
 import com.github.chenharryhua.nanjin.common.chrono.Tick
 import com.github.chenharryhua.nanjin.guard.config.ServiceParams
@@ -25,56 +25,48 @@ private object publisher {
     serviceParams: ServiceParams,
     metricRegistry: MetricRegistry,
     index: MetricIndex,
-    ts: Instant)(implicit F: MonadError[F, Throwable]): F[Unit] =
-    channel
-      .send(
-        MetricReport(
-          index = index,
-          serviceParams = serviceParams,
-          timestamp = serviceParams.toZonedDateTime(ts),
-          snapshot = MetricSnapshot(metricRegistry)))
-      .void
+    ts: Instant)(implicit F: Functor[F]): F[Unit] = {
+    val msg = MetricReport(
+      index = index,
+      serviceParams = serviceParams,
+      timestamp = serviceParams.toZonedDateTime(ts),
+      snapshot = MetricSnapshot(metricRegistry))
+
+    channel.send(msg).void
+  }
 
   def metricReset[F[_]](
     channel: Channel[F, NJEvent],
     serviceParams: ServiceParams,
     metricRegistry: MetricRegistry,
     index: MetricIndex,
-    ts: Instant)(implicit F: MonadError[F, Throwable]): F[Unit] =
-    channel
-      .send(
-        MetricReset(
-          index = index,
-          serviceParams = serviceParams,
-          timestamp = serviceParams.toZonedDateTime(ts),
-          snapshot = MetricSnapshot(metricRegistry)
-        ))
-      .map(_ => metricRegistry.getCounters().values().asScala.foreach(c => c.dec(c.getCount)))
+    ts: Instant)(implicit F: Functor[F]): F[Unit] = {
+    val msg = MetricReset(
+      index = index,
+      serviceParams = serviceParams,
+      timestamp = serviceParams.toZonedDateTime(ts),
+      snapshot = MetricSnapshot(metricRegistry))
+
+    channel.send(msg).map(_ => metricRegistry.getCounters().values().asScala.foreach(c => c.dec(c.getCount)))
+  }
 
   def serviceReStart[F[_]](channel: Channel[F, NJEvent], serviceParams: ServiceParams, tick: Tick)(implicit
-    F: MonadError[F, Throwable]): F[Unit] =
-    channel
-      .send(ServiceStart(serviceParams, tick))
-      .map(_.leftMap(_ => new Exception("service restart channel closed")))
-      .rethrow
+    F: Functor[F]): F[Unit] =
+    channel.send(ServiceStart(serviceParams, tick)).void
 
   def servicePanic[F[_]](
     channel: Channel[F, NJEvent],
     serviceParams: ServiceParams,
     tick: Tick,
-    ex: Throwable)(implicit F: MonadError[F, Throwable]): F[Unit] =
-    channel
-      .send(ServicePanic(serviceParams, NJError(ex), tick))
-      .map(_.leftMap(_ => new Exception("service panic channel closed")))
-      .rethrow
+    ex: Throwable)(implicit F: Functor[F]): F[Unit] =
+    channel.send(ServicePanic(serviceParams, NJError(ex), tick)).void
 
   def serviceStop[F[_]: Clock](
     channel: Channel[F, NJEvent],
     serviceParams: ServiceParams,
-    cause: ServiceStopCause)(implicit F: MonadError[F, Throwable]): F[Unit] =
-    serviceParams.zonedNow.flatMap(now =>
-      channel
-        .closeWithElement(ServiceStop(serviceParams = serviceParams, timestamp = now, cause = cause))
-        .void)
+    cause: ServiceStopCause)(implicit F: Monad[F]): F[Unit] =
+    serviceParams.zonedNow.flatMap { now =>
+      channel.closeWithElement(ServiceStop(serviceParams, now, cause)).void
+    }
 
 }
