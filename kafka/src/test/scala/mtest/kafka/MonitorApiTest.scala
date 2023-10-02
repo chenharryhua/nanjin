fe2903598b6c34eb98e499d35e56bfbcc936623e
Harry Chen <chen.harry.hua@gmail.com>
2023-10-03 00:25:32 +1100

    monitor

--- kafka/src/test/scala/mtest/kafka/MonitorApiTest.scala
+++ kafka/src/test/scala/mtest/kafka/MonitorApiTest.scala
@@ -19,9 +19,6 @@ class MonitorApiTest extends AnyFunSuite {
   val headers1: Headers = Headers.fromSeq(List(Header("a", "aaaaa")))
   val headers2: Headers = Headers.fromSeq(List(Header("b", ""), Header("warn", "value is null as expected")))
 
-  (ctx.admin(topic.topicName).iDefinitelyWantToDeleteTheTopicAndUnderstoodItsConsequence >> ctx.schemaRegistry
-    .register(topic.topicDef)).unsafeRunSync()
-
   val sender: Stream[IO, ProducerResult[Int, Array[Byte]]] = Stream
     .emits(List(
       ProducerRecord[Int, Array[Byte]](st.topicName.value, 0, Array(0, 0, 0, 1)).withHeaders(headers1),
