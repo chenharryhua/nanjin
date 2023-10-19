114750c77656cf3f02c46bfef741d6918566e57d
Harry Chen <chen.harry.hua@gmail.com>
2023-10-19 21:13:05 +1100

    cookie

--- http/src/main/scala/com/github/chenharryhua/nanjin/http/client/middleware/cookieBox.scala
+++ http/src/main/scala/com/github/chenharryhua/nanjin/http/client/middleware/cookieBox.scala
@@ -10,26 +10,24 @@ import java.net.{CookieManager, CookieStore, HttpCookie, URI}
 import scala.jdk.CollectionConverters.*
 
 object cookieBox {
-  def apply[F[_]](cookieManager: CookieManager)(implicit F: Sync[F]): Endo[Client[F]] = (client: Client[F]) =>
-    {
-      val cookieStore: CookieStore = cookieManager.getCookieStore
-      Client[F] { req =>
-        for {
-          cookies <- Resource.eval(
-            F.delay(
-              cookieStore
-                .get(URI.create(req.uri.renderString))
-                .asScala
-                .toList
-                .map(hc => RequestCookie(hc.getName, hc.getValue))))
-          out <- client.run(cookies.foldLeft(req) { case (r, c) => r.addCookie(c) })
-        } yield {
-          out.headers.headers
-            .filter(_.name === `Set-Cookie`.name)
-            .flatMap(c => HttpCookie.parse(c.value).asScala)
-            .foreach(hc => cookieStore.add(URI.create(req.uri.renderString), hc))
-          out
-        }
+  def apply[F[_]: Sync](cookieManager: CookieManager): Endo[Client[F]] = (client: Client[F]) => {
+    val cookieStore: CookieStore = cookieManager.getCookieStore
+    Client[F] { req =>
+      for {
+        cookies <- Resource.pure(
+          cookieStore
+            .get(URI.create(req.uri.renderString))
+            .asScala
+            .toList
+            .map(hc => RequestCookie(hc.getName, hc.getValue)))
+        out <- client.run(cookies.foldLeft(req) { case (r, c) => r.addCookie(c) })
+      } yield {
+        out.headers.headers
+          .filter(_.name === `Set-Cookie`.name)
+          .flatMap(c => HttpCookie.parse(c.value).asScala)
+          .foreach(hc => cookieStore.add(URI.create(req.uri.renderString), hc))
+        out
       }
     }
+  }
 }
