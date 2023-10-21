96e05fad01297ea43e214e20a0c4ac1d4ba0e3c2
Harry Chen <chen.harry.hua@gmail.com>
2023-10-21 14:39:43 +1100

    kafka observer

--- observers/kafka/src/main/scala/com/github/chenharryhua/nanjin/guard/observers/KafkaObserver.scala
+++ observers/kafka/src/main/scala/com/github/chenharryhua/nanjin/guard/observers/KafkaObserver.scala
@@ -1,11 +1,11 @@
 package com.github.chenharryhua.nanjin.guard.observers
 
 import cats.effect.kernel.Async
-import com.github.chenharryhua.nanjin.common.kafka.TopicName
+import com.github.chenharryhua.nanjin.common.kafka.{TopicName, TopicNameL}
 import com.github.chenharryhua.nanjin.guard.event.NJEvent
 import com.github.chenharryhua.nanjin.kafka.KafkaContext
 import com.github.chenharryhua.nanjin.messages.kafka.codec.KJson
-import fs2.kafka.ProducerRecord
+import fs2.kafka.{ProducerRecord, ProducerResult}
 import fs2.{Pipe, Stream}
 
 object KafkaObserver {
@@ -14,10 +14,13 @@ object KafkaObserver {
 
 final class KafkaObserver[F[_]: Async](ctx: KafkaContext[F]) {
 
-  def observe(topicName: TopicName): Pipe[F, NJEvent, Nothing] = { (ss: Stream[F, NJEvent]) =>
-    ss.map(evt => ProducerRecord(topicName.value, evt.serviceParams.taskParams.taskName, KJson(evt)))
-      .chunks
-      .through(ctx.topic[String, KJson[NJEvent]](topicName).produce.pipe)
-      .drain
+  def observe(topicName: TopicName): Pipe[F, NJEvent, ProducerResult[String, KJson[NJEvent]]] = {
+    (ss: Stream[F, NJEvent]) =>
+      ss.map(evt => ProducerRecord(topicName.value, evt.serviceParams.taskParams.taskName, KJson(evt)))
+        .chunks
+        .through(ctx.topic[String, KJson[NJEvent]](topicName).produce.pipe)
   }
+
+  def observe(topicName: TopicNameL): Pipe[F, NJEvent, ProducerResult[String, KJson[NJEvent]]] =
+    observe(TopicName(topicName))
 }
---
+++ observers/kafka/src/test/scala/mtest/kafka/KafkaObserverTest.scala
@@ -0,0 +1,26 @@
+package mtest.kafka
+
+import cats.effect.IO
+import cats.effect.unsafe.implicits.global
+import com.github.chenharryhua.nanjin.common.kafka.TopicName
+import com.github.chenharryhua.nanjin.guard.TaskGuard
+import com.github.chenharryhua.nanjin.guard.observers.KafkaObserver
+import com.github.chenharryhua.nanjin.kafka.KafkaSettings
+import org.scalatest.funsuite.AnyFunSuite
+import eu.timepit.refined.auto.*
+
+class KafkaObserverTest extends AnyFunSuite {
+  val topic = TopicName("observer")
+  val ctx   = KafkaSettings.local.ioContext
+  test("observer") {
+    TaskGuard[IO]("observer")
+      .service("observer")
+      .eventStream(_.action("observer", _.bipartite).retry(IO(())).run)
+      .through(KafkaObserver(ctx).observe(topic))
+      .debug()
+      .compile
+      .drain
+      .unsafeRunSync()
+
+  }
+}
