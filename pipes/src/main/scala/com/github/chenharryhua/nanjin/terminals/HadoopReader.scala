bcca6505372d1fe53abe90f04758ffa50b7ff021
Harry Chen <harry.chen@tabcorp.com.au>
2024-03-13 00:08:41 +1100

    terminal

--- pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/HadoopReader.scala
+++ pipes/src/main/scala/com/github/chenharryhua/nanjin/terminals/HadoopReader.scala
@@ -3,6 +3,7 @@ package com.github.chenharryhua.nanjin.terminals
 import cats.data.Reader
 import cats.effect.Resource
 import cats.effect.kernel.Sync
+import cats.implicits.{catsSyntaxApplicativeError, catsSyntaxMonadErrorRethrow, toBifunctorOps, toFunctorOps}
 import fs2.Stream
 import fs2.io.readInputStream
 import org.apache.avro.Schema
@@ -26,13 +27,22 @@ private object HadoopReader {
     for {
       is <- Resource.make(F.blocking(HadoopInputFile.fromPath(path, configuration).newStream()))(r =>
         F.blocking(r.close()))
-      dfs <- Resource.make[F, DataFileStream[GenericData.Record]](
-        F.blocking(new DataFileStream(is, new GenericDatumReader(schema))))(r => F.blocking(r.close()))
+      dfs <- Resource.make[F, DataFileStream[GenericData.Record]] {
+        F.blocking[DataFileStream[GenericData.Record]](new DataFileStream(is, new GenericDatumReader(schema)))
+          .attempt
+          .map(_.leftMap(err => new Exception(path.toString, err)))
+          .rethrow
+      }(r => F.blocking(r.close()))
     } yield dfs
 
   def parquetR[F[_]](readBuilder: Reader[Path, ParquetReader.Builder[GenericData.Record]], path: Path)(
     implicit F: Sync[F]): Resource[F, ParquetReader[GenericData.Record]] =
-    Resource.make(F.blocking(readBuilder.run(path).build()))(r => F.blocking(r.close()))
+    Resource.make {
+      F.blocking[ParquetReader[GenericData.Record]](readBuilder.run(path).build())
+        .attempt
+        .map(_.leftMap(err => new Exception(path.toString, err)))
+        .rethrow
+    }(r => F.blocking(r.close()))
 
   private def fileInputStream(path: Path, configuration: Configuration): InputStream = {
     val is: InputStream = HadoopInputFile.fromPath(path, configuration).newStream()
@@ -65,7 +75,7 @@ private object HadoopReader {
         case Failure(exception) =>
           exception match {
             case _: java.io.EOFException => None
-            case err                     => throw new Exception(path.getName, err)
+            case err                     => throw new Exception(path.toString, err)
           }
         case Success(value) => Some(value)
       }
@@ -84,7 +94,7 @@ private object HadoopReader {
         case Failure(exception) =>
           exception match {
             case _: java.io.EOFException => None
-            case err                     => throw new Exception(path.getName, err)
+            case err                     => throw new Exception(path.toString, err)
           }
         case Success(value) => Some(value)
       }
