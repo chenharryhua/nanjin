120658b95843a81d401af5fcfcbf0a608293a5b8
Harry Chen <harry.chen@tabcorp.com.au>
2024-10-19 14:47:53 +1100

    sqs

--- common/src/main/scala/com/github/chenharryhua/nanjin/common/aws.scala
+++ common/src/main/scala/com/github/chenharryhua/nanjin/common/aws.scala
@@ -2,10 +2,7 @@ package com.github.chenharryhua.nanjin.common
 
 import cats.data.NonEmptyList
 import eu.timepit.refined.api.{Refined, RefinedTypeOps}
-import eu.timepit.refined.auto.*
 import eu.timepit.refined.cats.*
-import eu.timepit.refined.collection.{MaxSize, NonEmpty}
-import eu.timepit.refined.numeric
 import eu.timepit.refined.predicates.all.{And, Not}
 import eu.timepit.refined.string.{EndsWith, MatchesRegex, Url}
 import io.circe.generic.JsonCodec
@@ -14,22 +11,26 @@ import io.lemonlabs.uri.Url as SUrl
 
 object aws {
   type IamArn = String Refined MatchesRegex["^arn:(aws[a-zA-Z-]*)?:iam::\\d{12}:role/[A-Za-z0-9-]+$"]
+
   object IamArn extends RefinedTypeOps[IamArn, String] with CatsRefinedTypeOpsSyntax
 
   type SnsArn = String Refined MatchesRegex["^arn:(aws[a-zA-Z-]*)?:sns:[A-Za-z0-9_-]+:\\d{12}:[A-Za-z0-9-]+$"]
+
   object SnsArn extends RefinedTypeOps[SnsArn, String] with CatsRefinedTypeOpsSyntax
 
   type KmsArn =
     String Refined MatchesRegex["^arn:(aws[a-zA-Z-]*)?:kms:[A-Za-z0-9-]+:\\d{12}:key/[A-Za-z0-9-]+$"]
+
   object KmsArn extends RefinedTypeOps[KmsArn, String] with CatsRefinedTypeOpsSyntax
 
   // https://docs.aws.amazon.com/AmazonCloudWatch/latest/monitoring/cloudwatch_concepts.html#Namespace
   type CloudWatchNamespace = String Refined MatchesRegex["""^[a-zA-Z0-9_.\-#:]+$"""]
+
   object CloudWatchNamespace extends RefinedTypeOps[CloudWatchNamespace, String] with CatsRefinedTypeOpsSyntax
 
   @JsonCodec
   final case class S3Path(bucket: String, key: String) {
-    val s3Url: SUrl = SUrl.parse(s"${S3Protocols.S3.entryName}://$bucket/$key")
+    val s3Url: SUrl = SUrl.parse(s"s3://$bucket/$key")
   }
 
   // https://docs.aws.amazon.com/systems-manager/latest/userguide/systems-manager-parameter-store.html
@@ -52,97 +53,11 @@ object aws {
 
   object SqsUrl {
     type Standard = String Refined And[Url, Not[EndsWith[".fifo"]]]
+
     object Standard extends RefinedTypeOps[Standard, String] with CatsRefinedTypeOpsSyntax
 
     type Fifo = String Refined And[Url, EndsWith[".fifo"]]
+
     object Fifo extends RefinedTypeOps[Fifo, String] with CatsRefinedTypeOpsSyntax
-  }
-
-  @JsonCodec
-  sealed trait SqsConfig {
-    def queueUrl: String
-
-    def maxNumberOfMessages: SqsConfig.MaxNumberOfMessages
-    def withMaxNumberOfMessages(num: SqsConfig.MaxNumberOfMessages): SqsConfig
-
-    def waitTimeSeconds: SqsConfig.WaitTimeSeconds
-    def withWaitTimeSeconds(seconds: SqsConfig.WaitTimeSeconds): SqsConfig
-
-    def visibilityTimeout: SqsConfig.VisibilityTimeout
-    def withVisibilityTimeout(seconds: SqsConfig.VisibilityTimeout): SqsConfig
-  }
-
-  object SqsConfig {
-    def apply(url: SqsUrl.Standard): Standard = Standard(url)
-    def apply(url: SqsUrl.Fifo): Fifo         = Fifo(url)
-
-    type MessageGroupID      = String Refined And[NonEmpty, MaxSize[128]]
-    type MaxNumberOfMessages = Int Refined numeric.Interval.Closed[1, 10]
-    type WaitTimeSeconds     = Int Refined numeric.Interval.Closed[0, 20]
-    type VisibilityTimeout   = Int Refined numeric.Interval.Closed[0, 43200] // 12 hours
-
-    private val defaultMaxNumberOfMessages: MaxNumberOfMessages = 10 // 10 messages in one fetch
-    private val defaultWaitTimeSeconds: WaitTimeSeconds         = 20 // 20 seconds
-    private val defaultVisibilityTimeout: VisibilityTimeout     = 30 // 30 seconds
-
-    final case class Standard(
-      value: SqsUrl.Standard,
-      maxNumberOfMessages: MaxNumberOfMessages,
-      waitTimeSeconds: WaitTimeSeconds,
-      visibilityTimeout: VisibilityTimeout)
-        extends SqsConfig {
-      override val queueUrl: String = value.value
-
-      override def withMaxNumberOfMessages(num: MaxNumberOfMessages): Standard =
-        copy(maxNumberOfMessages = num)
-
-      override def withWaitTimeSeconds(seconds: WaitTimeSeconds): Standard =
-        copy(waitTimeSeconds = seconds)
-
-      override def withVisibilityTimeout(seconds: VisibilityTimeout): Standard =
-        copy(visibilityTimeout = seconds)
-    }
-
-    object Standard {
-      def apply(standard: SqsUrl.Standard): Standard =
-        Standard(
-          value = standard,
-          maxNumberOfMessages = defaultMaxNumberOfMessages,
-          waitTimeSeconds = defaultWaitTimeSeconds,
-          visibilityTimeout = defaultVisibilityTimeout)
-    }
-
-    final case class Fifo(
-      value: SqsUrl.Fifo,
-      maxNumberOfMessages: MaxNumberOfMessages,
-      waitTimeSeconds: WaitTimeSeconds,
-      visibilityTimeout: VisibilityTimeout,
-      messageGroupId: MessageGroupID)
-        extends SqsConfig {
-      override val queueUrl: String = value.value
-
-      def withMessageGroupId(mgId: MessageGroupID): Fifo =
-        copy(messageGroupId = mgId)
-
-      override def withMaxNumberOfMessages(num: MaxNumberOfMessages): Fifo =
-        copy(maxNumberOfMessages = num)
-
-      override def withWaitTimeSeconds(seconds: WaitTimeSeconds): Fifo =
-        copy(waitTimeSeconds = seconds)
-
-      override def withVisibilityTimeout(seconds: VisibilityTimeout): Fifo =
-        copy(visibilityTimeout = seconds)
-    }
-
-    object Fifo {
-      def apply(fifo: SqsUrl.Fifo): Fifo =
-        Fifo(
-          value = fifo,
-          maxNumberOfMessages = defaultMaxNumberOfMessages,
-          waitTimeSeconds = defaultWaitTimeSeconds,
-          visibilityTimeout = defaultVisibilityTimeout,
-          messageGroupId = "nj.sqs.fifo.default.group"
-        )
-    }
   }
 }
