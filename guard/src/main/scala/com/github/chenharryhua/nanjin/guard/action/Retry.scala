94f4326ec813f5c6b8667803a56053d97d91ee38
Harry Chen <harry.chen@tabcorp.com.au>
2024-11-09 12:18:18 +1100

    retry

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/Retry.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/Retry.scala
@@ -1,5 +1,6 @@
 package com.github.chenharryhua.nanjin.guard.action
 
+import cats.data.Kleisli
 import cats.effect.Temporal
 import cats.implicits.{toFlatMapOps, toFunctorOps}
 import com.github.chenharryhua.nanjin.common.chrono.{Tick, TickStatus}
@@ -7,40 +8,45 @@ import com.github.chenharryhua.nanjin.common.chrono.{Tick, TickStatus}
 import scala.jdk.DurationConverters.JavaDurationOps
 
 sealed trait Retry[F[_]] {
-  def apply[A](f: Tick => F[A], worth: Throwable => Boolean): F[A]
+  def apply[A](arrow: (Tick, Option[Throwable]) => F[Either[Throwable, A]]): F[A]
 
-  final def apply[A](f: Tick => F[A]): F[A] =
-    apply(f, _ => true)
+  def apply[A](tfa: Tick => F[A]): F[A]
 
-  final def apply[A](fa: F[A], worth: Throwable => Boolean): F[A] =
-    apply((_: Tick) => fa, worth)
-
-  final def apply[A](fa: F[A]): F[A] =
-    apply((_: Tick) => fa, _ => true)
+  def apply[A](fa: F[A]): F[A]
 }
 
 object Retry {
 
-  private[guard] class Impl[F[_]](init: TickStatus)(implicit F: Temporal[F]) extends Retry[F] {
+  private[guard] class Impl[F[_]](initTS: TickStatus)(implicit F: Temporal[F]) extends Retry[F] {
 
-    override def apply[A](f: Tick => F[A], worth: Throwable => Boolean): F[A] =
-      F.tailRecM(init) { status =>
-        F.attempt(f(status.tick)).flatMap[Either[TickStatus, A]] {
-          case Right(a) => F.pure(Right(a))
-          case Left(ex) =>
-            if (worth(ex)) {
-              for {
-                next <- F.realTimeInstant.map(status.next)
-                tickStatus <- next match {
-                  case None => F.raiseError(ex) // run out of policy
-                  case Some(ts) => // sleep and continue
-                    F.sleep(ts.tick.snooze.toScala).as(Left(ts))
-                }
-              } yield tickStatus
-            } else {
-              F.raiseError(ex) // unworthy retry
+    private[this] def comprehensive[A](
+      arrow: Kleisli[F, (Tick, Option[Throwable]), Either[Throwable, A]]): F[A] =
+      F.tailRecM[(TickStatus, Option[Throwable]), A](initTS -> None) { case (status, lastError) =>
+        F.attempt(arrow.run((status.tick, lastError))).flatMap[Either[(TickStatus, Option[Throwable]), A]] {
+          case Right(errorOrValue) => // stop retry
+            errorOrValue match {
+              case Left(userError) => F.raiseError(userError) // quit with user provided error
+              case Right(value)    => F.pure(Right(value)) // quit with value
             }
+          case Left(ex) => // retrying
+            for {
+              next <- F.realTimeInstant.map(status.next)
+              ns <- next match {
+                case None => F.raiseError(ex) // run out of policy
+                case Some(ts) => // sleep and continue
+                  F.sleep(ts.tick.snooze.toScala).as(Left(ts -> Some(ex)))
+              }
+            } yield ns
         }
       }
+
+    override def apply[A](fa: F[A]): F[A] =
+      comprehensive(Kleisli(_ => fa.map(Right(_))))
+
+    override def apply[A](tfa: Tick => F[A]): F[A] =
+      comprehensive(Kleisli { case (tick: Tick, _: Option[Throwable]) => tfa(tick).map(Right(_)) })
+
+    override def apply[A](arrow: (Tick, Option[Throwable]) => F[Either[Throwable, A]]): F[A] =
+      comprehensive(Kleisli(arrow.tupled))
   }
 }
