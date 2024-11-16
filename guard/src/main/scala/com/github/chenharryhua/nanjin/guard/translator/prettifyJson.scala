63b3470abda6dd40acb4e351b4dd7c0cd95e93ab
Harry Chen <harry.chen@tabcorp.com.au>
2024-11-16 17:58:23 +1100

    yml

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/translator/prettifyJson.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/translator/prettifyJson.scala
@@ -4,6 +4,7 @@ import io.circe.optics.all.*
 import io.circe.syntax.EncoderOps
 import io.circe.{Encoder, Json}
 import monocle.function.Plated
+import org.apache.commons.lang3.StringUtils
 
 import java.time.Duration
 
@@ -22,4 +23,21 @@ object prettifyJson {
     }
 
   def apply[A: Encoder](a: A): Json = pretty_json(a.asJson)
+
+  def ymlShow(json: Json): List[String] =
+    json.fold(
+      jsonNull = Nil,
+      jsonBoolean = jb => List(jb.toString),
+      jsonNumber = jn => List(jn.toString),
+      jsonString = js => List(js),
+      jsonArray = vj => List(s"[${vj.flatMap(ymlShow).mkString(", ")}]"),
+      jsonObject = { jo =>
+        val jsMap: Map[String, Json] = jo.toMap
+        val maxLength: Int           = jsMap.keys.map(_.length).max
+        jsMap.map { case (key, value) =>
+          s"${StringUtils.rightPad(key, maxLength)}: ${ymlShow(value).mkString(", ")}"
+        }.toList
+      }
+    )
+
 }
