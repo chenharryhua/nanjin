b9a64a563e725dc7205f7848c6607b30057aabdb
Harry Chen <harry.chen@tabcorp.com.au>
2024-11-24 21:28:03 +1100

    worthy

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/metrics/MeasuredRetry.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/metrics/MeasuredRetry.scala
@@ -14,20 +14,20 @@ object MeasuredRetry {
   final class Builder[F[_]] private[guard] (
     isEnabled: Boolean,
     policy: Policy,
-    callback: TickedValue[Throwable] => F[Boolean])
+    worthy: TickedValue[Throwable] => F[Boolean])
       extends EnableConfig[Builder[F]] {
 
     override def enable(isEnabled: Boolean): Builder[F] =
-      new Builder[F](isEnabled, policy, callback)
+      new Builder[F](isEnabled, policy, worthy)
 
-    def worthRetry(f: TickedValue[Throwable] => F[Boolean]): Builder[F] =
-      new Builder[F](isEnabled, policy, f)
+    def worthRetry(worthy: TickedValue[Throwable] => F[Boolean]): Builder[F] =
+      new Builder[F](isEnabled, policy, worthy)
 
     def withPolicy(policy: Policy): Builder[F] =
-      new Builder[F](isEnabled, policy, callback)
+      new Builder[F](isEnabled, policy, worthy)
 
     def withPolicy(f: Policy.type => Policy): Builder[F] =
-      new Builder[F](isEnabled, f(Policy), callback)
+      new Builder[F](isEnabled, f(Policy), worthy)
 
     private[guard] def build(mtx: Metrics[F], zoneId: ZoneId)(implicit F: Async[F]): Resource[F, Retry[F]] =
       for {
@@ -38,7 +38,7 @@ object MeasuredRetry {
         retry <- Resource.eval(TickStatus.zeroth[F](policy, zoneId)).map(ts => new Retry.Impl[F](ts))
       } yield new Retry[F] {
         override def apply[A](fa: F[A]): F[A] =
-          F.guaranteeCase[(FiniteDuration, A)](retry.comprehensive(F.timed(fa), callback)) {
+          F.guaranteeCase[(FiniteDuration, A)](retry.comprehensive(F.timed(fa), worthy)) {
             case Outcome.Succeeded(ra) =>
               F.flatMap(ra)(a => succeedTimer.update(a._1) *> recentCounter.inc(1))
             case Outcome.Errored(_) => failCounter.inc(1)
