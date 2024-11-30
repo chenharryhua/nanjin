4c7fcfed744f20dbf93469a015bdf00155051925
Harry Chen <harry.chen@tabcorp.com.au>
2024-11-30 20:00:06 +1100

    cache

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/CaffeineCache.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/action/CaffeineCache.scala
@@ -4,10 +4,7 @@ import cats.effect.kernel.{Resource, Sync}
 import cats.implicits.{toFlatMapOps, toFunctorOps}
 import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.stats.CacheStats
-import com.github.chenharryhua.nanjin.guard.translator.fmt
 import io.circe.{Encoder, Json}
-
-import scala.concurrent.duration.DurationLong
 
 trait CaffeineCache[F[_], K, V] {
   def getIfPresent(key: K): F[Option[V]]
@@ -18,19 +15,13 @@ trait CaffeineCache[F[_], K, V] {
   def updateWith(key: K)(f: Option[V] => V): F[V]
 
   def invalidate(key: K): F[Unit]
+  def invalidateAll: F[Unit]
 
   def getStats: F[CaffeineCache.Stats]
 }
 
 object CaffeineCache {
-  final case class Stats(
-    hitCount: Long,
-    missCount: Long,
-    loadSuccessCount: Long,
-    loadFailureCount: Long,
-    totalLoadTime: String,
-    evictionCount: Long,
-    evictionWeight: Long)
+  final case class Stats(hitCount: Long, missCount: Long, evictionCount: Long)
 
   implicit val encoderStats: Encoder[Stats] = (a: Stats) =>
     Json.fromString(s"[hit: ${a.hitCount}, miss: ${a.missCount}, evict: ${a.evictionCount}]")
@@ -40,11 +31,7 @@ object CaffeineCache {
     def apply(cs: CacheStats): Stats = Stats(
       hitCount = cs.hitCount(),
       missCount = cs.missCount(),
-      loadSuccessCount = cs.loadSuccessCount(),
-      loadFailureCount = cs.loadFailureCount(),
-      totalLoadTime = fmt.format(cs.totalLoadTime().nano),
-      evictionCount = cs.evictionCount(),
-      evictionWeight = cs.evictionWeight()
+      evictionCount = cs.evictionCount()
     )
   }
 
@@ -54,14 +41,14 @@ object CaffeineCache {
     override def getIfPresent(key: K): F[Option[V]] =
       F.delay(Option(cache.getIfPresent(key)))
 
+    override def put(key: K, value: V): F[Unit] =
+      F.delay(cache.put(key, value))
+
     override def get(key: K, fv: => F[V]): F[V] =
       getIfPresent(key).flatMap {
         case Some(value) => F.pure(value)
-        case None        => F.defer(fv).map(v => cache.get(key, (_: K) => v))
+        case None        => F.defer(fv).flatTap(v => put(key, v))
       }
-
-    override def put(key: K, value: V): F[Unit] =
-      F.delay(cache.put(key, value))
 
     override def updateWith(key: K)(f: Option[V] => V): F[V] =
       getIfPresent(key).map(f).flatTap(v => put(key, v))
@@ -69,15 +56,16 @@ object CaffeineCache {
     override def invalidate(key: K): F[Unit] =
       F.delay(cache.invalidate(key))
 
+    override val invalidateAll: F[Unit] =
+      F.delay(cache.invalidateAll())
+
     override val getStats: F[Stats] =
       F.delay(Stats(cache.stats()))
 
-    val invalidateAll: F[Unit] =
-      F.delay(cache.invalidateAll())
-
+    val cleanUp: F[Unit] = F.delay(cache.cleanUp())
   }
 
   private[guard] def buildCache[F[_], K, V](cache: Cache[K, V])(implicit
     F: Sync[F]): Resource[F, CaffeineCache[F, K, V]] =
-    Resource.make(F.pure(new Impl(cache)))(_.invalidateAll)
+    Resource.make(F.pure(new Impl(cache)))(_.cleanUp)
 }
--- guard/src/test/scala/mtest/guard/CacheTest.scala
+++ guard/src/test/scala/mtest/guard/CacheTest.scala
@@ -1,10 +1,13 @@
 package mtest.guard
 
-import cats.effect.std.Queue
+import cats.effect.std.{Dispatcher, Queue}
 import cats.effect.unsafe.implicits.global
 import cats.effect.{IO, Resource}
+import cats.implicits.{catsSyntaxOptionId, toFunctorFilterOps}
 import com.github.benmanes.caffeine.cache.{Caffeine, RemovalCause, RemovalListener}
 import com.github.chenharryhua.nanjin.guard.TaskGuard
+import com.github.chenharryhua.nanjin.guard.event.ServiceStopCause.Successfully
+import com.github.chenharryhua.nanjin.guard.event.eventFilters
 import com.github.chenharryhua.nanjin.guard.observers.console
 import fs2.Stream
 import org.scalatest.funsuite.AnyFunSuite
@@ -16,11 +19,24 @@ class CacheTest extends AnyFunSuite {
   private val service = TaskGuard[IO]("cache").service("cache")
 
   test("1.put get") {
-    service.eventStream { agent =>
+    val ss = service.eventStream { agent =>
       agent.caffeineCache(Caffeine.newBuilder().build[Int, Int]()).use { cache =>
         cache.put(1, 101) >> cache.getIfPresent(1).flatMap(agent.herald.done(_))
       }
-    }.debug().compile.drain.unsafeRunSync()
+    }.mapFilter(eventFilters.serviceStop).compile.lastOrError.unsafeRunSync()
+    assert(ss.cause == Successfully)
+  }
+
+  test("2.invalidate") {
+    val ss = service.eventStream { agent =>
+      agent
+        .caffeineCache(Caffeine.newBuilder().build[Int, Int])
+        .use { cache =>
+          cache.put(1, 1) >> cache.invalidate(1) >> cache.getIfPresent(1).map(_.ensuring(_.isEmpty))
+        }
+        .void
+    }.mapFilter(eventFilters.serviceStop).compile.lastOrError.unsafeRunSync()
+    assert(ss.cause == Successfully)
   }
 
   test("2.stats") {
@@ -48,7 +64,7 @@ class CacheTest extends AnyFunSuite {
             .metered(1.second)
             .evalMap(i =>
               IO.println(i) >>
-                c.updateWith(i)(_ => i) >>
+                c.get(i, IO.sleep(1000.milli) *> IO(i)) >>
                 c.updateWith(i)(_.fold(0)(_ + 100)) >>
                 c.getIfPresent(i).map(_.exists(_ == i + 100).ensuring(b => b)))
             .compile
@@ -66,7 +82,8 @@ class CacheTest extends AnyFunSuite {
       .updateConfig(_.withMetricReport(_.crontab(_.every3Seconds)))
       .eventStream { agent =>
         val pair = for {
-          queue <- Resource.eval(Queue.unbounded[IO, Int])
+          dispatcher <- Dispatcher.sequential[IO]
+          queue <- Resource.eval(Queue.unbounded[IO, Option[Int]])
           cache <- agent.caffeineCache(
             Caffeine
               .newBuilder()
@@ -74,40 +91,48 @@ class CacheTest extends AnyFunSuite {
               .expireAfterAccess(3.seconds.toJava)
               .removalListener(new RemovalListener[Int, Int] {
                 override def onRemoval(key: Int, value: Int, cause: RemovalCause): Unit =
-                  (cause match {
-                    case RemovalCause.EXPLICIT => IO.println(s"EXPLICIT: $key, $value") >> queue.offer(value)
-                    case RemovalCause.REPLACED => IO.println(s"REPLACED: $key, $value")
+                  dispatcher.unsafeRunSync(cause match {
+                    case RemovalCause.EXPLICIT =>
+                      IO.println(s"EXPLICIT: $key, $value") >> queue.offer(value.some)
+                    case RemovalCause.REPLACED =>
+                      IO.println(s"REPLACED: $key, $value")
                     case RemovalCause.COLLECTED =>
-                      IO.println(s"COLLECTED: $key, $value") >> queue.offer(value)
-                    case RemovalCause.EXPIRED => IO.println(s"EXPIRED: $key, $value") >> queue.offer(value)
-                    case RemovalCause.SIZE    => IO.println(s"SIZE: $key, $value") >> queue.offer(value)
-                  }).unsafeRunSync()
+                      IO.println(s"COLLECTED: $key, $value") >> queue.offer(value.some)
+                    case RemovalCause.EXPIRED =>
+                      IO.println(s"EXPIRED: $key, $value") >> queue.offer(value.some)
+                    case RemovalCause.SIZE =>
+                      IO.println(s"SIZE: $key, $value") >> queue.offer(value.some)
+                  })
               })
               .recordStats()
               .build[Int, Int]())
-          _ <- agent.facilitate("cache")(_.gauge("stats").register(cache.getStats))
-        } yield (cache, Stream.fromQueueUnterminated(queue))
+          _ <- agent.facilitate("cache.stream")(_.gauge("stats").register(cache.getStats))
+        } yield (cache, queue)
 
-        pair.use { case (c, stream) =>
+        pair.use { case (c, queue) =>
           Stream
-            .range(0, 20)
-            .covary[IO]
-            .metered(1.second)
-            .evalMap(i =>
-              c.updateWith(i)(_ => i) >>
-                c.updateWith(i)(_.fold(0)(_ + 100)) >>
-                c.getIfPresent(i).map(_.exists(_ == i + 100).ensuring(b => b)))
-            .onFinalize(IO.sleep(3.seconds))
-            .concurrently(stream.map(i => s"out: $i").debug())
+            .fromQueueNoneTerminated(queue)
+            .concurrently(
+              Stream
+                .range(0, 20)
+                .covary[IO]
+                .metered(1.second)
+                .evalMap(i =>
+                  c.updateWith(i)(_ => i) >>
+                    c.updateWith(i)(_.fold(0)(_ + 100)) >>
+                    c.getIfPresent(i).map(_.exists(_ == i + 100).ensuring(b => b)))
+                .onFinalize(c.invalidateAll >> IO.sleep(1.second) >> queue.offer(None))
+            )
+            .map(i => s"out: $i")
+            .debug()
             .compile
-            .drain >> c.invalidate(19) >> agent.adhoc.report
+            .drain >> agent.adhoc.report
         }
       }
       .evalMap(console.text[IO])
       .compile
       .drain
       .unsafeRunSync()
-
   }
 
 }
