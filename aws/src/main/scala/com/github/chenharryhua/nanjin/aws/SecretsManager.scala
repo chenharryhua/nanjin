3d3588ea132c6febda6ff9be5cf1e407b13f8ee4
Harry Chen <chenharryhua@gmail.com>
2026-02-01 10:00:15 +1100

    SecretsManager

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SecretsManager.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SecretsManager.scala
@@ -1,47 +1,81 @@
 package com.github.chenharryhua.nanjin.aws
 
 import cats.Endo
-import cats.effect.kernel.{Resource, Sync}
-import cats.implicits.{catsSyntaxApplicativeError, toFunctorOps}
+import cats.effect.kernel.{Async, Resource, Sync}
 import org.typelevel.log4cats.Logger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 import software.amazon.awssdk.core.SdkBytes
 import software.amazon.awssdk.services.secretsmanager.model.{GetSecretValueRequest, GetSecretValueResponse}
 import software.amazon.awssdk.services.secretsmanager.{SecretsManagerClient, SecretsManagerClientBuilder}
 
+/** Provides access to AWS Secrets Manager. */
 trait SecretsManager[F[_]] {
+
+  /** Fetch the raw secret value response from AWS.
+   *
+   * Example:
+   * {{{
+   * val secretValue: F[GetSecretValueResponse] = secretsManager.getValue(
+   *   GetSecretValueRequest.builder().secretId("my-secret").build()
+   * )
+   * }}}
+   */
   def getValue(req: GetSecretValueRequest): F[GetSecretValueResponse]
+
+  /** Fetch a secret as a UTF-8 string.
+   *
+   * Example:
+   * {{{
+   * val secret: F[String] = secretsManager.getString("my-secret")
+   * }}}
+   */
   def getString(secretId: String): F[String]
+
+  /** Fetch a secret as raw binary.
+   *
+   * Example:
+   * {{{
+   * val secretBinary: F[SdkBytes] = secretsManager.getBinary("my-binary-secret")
+   * }}}
+   */
   def getBinary(secretId: String): F[SdkBytes]
 }
 
 object SecretsManager {
-  private val name: String = "aws.SecretsManager"
 
-  def apply[F[_]: Sync](f: Endo[SecretsManagerClientBuilder]): Resource[F, SecretsManager[F]] =
+  private val name = "aws.SecretsManager"
+
+  def apply[F[_]](f: Endo[SecretsManagerClientBuilder])(implicit
+                                                        F: Async[F]): Resource[F, SecretsManager[F]] =
     for {
       logger <- Resource.eval(Slf4jLogger.create[F])
-      ps <- Resource.makeCase(logger.info(s"initialize $name").as(new SecretsManagerImpl(f, logger))) {
-        case (cw, quitCase) => cw.shutdown(name, quitCase, logger)
-      }
-    } yield ps
+      _ <- Resource.eval(logger.info(s"initialize $name"))
+      client <- Resource.makeCase(
+        F.blocking(f(SecretsManagerClient.builder()).build())
+      )((sm, quitCase) => shutdown(name, quitCase, logger)(F.blocking(sm.close())))
+    } yield new SecretsManagerImpl(client, logger)
 
   final private class SecretsManagerImpl[F[_]](
-    buildFrom: Endo[SecretsManagerClientBuilder],
-    logger: Logger[F])(implicit F: Sync[F])
-      extends ShutdownService[F] with SecretsManager[F] {
-
-    private lazy val client: SecretsManagerClient = buildFrom(SecretsManagerClient.builder()).build()
+                                                client: SecretsManagerClient,
+                                                logger: Logger[F]
+                                              )(implicit F: Sync[F])
+    extends SecretsManager[F] {
 
     override def getValue(req: GetSecretValueRequest): F[GetSecretValueResponse] =
-      F.blocking(client.getSecretValue(req)).onError(ex => logger.error(ex)(req.toString))
+      blockingF(client.getSecretValue(req), req.toString, logger)
 
     override def getString(secretId: String): F[String] =
-      getValue(GetSecretValueRequest.builder().secretId(secretId).build()).map(_.secretString())
+      blockingF(
+        client.getSecretValue(GetSecretValueRequest.builder().secretId(secretId).build()).secretString(),
+        secretId,
+        logger
+      )
 
     override def getBinary(secretId: String): F[SdkBytes] =
-      getValue(GetSecretValueRequest.builder().secretId(secretId).build()).map(_.secretBinary())
-
-    override protected val closeService: F[Unit] = F.blocking(client.close())
+      blockingF(
+        client.getSecretValue(GetSecretValueRequest.builder().secretId(secretId).build()).secretBinary(),
+        secretId,
+        logger
+      )
   }
 }
