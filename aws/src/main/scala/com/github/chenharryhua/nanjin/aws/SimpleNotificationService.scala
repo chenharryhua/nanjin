16f6af2dba1c25001af2a274182fd36435143f38
Harry Chen <chenharryhua@gmail.com>
2026-02-01 11:38:51 +1100

    SimpleQueueService

--- aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SimpleNotificationService.scala
+++ aws/src/main/scala/com/github/chenharryhua/nanjin/aws/SimpleNotificationService.scala
@@ -8,9 +8,41 @@ import org.typelevel.log4cats.slf4j.Slf4jLogger
 import software.amazon.awssdk.services.sns.model.{PublishRequest, PublishResponse}
 import software.amazon.awssdk.services.sns.{SnsClient, SnsClientBuilder}
 
+/** A simple wrapper around AWS SNS for publishing messages.
+  *
+  * Provides methods to publish messages using a fully constructed `PublishRequest` or a builder-style
+  * function for convenience.
+  *
+  * Example usage:
+  * {{{
+  * import cats.effect.IO
+  * import cats.Endo
+  * import software.amazon.awssdk.services.sns.SnsClientBuilder
+  * import software.amazon.awssdk.services.sns.model.PublishRequest
+  *
+  * val snsResource: Resource[IO, SimpleNotificationService[IO]] =
+  *   SimpleNotificationService[IO](identity[SesClientBuilder])
+  *
+  * val message: PublishRequest = PublishRequest.builder()
+  *   .topicArn("arn:aws:sns:us-east-1:123456789012:MyTopic")
+  *   .message("Hello, SNS!")
+  *   .build()
+  *
+  * val sendIO = snsResource.use(_.publish(message))
+  * }}}
+  */
 trait SimpleNotificationService[F[_]] {
+
+  /** Publishes a message to SNS using a fully constructed `PublishRequest`. */
   def publish(request: PublishRequest): F[PublishResponse]
 
+  /** Publishes a message using a builder function for convenience.
+    *
+    * Example:
+    * {{{
+    * service.publish(_.topicArn("arn:aws:sns:us-east-1:123:topic").message("Hello!"))
+    * }}}
+    */
   final def publish(f: Endo[PublishRequest.Builder]): F[PublishResponse] =
     publish(f(PublishRequest.builder()).build())
 }
@@ -19,8 +51,23 @@ object SimpleNotificationService {
 
   private val name: String = "aws.SNS"
 
+  /** Creates a resource managing an SNS client and the SimpleNotificationService wrapper.
+    *
+    * Example usage:
+    * {{{
+    * import cats.effect.IO
+    * import cats.Endo
+    * import software.amazon.awssdk.services.sns.SnsClientBuilder
+    *
+    * val snsResource: Resource[IO, SimpleNotificationService[IO]] =
+    *   SimpleNotificationService[IO](identity[SesClientBuilder])
+    *
+    * val sendIO = snsResource.use(_.publish(_.topicArn("arn:aws:sns:us-east-1:123:topic").message("Hello!")))
+    * }}}
+    */
   def apply[F[_]](f: Endo[SnsClientBuilder])(implicit
-    F: Async[F]): Resource[F, SimpleNotificationService[F]] =
+    F: Async[F]
+  ): Resource[F, SimpleNotificationService[F]] =
     for {
       logger <- Resource.eval(Slf4jLogger.create[F])
       client <- Resource.makeCase(logger.info(s"initialize $name").map(_ => f(SnsClient.builder).build())) {
@@ -33,6 +80,5 @@ object SimpleNotificationService {
 
     override def publish(request: PublishRequest): F[PublishResponse] =
       blockingF(client.publish(request), request.toString, logger)
-
   }
 }
