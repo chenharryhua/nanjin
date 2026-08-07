63a6d9940b5e489d88a7641abd300797cc235b15
Harry Chen <chenharryhua@gmail.com>
2026-08-08 00:00:22 +1000

    hook

--- guard/src/main/scala/com/github/chenharryhua/nanjin/guard/batch/JobHook.scala
+++ guard/src/main/scala/com/github/chenharryhua/nanjin/guard/batch/JobHook.scala
@@ -75,8 +75,8 @@ object JobHook {
           js.result match {
             case Left(ex) =>
               js.completed.job.kind match {
-                case BatchKind.Quasi => log.warn(Json.obj("fail" -> json), ex)
-                case BatchKind.Value => log.error(Json.obj("fail" -> json), ex)
+                case BatchKind.Quasi => log.warn(Json.obj("nonfatal" -> json), ex)
+                case BatchKind.Value => log.error(Json.obj("critical" -> json), ex)
               }
             case Right(_) => log.good(Json.obj("done" -> json))
           }
