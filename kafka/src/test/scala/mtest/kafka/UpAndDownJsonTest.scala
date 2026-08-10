b37ebba82f7c949975fc2a63ed31ec5ce645f35e
Harry Chen <chenharryhua@gmail.com>
2026-08-11 07:43:11 +1000

    Merge branch '0.21' of github.com:chenharryhua/nanjin into 0.21

--- kafka/src/test/scala/mtest/kafka/UpAndDownJsonTest.scala
+++ kafka/src/test/scala/mtest/kafka/UpAndDownJsonTest.scala
@@ -4,13 +4,16 @@ import cats.effect.IO
 import cats.effect.unsafe.implicits.global
 import com.fasterxml.jackson.databind.JsonNode
 import com.github.chenharryhua.nanjin.kafka.config.KafkaSettings
-import com.github.chenharryhua.nanjin.kafka.serdes.{Primitive, Structured}
+import com.github.chenharryhua.nanjin.kafka.serdes.{
+  KafkaJsonSchema,
+  KafkaJsonSchemaCodec,
+  Primitive,
+  Structured
+}
 import com.github.chenharryhua.nanjin.kafka.{KafkaContext, TopicDef, TopicName}
 import org.scalatest.funsuite.AnyFunSuite
 
 import scala.concurrent.duration.DurationInt
-import com.github.chenharryhua.nanjin.kafka.serdes.KafkaJsonSchemaCodec
-import com.github.chenharryhua.nanjin.kafka.serdes.KafkaJsonSchema
 
 class UpAndDownJsonTest extends AnyFunSuite {
   private val ctx: KafkaContext[IO] =
